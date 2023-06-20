606596ceb9b650a972b1e3c89364295c9fa71d46
Tomek <TK-54-4B>
2023-06-20 17:36:44 +0200

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
   direction: "horizontal",
   loop: false,
   slidesPerView: 3,
-  spaceBetween: 20,
+  spaceBetween: 50,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
