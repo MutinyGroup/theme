27f83eeda6e110a0f46ea3c543ef32d4a8cb999b
Tomek <TK-54-4B>
2023-06-21 18:10:15 +0200

    opinions

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -1,7 +1,18 @@
-const swiper = new Swiper('.mySwiper', {
+const swiper = new Swiper('.swiperServices', {
   direction: "horizontal",
   loop: false,
   slidesPerView: 3,
+  spaceBetween: 50,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper = new Swiper('.swiperOpinions', {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 2,
   spaceBetween: 50,
   navigation: {
     nextEl: ".swiper-button-next",
