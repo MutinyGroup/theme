0092511a76491ae614ac94f2d75b0c9bd28eca40
Tomek <TK-54-4B>
2023-06-21 10:10:13 +0200

    pattern test

--- patterns/banner.php
+++ patterns/banner.php
@@ -13,7 +13,7 @@
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] wide:h-[1180px] desktop:h-[790px] h-[700px] bg-neutral-800 text-[30px]">
     <!-- wp:paragraph {"templateLock":"contentOnly"} -->
-    <p class="test1 edycja-z-kodu2">test</p>
+    <p class="test1 edycja-z-kodu po dodaniu banner test">test</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
