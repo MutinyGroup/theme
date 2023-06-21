1a66b5df3e1c4583ee9744d59e3d0b57a2acedeb
Tomek <TK-54-4B>
2023-06-21 09:53:57 +0200

    pattern test

---
+++ patterns/banner.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Banner test
+ * Slug: mutiny/banner
+ * Categories: test
+ * Block Types: mutiny/footer
+ * Viewport Width: 1024
+ */
+?>
+
+
+
+<!-- wp:group {"templateLock":"contentOnly"} -->
+<div class="wp-block-group banner relative mb-[150px] pt-[90px] overflow-hidden">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] wide:h-[1180px] desktop:h-[790px] h-[700px] bg-neutral-800">
+    <!-- wp:paragraph {"templateLock":"contentOnly"} -->
+    <p class="test1">test</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
