c30555e9b395f8429329316469a1b4501da723b9
Tomek <TK-54-4B>
2023-06-21 10:43:17 +0200

    clean banner pattern added

--- patterns/banner.php
+++ patterns/banner.php
@@ -1,8 +1,15 @@
-
+<?php
+/**
+ * Title: banner
+ * Slug: mutiny/banner
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
 
 <!-- wp:group {"templateLock":"contentOnly"} -->
 <div class="wp-block-group banner relative mb-[150px] pt-[90px] overflow-hidden">
-  <!-- wp:group -->
+  <!-- wp:group {"templateLock":"contentOnly"} -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] wide:h-[1180px] desktop:h-[790px] h-[700px] bg-neutral-800 text-[30px]">
     <!-- wp:paragraph {"templateLock":"contentOnly"} -->
     <p class="test1 edycja z kodu">test</p>
