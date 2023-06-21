db212259c65f0f6e3c70532afdc1f37f31301eaa
Tomek <TK-54-4B>
2023-06-21 10:05:57 +0200

    pattern test

--- patterns/banner.php
+++ patterns/banner.php
@@ -12,9 +12,9 @@
 <!-- wp:group {"templateLock":"contentOnly"} -->
 <div class="wp-block-group banner relative mb-[150px] pt-[90px] overflow-hidden">
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] wide:h-[1180px] desktop:h-[790px] h-[700px] bg-neutral-800">
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] wide:h-[1180px] desktop:h-[790px] h-[700px] bg-neutral-800 text-[30px]">
     <!-- wp:paragraph {"templateLock":"contentOnly"} -->
-    <p class="test1 edycja-z-kodu">test</p>
+    <p class="test1 edycja-z-kodu2">test</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
