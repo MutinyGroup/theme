c640f10474c791e0ccca128f77d8b763d8144a65
Tomek <TK-54-4B>
2023-06-21 13:43:01 +0200

    test

--- functions.php
+++ functions.php
@@ -301,9 +301,9 @@ function mutinyblocks_register_styles() {
 	wp_enqueue_script('swiper', '//cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', '9.4.1');
 	wp_enqueue_style('swiper-css', '//cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css', '9.4.1');
 	wp_enqueue_script('swiperMain', get_theme_file_uri('/assets/swiper-main.js'), '1.0');
-	wp_enqueue_script('google-fonts-api', '//fonts.googleapis.com', '1.0');
-	wp_enqueue_script('google-fonts-gstatic', '//fonts.gstatic.com', '1.0');
-	wp_enqueue_script('google-fonts', '//fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', '1.0');
+	wp_enqueue_style('google-fonts-api', '//fonts.googleapis.com', '1.0');
+	wp_enqueue_style('google-fonts-gstatic', '//fonts.gstatic.com', '1.0');
+	wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', '1.0');
 }
 add_action( 'wp_enqueue_scripts', 'mutinyblocks_register_styles' );
 
