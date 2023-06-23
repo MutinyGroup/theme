1f10f9625dcf70fff33f58cc775aea9912a60e36
Tomek <TK-54-4B>
2023-06-23 14:38:56 +0200

    mobile fix

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -9,13 +9,11 @@ window.addEventListener("load", function () {
     if (mainNavContainer.classList.contains("opacity-[0]")) {
       mainNavContainer.classList.remove("opacity-[0]");
       mainNavContainer.classList.add("opacity-[1]");
-      navMobile.classList.replace('bg-black','bg-[#101010]')
-      navMobile.classList.replace('z-[-2]','z-[3]')
+      mainNavContainer.classList.replace('z-[-2]','z-[3]')
     } else if (mainNavContainer.classList.contains("opacity-[1]")) {
       mainNavContainer.classList.remove("opacity-[1]");
       mainNavContainer.classList.add("opacity-[0]");
-      navMobile.classList.replace('bg-[#101010]', 'bg-black')
-      navMobile.classList.replace('z-[3]', 'z-[-2]')
+      mainNavContainer.classList.replace('z-[3]', 'z-[-2]')
 
     }
   });
