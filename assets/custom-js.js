1092e2bb85b5dfef99a94f1b9b7a2897ec762495
Tomek <TK-54-4B>
2023-06-23 14:48:02 +0200

    mobile fix

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -2,19 +2,20 @@
 
 window.addEventListener("load", function () {
   const btnOpenMenu = document.querySelector(".btn-open-menu");
-  const mainNavContainer = document.querySelector(".mobile-nav-menu");
+  const mainNavMenu = document.querySelector(".mobile-nav-menu");
   const navMobile = document.querySelector(".nav-mobile");
 
   btnOpenMenu.addEventListener("click", () => {
-    if (mainNavContainer.classList.contains("opacity-[0]")) {
-      mainNavContainer.classList.remove("opacity-[0]");
-      mainNavContainer.classList.add("opacity-[1]");
-      mainNavContainer.classList.replace('z-[-2]','z-[3]')
-    } else if (mainNavContainer.classList.contains("opacity-[1]")) {
-      mainNavContainer.classList.remove("opacity-[1]");
-      mainNavContainer.classList.add("opacity-[0]");
-      mainNavContainer.classList.replace('z-[3]', 'z-[-2]')
-
+    if (mainNavMenu.classList.contains("opacity-[0]")) {
+      mainNavMenu.classList.remove("opacity-[0]");
+      mainNavMenu.classList.add("opacity-[1]");
+      mainNavMenu.classList.replace('z-[-2]','z-[3]');
+      mainNavMenu.classList.remove('translate-x-[100%]');      
+    } else if (mainNavMenu.classList.contains("opacity-[1]")) {
+      mainNavMenu.classList.remove("opacity-[1]");
+      mainNavMenu.classList.add("opacity-[0]");
+      mainNavMenu.classList.replace('z-[3]', 'z-[-2]');
+      mainNavMenu.classList.add('translate-x-[100%]');
     }
   });
 });
