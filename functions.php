8a5e2956f5fb9c115f41b9070ce1cdee05ff515e
Tomek <TK-54-4B>
2023-06-23 11:15:39 +0200

    header mobile

--- functions.php
+++ functions.php
@@ -304,6 +304,7 @@ function mutinyblocks_register_styles() {
 	wp_enqueue_style('google-fonts-api', '//fonts.googleapis.com', '1.0');
 	wp_enqueue_style('google-fonts-gstatic', '//fonts.gstatic.com', '1.0');
 	wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', '1.0');
+	wp_enqueue_script('customJS', get_theme_file_uri('/assets/custom-js.js'), '1.0');
 }
 add_action( 'wp_enqueue_scripts', 'mutinyblocks_register_styles' );
 
