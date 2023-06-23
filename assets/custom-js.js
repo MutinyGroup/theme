e28d8359688b68204102359be00a6d02f62a618e
Tomek <TK-54-4B>
2023-06-23 13:52:16 +0200

    header mobile

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -1,16 +1,19 @@
 // navbar mobile handling
 
-const btnOpenMenu = document.querySelector(".btn-open-menu");
-const mainNavContainer = document.querySelector(".mobile-nav-wrapper");
-const mainNavUl = document.querySelector(".main-nav");
-const menuInput = document.querySelector(".menuInput");
+window.addEventListener("load", function () {
+  const btnOpenMenu = document.querySelector(".btn-open-menu");
+  const mainNavContainer = document.querySelector(".mobile-nav-wrapper");
+  const menuInput = document.querySelector(".menuInput");
 
-btnOpenMenu.addEventListener("click", () => {
-  if (mainNavContainer.classList.contains("opacity-[0]")) {
-    mainNavContainer.classList.remove("opacity-[0]");
-    mainNavContainer.classList.add("opacity-[1]");
-  } else if (mainNavContainer.classList.contains("opacity-[1]")) {
-    mainNavContainer.classList.remove("opacity-[1]");
-    mainNavContainer.classList.add("opacity-[0]");
-  }
+  btnOpenMenu.addEventListener("click", () => {
+    if (mainNavContainer.classList.contains("opacity-[0]")) {
+      mainNavContainer.classList.remove("opacity-[0]");
+      mainNavContainer.classList.add("opacity-[1]");
+      console.log('open');
+    } else if (mainNavContainer.classList.contains("opacity-[1]")) {
+      mainNavContainer.classList.remove("opacity-[1]");
+      mainNavContainer.classList.add("opacity-[0]");
+      console.log('close');
+    }
+  });
 });
