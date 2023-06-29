9a5e31e7f1c8c46696b47a0e73cf1d84a901c861
Tomek <TK-54-4B>
2023-06-29 19:11:22 +0200

    new page test

--- functions.php
+++ functions.php
@@ -326,7 +326,7 @@ if ( function_exists( 'register_block_pattern' ) ) {
 
 // tk test
 function custom_body_class( array $classes ) {
-	$new_class = is_page('about') ? get_post_meta( get_the_ID(), 'test-about', true ) : null;
+	$new_class = is_page('about') ? get_post_meta( get_the_ID(), 'body_class', true ) : null;
 	if ( $new_class ) {
 		$classes[] = $new_class;
 	};
