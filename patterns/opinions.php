6669bc87942bc57924cc3d83dec10936e3c11ac6
Tomek <TK-54-4B>
2023-06-30 17:50:59 +0200

    swipers btns hover

--- patterns/opinions.php
+++ patterns/opinions.php
@@ -35,10 +35,10 @@
         <!-- wp:group -->
         <div class="wp-block-group swiper-button-prev w-[100px] absolute desktop:top-[60px] top-[0px] desktop:left-[170px] left-[130px]">
           <!-- wp:paragraph -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180">
-            <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5"/>
-            <path d="M20 28L31 28" stroke="white" stroke-width="1.5"/>
-            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3"/>
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
+            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
+            <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
           </svg>
           <!-- /wp:paragraph -->
         </div>
@@ -47,10 +47,10 @@
         <!-- wp:group -->
         <div class="wp-block-group swiper-button-next w-[100px] absolute desktop:top-[60px] top-[0px] desktop:left-[240px] left-[200px]">
           <!-- wp:paragraph -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none">
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-white">
+            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
             <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5"/>
-            <path d="M20 28L31 28" stroke="white" stroke-width="1.5"/>
-            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3"/>
+            <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
           </svg>
           <!-- /wp:paragraph -->
         </div>
