c356666af7b68d3feb1885bcc8d01d1cbb15f729
Tomek <TK-54-4B>
2023-06-30 16:53:06 +0200

    wp:tags fix

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -180,10 +180,10 @@
         <!-- wp:group -->
         <div class="wp-block-group button-wrapper flex items-center gap-[64px]">
           <!-- wp:buttons -->
-          <div class="wp-block-buttons btn-more ml-[0px] p-[15px_30px] uppercase border-[2px] border-white rounded-full desktop:static absolute desktop:bottom-[-30px] bottom-[-80px] desktop:left-[0px] left-[50%] desktop:translate-x-0 translate translate-x-[-50%] hover:bg-white hover:text-black transition ease-out duration-300 ">
-            <!-- wp:paragraph -->
+          <div class="wp-block-buttons btn-more cursor-pointer ml-[0px] p-[15px_30px] uppercase border-[2px] border-white rounded-full desktop:static absolute desktop:bottom-[-30px] bottom-[-80px] desktop:left-[0px] left-[50%] desktop:translate-x-0 translate translate-x-[-50%] hover:bg-white hover:text-black transition ease-out duration-300">
+            <!-- wp:link -->
             <a href="#" class="uppercase w-[140px] text-[14px] font-bold">Dowiedz się więcej</a>
-            <!-- /wp:paragraph -->
+            <!-- /wp:link -->
           </div>
           <!-- /wp:buttons -->
           <!-- wp:group -->
