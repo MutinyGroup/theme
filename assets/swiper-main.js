0d692df890b3dd19656490400e670d47bb57e5b7
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-07-03 11:13:54 +0200

    Script js depedency

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -1,31 +1,32 @@
-new Swiper('[data-swiper-services="init"]', {
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 3,
-  spaceBetween: 50,
-  navigation: {
-    nextEl: '[data-swiper-services="next"]',
-    prevEl: '[data-swiper-services="prev"]',
-  },
-});
+document.addEventListener("DOMContentLoaded", function(event) {
+  new Swiper('[data-swiper-services="init"]', {
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    navigation: {
+      nextEl: '[data-swiper-services="next"]',
+      prevEl: '[data-swiper-services="prev"]',
+    },
+  });
 
-new Swiper('[data-swiper-opinions="init"]', {
-  spaceBetween: 30,
-  pagination: {
-    el: '[data-swiper-opinions="pagination"]',
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: '[data-swiper-opinions="next"]',
-    prevEl: '[data-swiper-opinions="prev"]',
-  },
-});
+  new Swiper('[data-swiper-opinions="init"]', {
+    spaceBetween: 30,
+    pagination: {
+      el: '[data-swiper-opinions="pagination"]',
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: '[data-swiper-opinions="next"]',
+      prevEl: '[data-swiper-opinions="prev"]',
+    },
+  });
 
-new Swiper('[data-swiper-blog="init"]', {
-  spaceBetween: 30,
-  pagination: {
-    el: '[data-swiper-blog="pagination"]',
-    type: "fraction",
-  },
+  new Swiper('[data-swiper-blog="init"]', {
+    spaceBetween: 30,
+    pagination: {
+      el: '[data-swiper-blog="pagination"]',
+      type: "fraction",
+    },
+  });
 });
-
