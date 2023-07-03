091b46a021a87143472431f387989ae58d23a8e8
Tomek <TK-54-4B>
2023-07-03 15:44:57 +0200

    opinions swiper

--- patterns/blog.php
+++ patterns/blog.php
@@ -172,7 +172,7 @@
       <!-- /wp:group -->
 
       <!-- wp:group -->
-      <div data-swiper-blog="prev" class="wp-block-group group swiper-button-prev w-[56px] h-[56px] desktop:block hidden desktop:block w-[100px] z-0 absolute desktop:top-[40px] top-[180px] desktop:left-[800px]">
+      <div data-swiper-blog="prev" class="wp-block-group group swiper-button-prev z-[1] w-[56px] h-[56px] desktop:block hidden desktop:block w-[100px] z-0 absolute desktop:top-[40px] top-[180px] desktop:left-[800px]">
         <!-- wp:paragraph -->
         <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
             <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
@@ -183,7 +183,7 @@
       </div>
       <!-- /wp:group -->
       <!-- wp:group -->
-      <div data-swiper-blog="next" class="wp-block-group group swiper-button-next w-[56px] h-[56px] desktop:block hidden w-[100px] z-0 absolute desktop:top-[40px] top-[180px] desktop:left-[880px]">
+      <div data-swiper-blog="next" class="wp-block-group group swiper-button-next z-[1] w-[56px] h-[56px] desktop:block hidden w-[100px] z-0 absolute desktop:top-[40px] top-[180px] desktop:left-[880px]">
         <!-- wp:paragraph -->
         <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-white">
             <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
