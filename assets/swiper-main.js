7c77f59586e64d03d95a68ecad7baefaad5e07fc
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-07-03 10:44:08 +0200

    Fix namespaces collisions

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -1,30 +1,30 @@
-const swiper = new Swiper('.swiperServices', {
+const swiper = new Swiper('[data-swiper-services]', {
   direction: "horizontal",
   loop: false,
   slidesPerView: 3,
   spaceBetween: 50,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: "[data-swiper-services] [data-swiper-next]",
+    prevEl: "[data-swiper-services] [data-swiper-prev]",
   },
 });
 
-const swiper2 = new Swiper('.swiperOpinions', {
+const swiper2 = new Swiper('[data-swiper-opinions]', {
   spaceBetween: 30,
   pagination: {
-    el: ".swiper-pagination",
+    el: "[data-swiper-opinions] [data-swiper-pagination]",
     type: "fraction",
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: "[data-swiper-opinions] [data-swiper-next]",
+    prevEl: "[data-swiper-opinions] [data-swiper-prev]",
   },
 });
 
-const swiper3 = new Swiper(".swiperBlog", {
+const swiper3 = new Swiper("[data-swiper-blog]", {
   spaceBetween: 30,
   pagination: {
-    el: ".swiper-pagination",
+    el: "[data-swiper-blog] [data-swiper-pagination]",
     type: "fraction",
   },
 });
