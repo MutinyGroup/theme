6c7845e466843ecd7cb1fc29e0a861e9e753f754
Tomek <TK-54-4B>
2023-07-03 14:01:44 +0200

    opinions swiper

--- patterns/blog.php
+++ patterns/blog.php
@@ -115,6 +115,45 @@
           </div>
           <!-- /wp:group -->
 
+          <!-- wp:paragraph -->
+          <p class="slide-date font-light flex items-center before:content-[''] before:w-[20px] before:h-[1px] before:bg-primaryYellow before:inline-block before:mr-[10px] mb-[20px]">10 / 03 / 2023</p>
+          <!-- /wp:paragraph -->
+
+          <!-- wp:paragraph -->
+          <p class="subtitle inline text-[23px] font-medium leading-[30px] border-b-[1px] border-transparent group-hover:border-primaryYellow group-hover:text-primaryYellow transition ease-out duration-[800ms]">
+            5 urodziny Galerii Navigator - zajrzyjmy na backstage
+          </p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:w-[444px] w-[280px] desktop:h-[675px] h-[520px]">
+          <!-- wp:group -->
+          <div class="wp-block-group img-wrapper mb-[20px]">
+            <!-- wp:group -->
+            <div class="wp-block-group overflow-hidden desktop:h-[435px] rounded-[20px]">
+              <!-- wp:image -->
+              <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
+                <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/blog-post-img/blog-post-img3.png" alt="blog-post-img">
+              </figure>
+              <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:paragraph -->
+            <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+                  <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+                </filter>
+              </defs>
+            </svg>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
 
 
 
