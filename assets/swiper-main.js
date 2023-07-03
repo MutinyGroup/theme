77b317990feb15904d2574b3ec73cc1ac8c82370
Tomek <TK-54-4B>
2023-07-03 17:24:46 +0200

    companies logos mobile swiper

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   new Swiper('[data-swiper-test="init"]', {
     direction: "horizontal",
     slidesPerView: 2,
-    spaceBetween: 30,
     cssMode: true,
     autoplay: {
       delay: 2000,
