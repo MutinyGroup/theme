b7ec3f140f925d7f9be0160dbd1de5bc3f3179a9
Tomek <TK-54-4B>
2023-07-04 19:40:49 +0200

    blog swiper

--- patterns/blog.php
+++ patterns/blog.php
@@ -216,6 +216,49 @@
         <div id="services" class="wp-block-group swiper swiperServices relative desktop:pt-[180px] absolute top-0 desktop:left-[150px] left-[20px]">
           <!-- wp:group -->
           <div class="wp-block-group swiper-wrapper">
+
+            <!-- wp:group -->
+            <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:min-w-[444px] w-[280px] desktop:h-[675px] h-[520px]">
+              <!-- wp:group -->
+              <div class="wp-block-group img-clip-path-rounded mb-[20px]">
+                <!-- wp:group -->
+                <div class="wp-block-group img-clip-path-blog overflow-hidden rounded-[20px]">
+                  <!-- wp:image -->
+                  <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
+                    <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/blog-post-img/blog-post-img1.png" alt="blog-post-img">
+                  </figure>
+                  <!-- /wp:image -->
+                </div>
+                <!-- /wp:group -->
+                <!-- wp:paragraph -->
+                <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+                  <defs>
+                    <filter id="flt_tag">
+                      <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+                      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+                      <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+                    </filter>
+                  </defs>
+                </svg>
+                <!-- /wp:paragraph -->
+              </div>
+              <!-- /wp:group -->
+
+
+
+
+              <!-- wp:paragraph -->
+              <p class="slide-date font-light flex items-center before:content-[''] before:w-[20px] before:h-[1px] before:bg-primaryYellow before:inline-block before:mr-[10px] mb-[20px]">10 / 03 / 2023</p>
+              <!-- /wp:paragraph -->
+
+              <!-- wp:paragraph -->
+              <p class="subtitle inline text-[23px] font-medium leading-[30px] border-b-[1px] border-transparent group-hover:border-primaryYellow group-hover:text-primaryYellow transition ease-out duration-[800ms]">
+                5 urodziny Galerii Navigator - zajrzyjmy na backstage
+              </p>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
             <!-- wp:group -->
             <div class="wp-block-group swiper-slide swiper-slide-services relative z-[2] group p-[25px] desktop:max-w-[354px] max-w-[310px] h-[450px] rounded-[20px] transition ease-out duration-300 cursor-pointer mr-[8px]">
               <!-- wp:group -->
