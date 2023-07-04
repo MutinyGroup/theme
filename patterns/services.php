a3e481e4f97013f9ea4817671f4d06ba2037d645
Tomek <TK-54-4B>
2023-07-04 11:15:52 +0200

    services title fix

--- patterns/services.php
+++ patterns/services.php
@@ -351,9 +351,9 @@
       <div class="wp-block-group gray-border hidden desktop:inline-block w-full h-[2px] bg-[#808080] opacity-[15%]"></div>
       <!-- /wp:group -->
       <!-- wp:buttons -->
-      <div class="wp-block-buttons  desktop:px-0 px-[20px] btn-more ml-[0px] p-[15px_30px] uppercase border-[2px] border-white rounded-full hover:bg-white hover:text-black transition ease-out duration-300">
+      <div class="wp-block-buttons text-center desktop:px-0 px-[20px] btn-more ml-[0px] p-[15px_30px] uppercase border-[2px] border-white rounded-full hover:bg-white hover:text-black transition ease-out duration-300">
         <!-- wp:paragraph -->
-        <a href="#" class="uppercase text-[14px] w-[140px] font-bold">Dowiedz się więcej</a>
+        <a href="#" class="uppercase text-[14px] w-[160px] font-bold">Dowiedz się więcej</a>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:buttons -->
