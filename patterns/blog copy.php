fbe5206917331268d9788b360a39b202d63cf8d3
Tomek <TK-54-4B>
2023-07-04 12:51:21 +0200

    blog swiper test

--- patterns/blog copy.php
+++ patterns/blog copy.php
@@ -7,27 +7,26 @@
  */
 ?>
 <!-- wp:group -->
-<div class="wp-block-group uslugi desktop:mb-[150px] mb-[80px]"></div>
+<div class="wp-block-group blog desktop:pt-[100px] pt-0 desktop:mb-[150px] mb-[60px]">
   <!-- wp:group -->
-  <div class="wp-block-group desktop:mb-[150px] mb-[80px] relative">
+  <div class="wp-block-group relative">
     <!-- wp:group -->
-    <div class="wp-block-group title-wrapper relative container mx-auto desktop:px-0 px-[20px] desktop:mb-[80px] mb-[50px] flex gap-[20px]">
+    <div class="wp-block-group container relative mx-auto desktop:px-0 px-[20px] title-wrapper relative desktop:mb-[90px] mb-[40px] flex gap-[20px]">
       <!-- wp:group -->
-      <div class="wp-block-group bg-flash desktop:top-[-80px] top-[-80px] left-[-20px]">
-        <!-- wp:paragraph -->
-        <p class="hidden">background yellow flash</p>
-        <!-- /wp:paragraph -->
-      </div>
+      <div class="wp-block-group bg-flash scale-[0.8]"></div>
       <!-- /wp:group -->
-      <!-- wp:group -->
-      <div class="wp-block-group bg-shape w-[440px] h-[394px] z-[-1] opacity-[0.20] absolute top-[0px] right-[10px] bg-[url('https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/services-bg-shape.png')] bg-center bg-no-repeat bg-contain">
-        <!-- wp:paragraph -->
-        <p class="hidden">background line shape</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
+      <!-- wp:image -->
+      <figure class="wp-block-image bg-shape desktop:w-[234px] w-[125px] z-[-1] opacity-[0.2] absolute desktop:top-[-130px] top-[-60px] desktop:left-[-60px] left-[-10px] mb-[20px] desktop:max-w-[444px] max-h-[450px]">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/blog-bg-shape.png" alt="background-shape">
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:image -->
+      <figure class="wp-block-image bg-shape hidden desktop:block desktop:w-[470px] w-[180px] z-[-1] opacity-[0.2] absolute top-[-280px] right-[100px] mb-[20px] desktop:max-w-[444px] max-h-[450px]">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/blog-bg-shape2.png" alt="background-shape">
+      </figure>
+      <!-- /wp:image -->
       <!-- wp:paragraph -->
-      <p class="section-title uppercase text-primaryYellow">BLOG</p>
+      <p class="section-title uppercase text-primaryYellow">Blog</p>
       <!-- /wp:paragraph -->
       <!-- wp:group -->
       <div class="wp-block-group uslugi-title desktop:leading-[55px] leading-[30px]">
@@ -37,71 +36,18 @@
         </p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="relative desktop:text-[50px] text-[26px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[20px] before:mr-[5px] before:bottom-0 before:left-0 desktop:before:w-[120px] before:w-[30px]  desktop:before:h-[3px] before:h-[2px] before:bg-white">z Mutiny group</p>
+        <p class="relative desktop:text-[50px] text-[26px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[5px] before:bottom-0 before:left-0 desktop:before:w-[120px] before:w-[30px]  desktop:before:h-[3px] before:h-[3px] before:bg-white">z Mutiny Group</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-
-    
     <!-- wp:group -->
     <div class="desktop:mt-[-120px] w-full overflow-hidden">
-
-      <!-- wp:group -->
-      <div data-swiper-services="init" class="wp-block-group swiper swiperServices relative desktop:pt-[180px] absolute top-0 desktop:left-[150px] left-[20px]">
+    <!-- wp:group -->
+      <div data-swiper-blog="init" class="wp-block-group swiper swiperBlog mt-[-170px] pt-[160px] desktop:pt-[180px] absolute top-[200px] desktop:left-[150px] left-[20px]">
         <!-- wp:group -->
         <div class="wp-block-group swiper-wrapper">
-          <!-- wp:group -->
-          <div class="wp-block-group swiper-slide swiper-slide-services relative z-[2] group p-[25px] desktop:max-w-[354px] max-w-[310px] h-[450px] rounded-[20px] transition ease-out duration-300 cursor-pointer mr-[8px]">
-            <!-- wp:group -->
-            <div class="wp-block-group img-clip-path-wrapper w-full h-full rounded-tr-[20px] rounded-br-[20px] overflow-hidden absolute left-0 top-0 z-[-1]">
-              <!-- wp:image -->
-              <figure class="wp-block-image img-clip-path-services">
-                <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/swiper-bg1.png" alt="services-backgroun-image">
-              </figure>
-              <!-- /wp:image -->
-              <!-- wp:paragraph -->
-              <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <filter id="flt_tag">
-                    <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
-                    <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
-                    <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-                  </filter>
-                </defs>
-              </svg>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group slide-title absolute desktop:bottom-[25px] bottom-[75px] left-[25px]">
-              <!-- wp:paragraph -->
-              <p class="mr-[20px] text-[25px] font-semibold mb-[15px] underline-offset-[7px] group-hover:underline group-hover:decoration-primaryYellow group-hover:translate-y-[-80px] transition ease-out duration-500">Marketing i PR
-              </p>
-              <!-- /wp:paragraph -->
-              <!-- wp:buttons -->
-              <div class="wp-block-buttons btn-swiper-more group text-[14px] uppercase transition ease-out duration-300">
-                <!-- wp:paragraph -->
-                <a href="#" class="inline-block">
-                  Dowiedz się więcej
-                  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none " class="inline-block ml-[10px] ">
-                    <rect x="1" y="1" width="34" height="34" rx="18" stroke="#D5D5D5" stroke-width="2" fill="transparent" class="transition ease-out duration-300 group-hover:fill-[#FEC400] group-hover:stroke-[#FEC400]"/>
-                    <path d="M19 22L23 18.0108L19.0217 14" stroke="white"/>
-                    <path d="M12 18L20 18" stroke="white" />
-                  </svg>
-                </a>
-                <!-- /wp:paragraph -->
-              </div>
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:paragraph -->
-            <p class="hover-text w-[250px] mt-[270px] translate-y-[70px] opacity-[0%] group-hover:opacity-[100%] desktop:group-hover:translate-y-[10px] group-hover:translate-y-[-35px] transition-all ease-out duration-500 text-[15px] font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet in aspernatur alias ipsa, fuga ut!
-            </p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-
           <!-- wp:group -->
           <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:min-w-[444px] w-[280px] desktop:h-[675px] h-[520px]">
             <!-- wp:group -->
@@ -224,30 +170,29 @@
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
-
-
         </div>
         <!-- /wp:group -->
 
         <!-- wp:group -->
-        <div data-swiper-services="prev" class="wp-block-group group swiper-button-prev desktop:inline-block hidden w-[56px] h-[56px] absolute desktop:top-[90px] top-[35px] desktop:left-[800px] left-[190px]">
+        <div data-swiper-blog="prev" class="wp-block-group group swiper-button-prev z-[1] w-[56px] h-[56px] desktop:block hidden w-[100px] z-0 absolute desktop:top-[25px] top-[180px] desktop:left-[800px]">
           <!-- wp:paragraph -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
-            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
-            <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-            <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group rotate-180 fill-transparent group-hover:fill-white">
+              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
+              <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+              <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            </svg>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
+
         <!-- wp:group -->
-        <div data-swiper-services="next" class="wp-block-group group swiper-button-next desktop:inline-block hidden w-[56px] h-[56px] absolute desktop:top-[90px] top-[35px] desktop:left-[870px] left-[260px]">
+        <div data-swiper-blog="next" class="wp-block-group group swiper-button-next z-[1] w-[56px] h-[56px] desktop:block hidden desktop:block w-[100px] z-0 absolute desktop:top-[25px] top-[180px] desktop:left-[880px]">
           <!-- wp:paragraph -->
           <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-white">
-            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
-            <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-            <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-          </svg>
+              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
+              <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+              <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            </svg>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -258,15 +203,17 @@
     </div>
     <!-- /wp:group -->
 
+
+
     <!-- wp:group -->
-    <div class="wp-block-group button-wrapper container mx-auto mt-[50px] flex items-center justify-center gap-[64px] cursor-pointer">
+    <div class="wp-block-group button-wrapper flex items-center justify-center desktop:gap-[80px] gap-[20px] cursor-pointer">
       <!-- wp:group -->
-      <div class="wp-block-group gray-border hidden desktop:inline-block w-full h-[2px] bg-[#808080] opacity-[15%]"></div>
+      <div class="wp-block-group gray-border w-full h-[2px] bg-[#808080] opacity-[25%]"></div>
       <!-- /wp:group -->
       <!-- wp:buttons -->
-      <div class="wp-block-buttons text-center px-[20px] btn-more ml-[0px] p-[15px_30px] uppercase border-[2px] border-white rounded-full hover:bg-white hover:text-black transition ease-out duration-300">
+      <div class="wp-block-buttons btn-more w-[390px] ml-[0px] p-[15px_30px] uppercase border-[2px] border-white rounded-full hover:bg-white hover:text-black transition ease-out duration-300">
         <!-- wp:paragraph -->
-        <a href="#" class="uppercase text-[14px] w-[160px] font-bold">Dowiedz się więcej</a>
+        <a href="#" class="uppercase w-[220px] text-[14px] font-bold">Zobacz wszystkie realizacje</a>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:buttons -->
@@ -276,9 +223,3 @@
   <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
-
-<!-- wp:image -->
-<figure class="wp-block-image img-separator desktop:mb-[150px] mb-[80px] desktop:h-[600px] h-[180px]">
-  <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/section-separator.jpg" alt="image-page-separator">
-</figure>
-<!-- /wp:image -->
