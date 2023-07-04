efb9449a1b44891e3458c44ae9b523c5f3f64515
Tomek <TK-54-4B>
2023-07-04 14:48:09 +0200

    wp:tags fixing

--- patterns/blog.php
+++ patterns/blog.php
@@ -172,8 +172,8 @@
       </div>
       <!-- /wp:group -->
 
-      <!-- wp:group {"attributes":"data"} -->
-      <div data-swiper-blog="prev" class="wp-block-group group swiper-button-prev z-[1] w-[56px] h-[56px] desktop:block hidden w-[100px] z-0 absolute desktop:top-[25px] top-[180px] desktop:left-[800px]">
+      <!-- wp:group -->
+      <div id="prev" class="wp-block-group group swiper-button-prev z-[1] w-[56px] h-[56px] desktop:block hidden w-[100px] z-0 absolute desktop:top-[25px] top-[180px] desktop:left-[800px]">
         <!-- wp:paragraph -->
         <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group rotate-180 fill-transparent group-hover:fill-white">
             <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
