95e02f577f4ab9c9c34c8a42cb6c07ce3678b8f3
Tomek <TK-54-4B>
2023-07-04 12:05:22 +0200

    services swiper mobile fix

--- patterns/services.php
+++ patterns/services.php
@@ -60,7 +60,7 @@
     <!-- /wp:paragraph -->
     
     <!-- wp:group -->
-    <div class="mt-[-120px] w-full overflow-hidden">
+    <div class="desktop:mt-[-120px] w-full overflow-hidden">
 
       <!-- wp:group -->
       <div data-swiper-services="init" class="wp-block-group swiper swiperServices relative desktop:pt-[180px] absolute top-0 desktop:left-[150px] left-[20px]">
