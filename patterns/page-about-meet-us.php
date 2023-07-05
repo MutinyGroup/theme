45fd5a1f859e5c80a47c931e65523b80ede0bceb
Tomek <TK-54-4B>
2023-07-05 14:24:28 +0200

    page about - meet us  gallery

--- patterns/page-about-meet-us.php
+++ patterns/page-about-meet-us.php
@@ -177,6 +177,25 @@
     </div>
     <!-- /wp:grup -->
 
+    <!-- wp:group -->
+    <div class="paragraph-wrapper relative w-full overflow-x-hidden">
+      <!-- wp:paragraph -->
+      <svg class="w-[1400px] z-[-1] absolute top-[0px] left-[200px]" xmlns="http://www.w3.org/2000/svg" width="1051" height="810" viewBox="0 0 1051 810" fill="none">
+        <path d="M1 1.45249L938.266 329.35L938.596 329.465L938.927 329.35L1406.89 165.634V409.787L938.596 573.618L234.979 327.462L233.649 326.996V328.406V807.61L1 726.219V1.45249Z" stroke="#FFFFFF" stroke-width="2"/>
+      </svg>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p
+      class="hidden mb-[150px] text-[35px] font-semibold leading-[42px] w-[897px]"
+      >
+      Naszą misją jest lorem ipsum dolor sit amet, consectetur adipiscing
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua.
+    </p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+
 
   </div>
   <!-- /wp:group -->
