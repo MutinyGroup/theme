a45c6651b8318c12797d924f5203c3f978d556b5
Tomek <TK-54-4B>
2023-07-05 17:22:18 +0200

    page-about team mutiny

---
+++ patterns/page-about-team.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: page-about-meet-us
+ * Slug: page-about-meet-us
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group -->
+<div class="wp-block-group meet-us overflow-hidden">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]">
+    <!-- wp:group -->
+    <div class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[120px] mb-[60px] flex">
+      <!-- wp:paragraph -->
+      <svg class="z-[-1] absolute top-0 right-[-200px]" xmlns="http://www.w3.org/2000/svg" width="470" height="396" viewBox="0 0 470 396" fill="none">
+        <path d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z" stroke="#FFFFFF" stroke-width="2"/>
+      </svg>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="section-title desktop:basis-auto desktop:w-[145px] basis-[80%] uppercase text-primaryYellow ">Team Mutiny</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group uslugi-title">
+        <!-- wp:paragraph -->
+        <p class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">
+        Zgrany zespół  <br> to
+          <span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black">podstawa</span>
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -10,7 +10,7 @@
 <!-- wp:group -->
 <div class="wp-block-group video relative container mx-auto overscroll-x-hidden mb-[150px]">
   <!-- wp:group -->
-  <div class="wp-block-group group img-clip-path-rounded">
+  <div class="wp-block-group group cursor-pointer img-clip-path-rounded">
     <!-- wp:group -->
     <div class="wp-block-group img-clip-path-video overflow-hidden rounded-tr-[22px]">
       <!-- wp:image -->
