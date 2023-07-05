7aa31651966897a6f6dfa2eb641ba76a17251637
Tomek <TK-54-4B>
2023-07-05 12:40:28 +0200

    page about - meet us section

---
+++ patterns/page-about-meet-us.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Title: about-meet-us
+ * Slug: about-meet-us
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group -->
+<!-- wp:group -->
+<div class="wp-block-group meet-us desktop:mb-[150px] mb-[200px]">
+  <!-- wp:group -->
+  <div class="wp-block-group relative desktop:px-0 px-[20px]">
+    <!-- wp:group -->
+    <div class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[120px] mb-[60px] flex desktop:gap-[20px] gap-[0px]">
+      <!-- wp:paragraph -->
+      <p class="section-title desktop:basis-auto desktop:w-[145px] basis-[80%] uppercase text-primaryYellow ">Poznajmy się</p>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group uslugi-title">
+        <!-- wp:paragraph -->
+        <p class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">
+          Kampanie realizujemy
+          <span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-white">kompleksowo</span>
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+    <!-- /wp:group -->
+    
+    <!-- wp:group -->
+    <div class="wp-block-group cols-wrapper flex basis mb-[100px]">
+      <!-- wp:group -->
+      <div class="wp-block-group col1 pl-[105px] basis-[40%]">
+        <!-- wp:paragraph -->
+        <p class="max-w-[173px] text-[16px] leading-[21px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor.
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <div class=" wp-block-groupcol2 basis-[60%]">
+        <!-- wp:paragraph -->
+        <p class="text-[20px] leading-[30px] mb-[50px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="text-[16px] leading-[21px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <hr class="mb-[50px]" />
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
