bc5f1ebcc7e35c79d6f258ff79c9dc5ea0683307
Tomek <TK-54-4B>
2023-07-05 13:10:15 +0200

    page about - meet us section wp:tags

--- patterns/page-about-meet-us.php
+++ patterns/page-about-meet-us.php
@@ -10,7 +10,7 @@
 <!-- wp:group -->
 <div class="wp-block-group meet-us desktop:mb-[150px] mb-[200px]">
   <!-- wp:group -->
-  <div class="wp-block-group relative desktop:px-0 px-[20px]">
+  <div class="wp-block-group container relative desktop:px-0 px-[20px]">
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[120px] mb-[60px] flex desktop:gap-[20px] gap-[0px]">
       <!-- wp:paragraph -->
