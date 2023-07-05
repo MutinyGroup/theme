57f21cd7e2292f9b899c0d6a23291b5be4d6766a
Tomek <TK-54-4B>
2023-07-05 13:37:40 +0200

    page about - meet us bg shape

--- patterns/page-about-meet-us.php
+++ patterns/page-about-meet-us.php
@@ -13,6 +13,16 @@
   <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]">
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[120px] mb-[60px] flex desktop:gap-[20px] gap-[0px]">
+      <!-- wp:image -->
+      <figure
+        class="wp-block-iamge bg-shape w-[470px] absolute top-[0px] right-[-200px]"
+      >
+        <img
+          src="./assets/img/page-about-bg-shape.png"
+          alt="bg-shape"
+        />
+      </figure>
+      <!-- /wp:image -->
       <!-- wp:paragraph -->
       <p class="section-title desktop:basis-auto desktop:w-[145px] basis-[80%] uppercase text-primaryYellow ">Poznajmy się</p>
       <!-- /wp:paragraph -->
@@ -21,7 +31,7 @@
         <!-- wp:paragraph -->
         <p class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">
           Kampanie realizujemy
-          <span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-white">kompleksowo</span>
+          <span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black">kompleksowo</span>
         </p>
         <!-- /wp:paragraph -->
       </div>
