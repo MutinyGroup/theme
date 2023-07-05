7e3a19d429c83dad21431a9341e56767e7c85140
Tomek <TK-54-4B>
2023-07-05 17:03:49 +0200

    page about: video

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -10,11 +10,11 @@
 <!-- wp:group -->
 <div class="wp-block-group video relative container mx-auto overscroll-x-hidden mb-[150px]">
   <!-- wp:group -->
-  <div class="wp-block-group img-clip-path-rounded">
+  <div class="wp-block-group group img-clip-path-rounded">
     <!-- wp:group -->
     <div class="wp-block-group img-clip-path-video overflow-hidden rounded-tr-[22px]">
       <!-- wp:image -->
-      <figure class="wp-block-image ">
+      <figure class="wp-block-image grayscale group-hover:grayscale-0 transition ease-out duration-300">
         <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/about-video.jpg" alt="image">
       </figure>
       <!-- /wp:image -->
