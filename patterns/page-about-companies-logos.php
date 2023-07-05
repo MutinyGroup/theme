68f5a01e4f49cfec4778824baea70bc94203dbcb
Tomek <TK-54-4B>
2023-07-05 20:17:08 +0200

    logos companies

---
+++ patterns/page-about-companies-logos.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Title: companies-logos
+ * Slug: companies-logos
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+<!-- wp:group -->
+<div class="wp-block-group zaufali-nam mb-0">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+    <!-- wp:paragraph -->
+    <p class="uppercase mb-[82px] text-[40px] font-medium text-center">Zaufali nam</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:group -->
+    <div id="logos" class="wp-block-group swiper desktop:hidden">
+      <!-- wp:group -->
+      <div class="wp-block-group swiper-wrapper">
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-serenada.png" alt="logo-serenada">
+        </figure>
+        <!-- /wp:core/image -->
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-westfield.png" alt="logo-westfield">
+        </figure>
+        <!-- /wp:core/image -->
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-metropolia.png" alt="logo-metropolia">
+        </figure>
+        <!-- /wp:core/image -->
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-efc.png" alt="logo-efc">
+        </figure>
+        <!-- /wp:core/image -->
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-navigator.png" alt="logo-navigator">
+        </figure>
+        <!-- /wp:core/image -->
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-sfera.png" alt="logo-sfera">
+        </figure>
+        <!-- /wp:core/image -->
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-citypark.png" alt="logo-citypark">
+        </figure>
+        <!-- /wp:core/image -->
+        <!-- wp:core/image -->
+        <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-gorski.png" alt="logo-gorski">
+        </figure>
+        <!-- /wp:core/image -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group companies-wrapper hidden desktop:flex desktop:gap-[110px] gap-[40px] flex-wrap items-center justify-between">
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-serenada.png" alt="logo-serenada">
+      </figure>
+      <!-- /wp:core/image -->
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-westfield.png" alt="logo-westfield">
+      </figure>
+      <!-- /wp:core/image -->
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-metropolia.png" alt="logo-metropolia">
+      </figure>
+      <!-- /wp:core/image -->
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-efc.png" alt="logo-efc">
+      </figure>
+      <!-- /wp:core/image -->
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-navigator.png" alt="logo-navigator">
+      </figure>
+      <!-- /wp:core/image -->
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-sfera.png" alt="logo-sfera">
+      </figure>
+      <!-- /wp:core/image -->
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-citypark.png" alt="logo-citypark">
+      </figure>
+      <!-- /wp:core/image -->
+      <!-- wp:core/image -->
+      <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logo-companies/logo-gorski.png" alt="logo-gorski">
+      </figure>
+      <!-- /wp:core/image -->
+    </div>
+    <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
