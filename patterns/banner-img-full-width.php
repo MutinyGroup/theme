04a4949f13a265ff73d9dff77f7a773814c3dd08
Tomek <TK-54-4B>
2023-07-05 09:24:13 +0200

    page about banner

--- patterns/banner-img-full-width.php
+++ patterns/banner-img-full-width.php
@@ -6,78 +6,79 @@
  * Viewport Width: 1280
  */
 ?>
+
+<!-- wp:group -->
+<div class="wp-block-group about-banner h-screen mb-[150px]">
+  <!-- wp:group -->
+  <div
+    class="wp-block-group container relative mx-auto desktop:px-0 px-[20px] pt-[120px]"
+  >
     <!-- wp:group -->
-    <div class="wp-block-group about-banner mb-[150px]">
+    <div
+      class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px]"
+    >
+      <!-- wp:group -->
+      <div class="wp-block-group bg-flash top-[70px] z-[-1]"></div>
+      <!-- /wp:group -->
       <!-- wp:group -->
       <div
-        class="wp-block-group container relative mx-auto desktop:px-0 px-[20px] pt-[120px]"
+        class="wp-block-group title-wrapper relative container mx-auto flex desktop:gap-[20px] gap-[0px]"
       >
         <!-- wp:group -->
-        <div
-          class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px]"
+        <div class="wp-block-group bg-flash hidden"></div>
+        <!-- /wp:group -->
+        <!-- wp:paragraph -->
+        <p
+          class="section-title desktop:basis-auto basis-[20%] uppercase text-primaryYellow"
         >
-          <!-- wp:group -->
-          <div class="wp-block-group bg-flash top-[70px] z-[-1]"></div>
-          <!-- /wp:group -->
-          <!-- wp:group -->
-          <div
-            class="wp-block-group title-wrapper relative container mx-auto flex desktop:gap-[20px] gap-[0px]"
+          O nas
+        </p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group uslugi-title">
+          <!-- wp:paragraph -->
+          <p
+            class="uppercase desktop:text-[60px] text-[24px] font-medium desktop:leading-[55px] leading-[32px]"
           >
-            <!-- wp:group -->
-            <div class="wp-block-group bg-flash hidden"></div>
-            <!-- /wp:group -->
-            <!-- wp:paragraph -->
-            <p
-              class="section-title desktop:basis-auto basis-[80%] uppercase text-primaryYellow"
+            Nie boimy sie <br />
+            przekraczać <br />
+            <span
+              class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block before:mr-[6px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
+              >granic.</span
             >
-              O nas
-            </p>
-            <!-- /wp:paragraph -->
-            <!-- wp:group -->
-            <div class="wp-block-group uslugi-title">
-              <!-- wp:paragraph -->
-              <p
-                class="uppercase desktop:text-[60px] text-[24px] font-medium desktop:leading-[55px] leading-[32px]"
-              >
-                Nie boimy sie <br />
-                przekraczać
-                <span
-                  class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block before:mr-[6px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[20px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
-                  >granic.</span
-                >
-              </p>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-          </div>
-          <!-- /wp:group -->
+          </p>
+          <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
       </div>
       <!-- /wp:group -->
-      <!-- wp:group -->
-      <div class="wp-block-group about-banner-img relative desktop:ml-[80px]">
-        <!-- wp:image -->
-        <figure class="wp-block-image object-cover h-full">
-          <img
-            src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/about-banner-bg.jpg"
-            alt="bg-image"
-          />
-        </figure>
-        <!-- /wp:image -->
-        <!-- wp:buttons -->
-        <div
-          class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] desktop:left-[1080px] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
-        >
-          <!-- wp:paragraph -->
-          <p>Zobacz</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="uppercase text-[25px] font-extrabold">WIĘCEJ</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:buttons -->
-      </div>
-      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+  <!-- wp:group -->
+  <div class="wp-block-group about-banner-img relative desktop:ml-[80px]">
+    <!-- wp:image -->
+    <figure class="wp-block-image object-cover h-full">
+      <img
+        src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/about-banner-bg.jpg"
+        alt="bg-image"
+      />
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:buttons -->
+    <div
+      class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[200px] desktop:left-[1080px] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+    >
+      <!-- wp:paragraph -->
+      <p>Zobacz</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="uppercase text-[25px] font-extrabold">WIĘCEJ</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:buttons -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
