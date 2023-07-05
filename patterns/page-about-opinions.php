fc328fc775496198a347bcf8df7ce73ad0cd3dd5
Tomek <TK-54-4B>
2023-07-05 20:43:36 +0200

    page-about opinions

---
+++ patterns/page-about-opinions.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * Title: page-about-opinions
+ * Slug: page-about-opinions
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+<!-- wp:group -->
+<div class="wp-block-group opinions desktop:mb-[150px] mb-[60px]">
+  <!-- wp:group -->
+  <div class="wp-block-group relative mx-auto desktop:ml-[83px] desktop:px-0 px-[20px] overflow-hidden bg-white">
+    <!-- wp:group -->
+    <div class="wp-block-group title-wrapper relative container mx-auto desktop:ml-[120px] ml-0 desktop:pt-[100px] desktop:mt-0 mt-[60px] desktop:mb-[80px] flex gap-[20px]">
+      <!-- wp:paragraph -->
+      <p class="section-title uppercase text-primaryYellow">Opinie</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group uslugi-title">
+        <!-- wp:paragraph -->
+        <p class="uppercase desktop:text-[35px] text-[26px] font-medium leading-[32px]">
+          Co mówią nasi klienci?
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+    
+    <!-- wp:group -->
+    <div class="wp-block-group swiper-section-wrapper relative desktop:flex desktop:after:content-[''] desktop:after:w-[440px] after:h-full after:bg-black after:opacity-[60%] desktop:after:z-[1] after:absolute after:right-0 after:top-0">
+
+      <!-- wp:group -->
+      <div class="wp-block-group swiper-nav-wrapper desktop:block hidden relative basis-[50%] z-0">
+        <!-- wp:group -->
+        <div id="opinions-prev" class="wp-block-group group swiper-button-prev w-[56px] h-[56px] absolute desktop:top-[60px] top-[0px] desktop:left-[170px] left-[130px]">
+          <!-- wp:paragraph -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
+            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
+            <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+          </svg>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div id="opinions-next" class="wp-block-group group swiper-button-next w-[56px] h-[56px] absolute desktop:top-[60px] top-[0px] desktop:left-[240px] left-[200px]">
+          <!-- wp:paragraph -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-white">
+            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
+            <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+        </svg>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div id="opinions-pagination" class="wp-block-group swiper-pagination text-primaryYellow text-[50px] absolute desktop:left-[10px] left-[100px] desktop:top-[110px] top-[-70px]">
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div id="opinions" class="wp-block-group swiper swiperOpinions">
+        <!-- wp:group -->
+        <div class="wp-block-group swiper-wrapper">
+          <!-- wp:group -->
+          <div class="wp-block-group swiper-slide relative z-[2] group desktop:max-w-[444px] max-w-[280px] desktop:h-[430px] max-h-[530px] cursor-pointer mr-[20px]">
+            <!-- wp:group -->
+            <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
+              <!-- wp:paragraph -->
+              <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
+                <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
+              </svg>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:paragraph -->
+            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <!-- /wp:paragraph -->
+            <!-- wp:group -->
+            <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
+              <!-- wp:image -->
+              <figure class="wp-block-image avatar rounded-full w-[60px] overflow-hidden">
+                <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/avatars-opinions/avatar1.jpg" alt="avatar">
+              </figure>
+              <!-- /wp:image -->
+              <!-- wp:group -->
+              <div class="wp-block-group author-wrapper">
+                <!-- wp:paragraph -->
+                <p class="author-name font-semibold">Mateusz Rowan</p>
+                <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p class="author-company font-light">Dyrektor, Galeria Metropolia</p>
+                <!-- /wp:paragraph -->
+              </div>
+              <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+          </div>
+          <!-- /wp:group -->
+          <!-- wp:group -->
+          <div class="wp-block-group swiper-slide relative z-[2] group desktop:max-w-[444px] max-w-[280px] desktop:h-[430px] max-h-[530px] cursor-pointer mr-[20px]">
+            <!-- wp:group -->
+            <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
+              <!-- wp:paragraph -->
+              <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
+                <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
+              </svg>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:paragraph -->
+            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <!-- /wp:paragraph -->
+            <!-- wp:group -->
+            <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
+              <!-- wp:image -->
+              <figure class="wp-block-image avatar rounded-full w-[60px] overflow-hidden">
+                <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/avatars-opinions/avatar1.jpg" alt="avatar">
+              </figure>
+              <!-- /wp:image -->
+              <!-- wp:group -->
+              <div class="wp-block-group author-wrapper">
+                <!-- wp:paragraph -->
+                <p class="author-name font-semibold">Mateusz Rowan</p>
+                <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p class="author-company font-light">Dyrektor, Galeria Metropolia</p>
+                <!-- /wp:paragraph -->
+              </div>
+              <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+          </div>
+          <!-- /wp:group -->
+          <!-- wp:group -->
+          <div class="wp-block-group swiper-slide relative z-[2] group desktop:max-w-[444px] max-w-[280px] desktop:h-[430px] max-h-[530px] cursor-pointer mr-[20px]">
+            <!-- wp:group -->
+            <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
+              <!-- wp:paragraph -->
+              <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
+                <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
+              </svg>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+            <!-- wp:paragraph -->
+            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <!-- /wp:paragraph -->
+            <!-- wp:group -->
+            <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
+              <!-- wp:image -->
+              <figure class="wp-block-image avatar rounded-full w-[60px] overflow-hidden">
+                <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/avatars-opinions/avatar1.jpg" alt="avatar">
+              </figure>
+              <!-- /wp:image -->
+              <!-- wp:group -->
+              <div class="wp-block-group author-wrapper">
+                <!-- wp:paragraph -->
+                <p class="author-name font-semibold">Mateusz Rowan</p>
+                <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p class="author-company font-light">Dyrektor, Galeria Metropolia</p>
+                <!-- /wp:paragraph -->
+              </div>
+              <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
