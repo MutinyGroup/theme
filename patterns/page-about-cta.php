f1a0c2260ded4742205cb5fffbcdfeb23f4654cf
Tomek <TK-54-4B>
2023-07-05 20:45:48 +0200

    page-about cta

--- patterns/page-about-cta.php
+++ patterns/page-about-cta.php
@@ -31,7 +31,7 @@
         </p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="relative uppercase italic before:content-[''] before:inline-block before:mr-[-5px] before:bottom-0 before:left-0 desktop:before:w-[120px] before:w-[30px] before:h-[2px] before:bg-white">Mutiny.</p>
+        <p class="relative uppercase italic before:content-[''] before:inline-block before:mr-[-5px] before:bottom-0 before:left-0 desktop:before:w-[120px] before:w-[30px] before:h-[2px] before:bg-black">Mutiny<span class="text-[100px] italic">.</span></p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
