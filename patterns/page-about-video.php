5be7680a1b0ee640ddef7029d309938e34d9e5a8
Tomek <TK-54-4B>
2023-07-05 16:09:08 +0200

    page about: video player

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -7,4 +7,6 @@
  */
 ?>
 
-page-video test
+<!-- wp:core/video -->
+<figure class="wp-block-video"><video controls src="https://www.youtube.com/watch?v=P8CnBbLEwW0"></video><figcaption class="wp-element-caption">My video</figcaption></figure>
+<!-- /wp:core/video -->
