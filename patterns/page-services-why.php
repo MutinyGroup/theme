f2f4760517b582168d2000e6bcfd6981333aed87
Tomek <TK-54-4B>
2023-07-06 15:59:06 +0200

    page-services why section

---
+++ patterns/page-services-why.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: page-services-why
+ * Slug: page-services-why
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group -->
+<div class="wp-block-group page-about-services mt-[150px] overflow-hidden">
+  <!-- wp:group -->
+  <div
+    class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
+  >
+    <!-- wp:group -->
+    <div
+      class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[120px] mb-[60px] flex"
+    >
+      <!-- wp:paragraph -->
+      <svg
+        class="z-[-1] absolute top-0 right-[-200px]"
+        xmlns="http://www.w3.org/2000/svg"
+        width="470"
+        height="396"
+        viewBox="0 0 470 396"
+        fill="none"
+      >
+        <path
+          d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z"
+          stroke="#FFFFFF"
+          stroke-width="2"
+        />
+      </svg>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p
+        class="section-title desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
+      >
+        Nasze podejście
+      </p>
+      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group uslugi-title">
+        <!-- wp:paragraph -->
+        <p
+          class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"
+        >
+          Dlaczego warto z nami <br />
+          <span
+            class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
+          >
+          Współpracować?</span
+          >
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
