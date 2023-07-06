152e10b005fb3e60a7372d8a4e01456dc0dc136a
Tomek <TK-54-4B>
2023-07-06 13:15:19 +0200

    wp:tags fix

--- patterns/page-about-companies-logos.php
+++ patterns/page-about-companies-logos.php
@@ -114,14 +114,14 @@
   <!-- /wp:group -->
 
       <!-- wp:group -->
-      <div class="sport-logos relative overflow-hidden mt-[100px] w-full h-[120px]">
+      <div class="wp-block-group sport-logos relative overflow-hidden mt-[100px] w-full h-[120px]">
       <!-- wp:group -->
-      <div class="h-[120px] w-full px-[50px] bg-white flex items-center absolute bottom-0 desktop:left-[11%] left-[20px] is-layout-flow">
+      <div class="wp-block-group h-[120px] w-full px-[50px] bg-white flex items-center absolute bottom-0 desktop:left-[11%] left-[20px] is-layout-flow">
         <!-- wp:paragraph -->
         <p class="text-[22px] font-medium">Dumnie wspieramy lokalny sport</p>
         <!-- /wp:paragraph -->
         <!-- wp:group -->
-        <div class="logos-wrapper flex items-center gap-[100px]">
+        <div class="wp-block-group logos-wrapper flex items-center gap-[100px]">
           <!-- wp:image -->
           <figure class="wp-block-image">
             <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logos-companies/logo-leniwce.png" alt="leniwce-logo" class="max-h-[80px]">
