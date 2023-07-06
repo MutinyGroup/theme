ad04986d98c24a09ce32400980cd833bbb6924dd
Tomek <TK-54-4B>
2023-07-06 15:26:19 +0200

    page services banner

---
+++ patterns/page-services-banner.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Title: banner
+ * Slug: page-about-banner-img-full-width
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group -->
+<div class="wp-block-group about-banner desktop:h-[940px] h-screen mb-[150px]">
+  <!-- wp:group -->
+  <div
+    class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]"
+  >
+    <!-- wp:group -->
+    <div
+      class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] pt-[90px]"
+    >
+      <!-- wp:group -->
+      <div
+        class="wp-block-group title-wrapper relative container mx-auto flex desktop:gap-[20px] gap-[0px]"
+      >
+        <!-- wp:paragraph -->
+        <p
+          class="section-title desktop:basis-auto basis-[20%] uppercase text-primaryYellow"
+        >
+          Usługi
+        </p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group uslugi-title">
+          <!-- wp:paragraph -->
+          <p
+            class="uppercase desktop:text-[60px] text-[24px] font-normal desktop:leading-[60px] leading-[32px]"
+          >
+            Jak możemy <br />
+            Ci
+            <span
+              class="relative desktop:inline-block block desktop:text-[60px] text-[24px] uppercase italic before:content-[''] before:inline-block before:mr-[6px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
+              >pomóc?</span
+            >
+          </p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+  <!-- wp:group -->
+  <div class="wp-block-group about-banner-img relative desktop:ml-[80px]">
+    <!-- wp:image -->
+    <figure class="wp-block-image object-cover h-full">
+      <img
+        src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-services/services-banner-bg.jpg"
+        alt="bg-image"
+      />
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:buttons -->
+    <div
+      class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[300px] desktop:left-[1080px] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+    >
+      <!-- wp:paragraph -->
+      <p>Zobacz</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="uppercase text-[25px] font-extrabold">WIĘCEJ</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:buttons -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
