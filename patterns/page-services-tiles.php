d5632921500cf1761df431863007d8cddd23f31c
Tomek <TK-54-4B>
2023-07-06 15:18:06 +0200

    page services clip path at before

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -69,7 +69,7 @@
         </p>
       </div>
 
-      <div class="gallery-cols relative flex gap-[8px]">
+      <div class="gallery-cols relative flex gap-[8px] pb-[100px]">
         <div class="col1 w-[50%]">
           <!-- wp:group -->
           <div
@@ -211,7 +211,7 @@
           <!-- /wp:group -->
           <!-- wp:group -->
           <div
-            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px]"
+            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px] mb-[10px]"
           >
             <!-- wp:group -->
             <div
@@ -348,7 +348,7 @@
           </div>
           <!-- /wp:group -->
           <div
-            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px]"
+            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px] mb-[10px]"
           >
             <!-- wp:group -->
             <div
@@ -627,7 +627,7 @@
           <!-- /wp:group -->
           <!-- wp:group -->
           <div
-            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px]"
+            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px] mb-[10px]"
           >
             <!-- wp:group -->
             <div
@@ -765,7 +765,7 @@
           <!-- /wp:group -->
           <!-- /wp:group -->
           <div
-            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px]"
+            class="wp-block-group group relative gallery-tile z-[2] group p-[25px] h-[600px] rounded-[20px] transition ease-out overflow-hidden duration-300 cursor-pointer mr-[8px] mb-[10px]"
           >
             <!-- wp:group -->
             <div
