6c720186858dc1b883f2537e97870cfa37acd1dc
Tomek <TK-54-4B>
2023-07-07 09:08:42 +0200

    page-services img fix

--- patterns/page-services-why.php
+++ patterns/page-services-why.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group -->
-<div class="wp-block-group why mt-[150px]">
+<div class="wp-block-group why mt-[150px] overflow-hidden">
   <!-- wp:group -->
   <div
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
@@ -200,6 +200,286 @@
         <!-- /wp:group -->
       </li>
       <!-- /wp:list-item -->
+
+      <!-- wp:list-item -->
+      <li>
+        <!-- wp:group -->
+        <div class="wp-block-group cols-wrapper flex">
+          <!-- wp:group -->
+          <div class="wp-block-group col1 relative w-[50%]">
+            <!-- wp:paragraph -->
+            <p class="text-[50px] text-primaryYellow">
+              01 /
+              <span class="text-[20px] text-black font-medium">03</span>
+            </p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph -->
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="236"
+              height="317"
+              viewBox="0 0 236 317"
+              fill="none"
+              class="absolute top-[190px] left-[30px] opacity-[60%]"
+            >
+              <path
+                d="M234.242 314.924L1.62109 236.649V1.91746L234.242 80.1919V314.924Z"
+                stroke="white"
+                stroke-width="2"
+              />
+            </svg>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+          <!-- wp:group -->
+          <div class="wp-block-group col2 w-[50%]">
+            <!-- wp:group -->
+            <div class="wp-block-group mb-[95px]">
+              <!-- wp:group -->
+              <div class="wp-block-group img-clip-path-rounded h-[250px] w-[800px]">
+                <!-- wp:group -->
+                <div
+                  class="wp-block-group img-clip-path-wrapper img-clip-path-why mb-[50px] w-full h-full rounded-[20px] overflow-hidden"
+                >
+                  <!-- wp:image -->
+                  <figure
+                    class="wp-block-image group-hover:grayscale transition ease-in-out duration-[400ms]"
+                  >
+                    <img
+                      src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-services/why-img2.jpg"
+                      alt="services-background-image"
+                    />
+                  </figure>
+                  <!-- /wp:image -->
+                  <!-- wp:paragraph -->
+                  <svg
+                    class="flt_svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <defs>
+                      <filter id="flt_tag">
+                        <feGaussianBlur
+                          in="SourceGraphic"
+                          stdDeviation="8"
+                          result="blur"
+                        />
+                        <feColorMatrix
+                          in="blur"
+                          mode="matrix"
+                          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                          result="flt_tag"
+                        />
+                        <feComposite
+                          in="SourceGraphic"
+                          in2="flt_tag"
+                          operator="atop"
+                        />
+                      </filter>
+                    </defs>
+                  </svg>
+                  <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:group -->
+              </div>
+              <!-- /wp:group -->
+
+              <!-- wp:paragraph -->
+              <p
+                class="my-[50px] text-[35px] leading-[42px] font-semibold uppercase"
+              >
+                Mamy nieszablonowe <br />
+                podejście
+              </p>
+              <!-- /wp:paragraph -->
+              <!-- wp:paragraph -->
+              <p class="mb-[50px] text-[20px] leading-[30px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <!-- /wp:paragraph -->
+              <!-- wp:paragraph -->
+              <p class="mb-[50px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam.
+              </p>
+              <!-- /wp:paragraph -->
+              <!-- wp:list -->
+              <ul class="wp-block-list leading-[32px] font-semibold">
+                <!-- wp:list-item -->
+                <li
+                  class="before:content-[''] before:inline-block desktop:before:mr-[5px] before:mb-[5px] desktop:before:w-[20px] desktop:before:h-[2px] before:bg-primaryYellow"
+                >
+                  Lorem ipsum dolor sit amet
+                </li>
+                <!-- /wp:list-item -->
+                <!-- wp:list-item -->
+                <li
+                  class="before:content-[''] before:inline-block desktop:before:mr-[5px] before:mb-[5px] desktop:before:w-[20px] desktop:before:h-[2px] before:bg-primaryYellow"
+                >
+                  Consectetur adipiscing elit
+                </li>
+                <!-- /wp:list-item -->
+                <!-- wp:list-item -->
+                <li
+                  class="before:content-[''] before:inline-block desktop:before:mr-[5px] before:mb-[5px] desktop:before:w-[20px] desktop:before:h-[2px] before:bg-primaryYellow"
+                >
+                  Sed do eiusmod tempor incididunt
+                </li>
+                <!-- /wp:list-item -->
+              </ul>
+              <!-- /wp:list -->
+            </div>
+            <!-- /wp:group -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+      </li>
+      <!-- /wp:list-item -->
+
+      <!-- wp:list-item -->
+      <li>
+        <!-- wp:group -->
+        <div class="wp-block-group cols-wrapper flex">
+          <!-- wp:group -->
+          <div class="wp-block-group col1 relative w-[50%]">
+            <!-- wp:paragraph -->
+            <p class="text-[50px] text-primaryYellow">
+              01 /
+              <span class="text-[20px] text-black font-medium">03</span>
+            </p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph -->
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="236"
+              height="317"
+              viewBox="0 0 236 317"
+              fill="none"
+              class="absolute top-[190px] left-[30px] opacity-[60%]"
+            >
+              <path
+                d="M234.242 314.924L1.62109 236.649V1.91746L234.242 80.1919V314.924Z"
+                stroke="white"
+                stroke-width="2"
+              />
+            </svg>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+          <!-- wp:group -->
+          <div class="wp-block-group col2 w-[50%]">
+            <!-- wp:group -->
+            <div class="wp-block-group mb-[95px]">
+              <!-- wp:group -->
+              <div class="wp-block-group img-clip-path-rounded h-[250px] w-[800px]">
+                <!-- wp:group -->
+                <div
+                  class="wp-block-group img-clip-path-wrapper img-clip-path-why mb-[50px] w-full h-full rounded-[20px] overflow-hidden"
+                >
+                  <!-- wp:image -->
+                  <figure
+                    class="wp-block-image group-hover:grayscale transition ease-in-out duration-[400ms]"
+                  >
+                    <img
+                      src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-services/why-img3.jpg"
+                      alt="services-background-image"
+                    />
+                  </figure>
+                  <!-- /wp:image -->
+                  <!-- wp:paragraph -->
+                  <svg
+                    class="flt_svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <defs>
+                      <filter id="flt_tag">
+                        <feGaussianBlur
+                          in="SourceGraphic"
+                          stdDeviation="8"
+                          result="blur"
+                        />
+                        <feColorMatrix
+                          in="blur"
+                          mode="matrix"
+                          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                          result="flt_tag"
+                        />
+                        <feComposite
+                          in="SourceGraphic"
+                          in2="flt_tag"
+                          operator="atop"
+                        />
+                      </filter>
+                    </defs>
+                  </svg>
+                  <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:group -->
+              </div>
+              <!-- /wp:group -->
+
+              <!-- wp:paragraph -->
+              <p
+                class="my-[50px] text-[35px] leading-[42px] font-semibold uppercase"
+              >
+                Mamy nieszablonowe <br />
+                podejście
+              </p>
+              <!-- /wp:paragraph -->
+              <!-- wp:paragraph -->
+              <p class="mb-[50px] text-[20px] leading-[30px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <!-- /wp:paragraph -->
+              <!-- wp:paragraph -->
+              <p class="mb-[50px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam.
+              </p>
+              <!-- /wp:paragraph -->
+              <!-- wp:list -->
+              <ul class="wp-block-list leading-[32px] font-semibold">
+                <!-- wp:list-item -->
+                <li
+                  class="before:content-[''] before:inline-block desktop:before:mr-[5px] before:mb-[5px] desktop:before:w-[20px] desktop:before:h-[2px] before:bg-primaryYellow"
+                >
+                  Lorem ipsum dolor sit amet
+                </li>
+                <!-- /wp:list-item -->
+                <!-- wp:list-item -->
+                <li
+                  class="before:content-[''] before:inline-block desktop:before:mr-[5px] before:mb-[5px] desktop:before:w-[20px] desktop:before:h-[2px] before:bg-primaryYellow"
+                >
+                  Consectetur adipiscing elit
+                </li>
+                <!-- /wp:list-item -->
+                <!-- wp:list-item -->
+                <li
+                  class="before:content-[''] before:inline-block desktop:before:mr-[5px] before:mb-[5px] desktop:before:w-[20px] desktop:before:h-[2px] before:bg-primaryYellow"
+                >
+                  Sed do eiusmod tempor incididunt
+                </li>
+                <!-- /wp:list-item -->
+              </ul>
+              <!-- /wp:list -->
+            </div>
+            <!-- /wp:group -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+      </li>
+      <!-- /wp:list-item -->      
 
     </ul>
     <!-- /wp:list -->
