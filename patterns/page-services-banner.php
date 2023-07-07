184f4e927ea8a05c98448bcbb40c050732ae5a46
Tomek <TK-54-4B>
2023-07-07 14:29:40 +0200

    banners title fix

--- patterns/page-services-banner.php
+++ patterns/page-services-banner.php
@@ -27,7 +27,7 @@
         class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
         >
           <!-- wp:paragraph -->
-          <p class="justify-self-end pr-[15px]">Usługi</p>
+          <p class="justify-self-end pt-[3px] pr-[15px]">Usługi</p>
           <!-- /wp:paragraph -->
         </div>  
         <!-- /wp:group -->
