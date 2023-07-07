7220ed7c29b79a787f3c369e489869e31e7b42a4
Tomek <TK-54-4B>
2023-07-07 22:10:37 +0200

    page-aboute general fixing

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -8,47 +8,51 @@
 ?>
 
 <!-- wp:group -->
-<div class="wp-block-group video relative container mx-auto overscroll-x-hidden mb-[150px]">
+<div class="wp-block-group meet-us overflow-hidden mb-[80px] desktop:mb-[150px]">
+<!-- wp:group -->
+  <div class="wp-block-group video relative container mx-auto overflow-x-hidden mb-[150px]">
     <!-- wp:group -->
-    <div class="wp-block-group paragraph-wrapper relative w-full mb-[150px]">
-    <!-- wp:paragraph -->
-    <svg class="shape w-[1400px] z-[-1] absolute top-[-120px] left-[30px]" xmlns="http://www.w3.org/2000/svg" width="1051" height="810" viewBox="0 0 1051 810" fill="none">
-      <path d="M1 1.45249L938.266 329.35L938.596 329.465L938.927 329.35L1406.89 165.634V409.787L938.596 573.618L234.979 327.462L233.649 326.996V328.406V807.61L1 726.219V1.45249Z" stroke="#FFFFFF" stroke-width="2"/>
-    </svg>
-    <!-- /wp:paragraph --> 
-    <!-- wp:paragraph -->
-    <p
-    class="desktop:mb-[150px] mb-[50px] desktop:ml-[100px] desktop:text-[35px] text-[22px] font-medium desktop:leading-[42px] leading-[32px] desktop:w-[897px]"
-    >
-    Naszą misją jest lorem ipsum dolor sit amet, consectetur adipiscing
-    elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-    aliqua.
-    </p>
-    <!-- /wp:paragraph -->
-  </div>
-  <!-- /wp:group -->
-  <!-- wp:group -->
-  <div class="wp-block-group group cursor-pointer img-clip-path-rounded">
-    <!-- wp:group -->
-    <div class="wp-block-group img-clip-path-video overflow-hidden rounded-tr-[22px]">
-      <!-- wp:image -->
-      <figure class="wp-block-image grayscale group-hover:grayscale-0 transition ease-out duration-300">
-        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/about-video.jpg" alt="image">
-      </figure>
-      <!-- /wp:image -->
+    <div class="wp-block-group paragraph-wrapper relative w-full mb-[150px] pt-[100px]">
+      <!-- wp:paragraph -->
+      <svg class="shape w-[1400px] z-[-1] absolute top-[-120px] left-[30px]" xmlns="http://www.w3.org/2000/svg" width="1051" height="810" viewBox="0 0 1051 810" fill="none">
+        <path d="M1 1.45249L938.266 329.35L938.596 329.465L938.927 329.35L1406.89 165.634V409.787L938.596 573.618L234.979 327.462L233.649 326.996V328.406V807.61L1 726.219V1.45249Z" stroke="#FFFFFF" stroke-width="2"/>
+      </svg>
+      <!-- /wp:paragraph --> 
+      <!-- wp:paragraph -->
+      <p
+      class="desktop:mb-[150px] mb-[50px] desktop:ml-[100px] desktop:text-[35px] text-[22px] font-medium desktop:leading-[42px] leading-[32px] desktop:w-[897px]"
+      >
+        Naszą misją jest lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua.
+      </p>
+      <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
-    <!-- wp:paragraph -->
-    <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <filter id="flt_tag">
-          <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
-          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
-          <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-        </filter>
-      </defs>
-    </svg>
-    <!-- /wp:paragraph -->
+    <!-- wp:group -->
+    <div class="wp-block-group group cursor-pointer img-clip-path-rounded">
+      <!-- wp:group -->
+      <div class="wp-block-group img-clip-path-video overflow-hidden rounded-tr-[22px]">
+        <!-- wp:image -->
+        <figure class="wp-block-image grayscale group-hover:grayscale-0 transition ease-out duration-300">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/about-video.jpg" alt="image">
+        </figure>
+        <!-- /wp:image -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <filter id="flt_tag">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+            <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+          </filter>
+        </defs>
+      </svg>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
