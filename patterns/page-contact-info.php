5cf46e1ba49d759916fd9f412da52a8b83fb068c
Tomek <TK-54-4B>
2023-07-07 21:37:43 +0200

    page contact contact info bg shape

--- patterns/page-contact-info.php
+++ patterns/page-contact-info.php
@@ -8,158 +8,162 @@
 ?>
 
 <!-- wp:group -->
-<div class="wp-block-group page-contact-info my-[150px] overflow-y-visible overflow-x-hidden">
+<div class="wp-block-group page-contact-info my-[150px] overflow-y-visible">
   <!-- wp:group -->
-  <div
-    class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
-  >
+  <div class="wp-block-group page-contact-info my-[150px] overflow-x-hidden">
     <!-- wp:group -->
     <div
-      class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[100px] mb-[60px] flex"
+      class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
     >
-      <!-- wp:paragraph -->
-      <svg class="flash absolute top-[-330px] left-[-140px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
-        <g filter="url(#filter0_f_1787_1988)">
-        <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
-        </g>
-        <defs>
-        <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-        <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
-        </filter>
-        <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#FEC400" stop-opacity="0.6"/>
-        <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
-        </linearGradient>
-        </defs>
-      </svg>
-      <!-- /wp:paragraph -->
-      
-      <!-- wp:paragraph -->
-      <svg
-        class="shape opacity-[60%] z-[-1] absolute top-[130px] right-[-110px]"
-        xmlns="http://www.w3.org/2000/svg"
-        width="470"
-        height="396"
-        viewBox="0 0 470 396"
-        fill="none"
-      >
-        <path
-          d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z"
-          stroke="#FFFFFF"
-          stroke-width="2"
-        />
-      </svg>
-      <!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <p
-        class="section-title desktop:basis-[15%] uppercase text-primaryYellow"
-      >
-        Marketing robimy kompleksowo
-      </p>
-      <!-- /wp:paragraph -->
       <!-- wp:group -->
-      <div class="wp-block-group uslugi-title">
+      <div
+        class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[100px] mb-[60px] flex"
+      >
+        <!-- wp:paragraph -->
+        <svg class="flash absolute top-[-330px] left-[-140px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+          <g filter="url(#filter0_f_1787_1988)">
+          <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
+          </g>
+          <defs>
+          <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
+          </filter>
+          <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FEC400" stop-opacity="0.6"/>
+          <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
+          </linearGradient>
+          </defs>
+        </svg>
+        <!-- /wp:paragraph -->
+        
+        <!-- wp:paragraph -->
+        <svg
+          class="shape opacity-[60%] z-[-1] absolute top-[130px] right-[-110px]"
+          xmlns="http://www.w3.org/2000/svg"
+          width="470"
+          height="396"
+          viewBox="0 0 470 396"
+          fill="none"
+        >
+          <path
+            d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z"
+            stroke="#FFFFFF"
+            stroke-width="2"
+          />
+        </svg>
+        <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
         <p
-          class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"
+          class="section-title desktop:basis-[15%] uppercase text-primaryYellow"
         >
-          Porozmawiajmy o <br />
-          Twoim
-          <span
-            class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
+          Marketing robimy kompleksowo
+        </p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group uslugi-title">
+          <!-- wp:paragraph -->
+          <p
+            class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"
           >
-            Biznesie</span
-          >
+            Porozmawiajmy o <br />
+            Twoim
+            <span
+              class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
+            >
+              Biznesie</span
+            >
+          </p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+      
+      <!-- wp:group -->
+      <div class="wp-block-group paragrapth-wrapper mb-[100px] flex justify-end">
+        <!-- wp:paragraph -->
+        <p class="text text-[20px] leading-[30px] max-w-[625px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-    
-    <!-- wp:group -->
-    <div class="wp-block-group paragrapth-wrapper mb-[100px] flex justify-end">
-      <!-- wp:paragraph -->
-      <p class="text text-[20px] leading-[30px] max-w-[625px]">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
-    
-    <!-- wp:group -->
-    <div class="wp-block-group contact-info-cols-wrapper flex gap-[80px]">
-      <!-- wp:group -->
-      <div class="wp-block-group col1 w-[30%] img-clip-path-rounded">
-        <!-- wp:group -->
-        <div
-          class="wp-block-group img-clip-path-wrapper img-clip-path-realizations w-full h-full-[20px] rounded-br-[20px] overflow-hidden"
-        >
-          <!-- wp:image -->
-          <figure
-            class="wp-block-image group-hover:grayscale transition ease-in-out duration-[400ms]"
-          >
-            <img
-              src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-contact/co-founder-img.jpg"
-              alt="services-backgroun-image"
-            />
-          </figure>
-          <!-- /wp:image -->
-          <!-- wp:paragraph -->
-          <svg
-            class="flt_svg"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <filter id="flt_tag">
-                <feGaussianBlur
-                  in="SourceGraphic"
-                  stdDeviation="8"
-                  result="blur"
-                />
-                <feColorMatrix
-                  in="blur"
-                  mode="matrix"
-                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
-                  result="flt_tag"
-                />
-                <feComposite
-                  in="SourceGraphic"
-                  in2="flt_tag"
-                  operator="atop"
-                />
-              </filter>
-            </defs>
-          </svg>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:group -->
       
       <!-- wp:group -->
-      <div class="wp-block-group col2 self-center text-wrapper">
+      <div class="wp-block-group contact-info-cols-wrapper flex gap-[80px]">
         <!-- wp:group -->
-        <div class="wp-block-group mb-[30px] name-wrapper leading-[28px]">
-          <!-- wp:paragraph -->
-          <p class="text-[35px] font-medium">Karol Anusiewicz</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="text-[20px] font-light">Co-founder & Project Manager</p>
-          <!-- /wp:paragraph -->
+        <div class="wp-block-group col1 w-[30%] img-clip-path-rounded">
+          <!-- wp:group -->
+          <div
+            class="wp-block-group img-clip-path-wrapper img-clip-path-realizations w-full h-full-[20px] rounded-br-[20px] overflow-hidden"
+          >
+            <!-- wp:image -->
+            <figure
+              class="wp-block-image group-hover:grayscale transition ease-in-out duration-[400ms]"
+            >
+              <img
+                src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-contact/co-founder-img.jpg"
+                alt="services-backgroun-image"
+              />
+            </figure>
+            <!-- /wp:image -->
+            <!-- wp:paragraph -->
+            <svg
+              class="flt_svg"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
-
+        
         <!-- wp:group -->
-        <div class="wp-block-group mail-phone-wrapper flex flex-col gap-y-[14px] desktop:leading-[38px] leading-[24px]">
-          <!-- wp:paragraph -->
-          <p class="w-fit desktop:text-[25px] text-[18px] leading-[24px] inline font-medium border-b-[1px] border-black hover:border-primaryYellow hover:text-primaryYellow transition ease-out duration-300 cursor-pointer">kontakt@mutiny.pl</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="w-fit desktop:text-[20px] text-[15px] leading-[24px] inline font-medium border-b-[1px] border-black hover:border-primaryYellow hover:text-primaryYellow transition ease-out duration-300 cursor-pointer">+48 795 123 584</p>
-          <!-- /wp:paragraph -->
+        <div class="wp-block-group col2 self-center text-wrapper">
+          <!-- wp:group -->
+          <div class="wp-block-group mb-[30px] name-wrapper leading-[28px]">
+            <!-- wp:paragraph -->
+            <p class="text-[35px] font-medium">Karol Anusiewicz</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="text-[20px] font-light">Co-founder & Project Manager</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group mail-phone-wrapper flex flex-col gap-y-[14px] desktop:leading-[38px] leading-[24px]">
+            <!-- wp:paragraph -->
+            <p class="w-fit desktop:text-[25px] text-[18px] leading-[24px] inline font-medium border-b-[1px] border-black hover:border-primaryYellow hover:text-primaryYellow transition ease-out duration-300 cursor-pointer">kontakt@mutiny.pl</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="w-fit desktop:text-[20px] text-[15px] leading-[24px] inline font-medium border-b-[1px] border-black hover:border-primaryYellow hover:text-primaryYellow transition ease-out duration-300 cursor-pointer">+48 795 123 584</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
       </div>
