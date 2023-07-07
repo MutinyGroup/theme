ad4bc0fe22fc9d3ca164d77cdb9152fb0ab5c0af
Tomek <TK-54-4B>
2023-07-07 18:30:40 +0200

    bright cta fix

--- patterns/bright-cta1.php
+++ patterns/bright-cta1.php
@@ -25,7 +25,7 @@
       <p class="section-title text-[20px] text-primaryYellow">Wymagaj więcej</p>
       <!-- /wp:paragraph -->
       <!-- wp:group -->
-      <div class="wp-block-group uslugi-title font-normal desktop:text-[65px] text-[32px] desktop:leading-[75px] leading-[36px]">
+      <div class="wp-block-group uslugi-title font-normal desktop:text-[65px] text-[32px] desktop:leading-[70px] leading-[36px]">
         <!-- wp:paragraph -->
         <p class="uppercase">
           Poznaj ofertę <br>
