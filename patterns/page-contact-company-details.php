e1637057bca3d665b484257fb7942a9a097a17ce
Tomek <TK-54-4B>
2023-07-07 20:43:06 +0200

    page contact contact info

--- patterns/page-contact-company-details.php
+++ patterns/page-contact-company-details.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title:page-contact-details
- * Slug:page-contact-details
+ * Title: page-contact-details
+ * Slug: page-contact-details
  * Categories: text, banner
  * Viewport Width: 1280
  */
