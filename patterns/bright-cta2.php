23a72eff56e4f54517b0ef083ed4141740f04727
Tomek <TK-54-4B>
2023-07-07 18:18:04 +0200

    bright cta fix

---
+++ patterns/bright-cta2.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Title: bright-cta
+ * Slug: bright-cta
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+<!-- wp:group -->
+<div class="wp-block-group relative cta desktop:mb-[150px] overflow-hidden">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[120px] wide:h-[1180px] desktop:h-[790px] h-[480px]">
+    <!-- wp:group -->
+    <div class="wp-block-group title-wrapper relative desktop:pt-[110px] pt-[0px] desktop:mb-[80px] mb-[60px]">
+      <!-- wp:group -->
+      <div class="wp-block-group bg-flash top-[80px] z-[-1]"></div>
+      <!-- /wp:group -->
+      <!-- wp:image -->
+      <figure class="wp-block-image bg-shape w-[1290px] z-[-1] opacity-[0.15] absolute top-[-150px] left-[-140px]">
+        <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/cta-bg-shape.png" alt="background-shape">
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph -->
+      <p class="section-title text-[20px] text-primaryYellow">Wymagaj więcej</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group uslugi-title font-normal desktop:text-[65px] text-[32px] desktop:leading-[75px] leading-[36px]">
+        <!-- wp:paragraph -->
+        <p class="uppercase">
+          Porozmawiajmy <br> o potrzebach
+        </p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="relative uppercase italic before:content-[''] before:inline-block before:mr-[-5px] before:bottom-0 before:left-0 desktop:before:w-[120px] before:w-[30px] before:h-[2px] before:bg-white"> Twojej marki.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+    <!-- wp:buttons -->
+    <div class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] desktop:ml-[120px] mx-auto text-center rounded-full border border-[4px] border-[#D5D5D5] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
+      <!-- wp:paragraph -->
+      <p>Skontaktuj się</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="uppercase text-[25px] font-extrabold">TERAZ</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:buttons -->
+  </div>
+  <!-- /wp:group -->
+  
+  <!-- wp:group -->
+  <div class="wp-block-group img-clip-path-wrapper w-[58%] h-full rounded-tl-[20px] overflow-hidden absolute right-0 desktop:top-0 top-[30px] z-[-1]">
+    <!-- wp:image -->
+    <figure class="wp-block-image img-clip-path-cta">
+      <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/cta-bright-bg2.jpg" alt="cta-backgroun-image">
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:paragraph -->
+    <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <filter id="flt_tag">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+          <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+        </filter>
+      </defs>
+    </svg>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
