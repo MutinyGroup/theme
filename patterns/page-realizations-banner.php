31e6a7ec350e739ac7791dbe4d5881c44a6a012d
Tomek <TK-54-4B>
2023-07-07 14:31:25 +0200

    banners title fix

--- patterns/page-realizations-banner.php
+++ patterns/page-realizations-banner.php
@@ -60,7 +60,7 @@
     <!-- /wp:group -->
     
     <!-- wp:group -->
-    <div class="wp-block-group paragrapth-wrapper ml-[180px]">
+    <div class="wp-block-group paragrapth-wrapper ml-[230px]">
       <!-- wp:paragraph -->
       <p class="text text-[20px] leading-[30px] max-w-[445px]">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
