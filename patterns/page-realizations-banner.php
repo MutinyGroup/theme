aa127efe6bc440ac9984bacf8b6931d117e61ead
Tomek <TK-54-4B>
2023-07-07 10:58:31 +0200

    page-realizations

--- patterns/page-realizations-banner.php
+++ patterns/page-realizations-banner.php
@@ -46,12 +46,12 @@
         <p
           class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"
         >
-          Wybierz usługę którą <br />
-          jesteś
+          Projekty z którch <br />
+          jesteśmy
           <span
             class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
           >
-            zainteresowany</span
+            dumni</span
           >
         </p>
         <!-- /wp:paragraph -->
