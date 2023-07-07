2d74e9fc0ced84558fe7ddc56602954a233a5a57
Tomek <TK-54-4B>
2023-07-07 21:57:25 +0200

    page contact contact info bg shape

--- patterns/page-about-meet-us.php
+++ patterns/page-about-meet-us.php
@@ -14,6 +14,24 @@
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[120px] mb-[60px] flex">
       <!-- wp:paragraph -->
+      <svg class="flash absolute top-[-330px] left-[-140px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+        <g filter="url(#filter0_f_1787_1988)">
+        <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
+        </g>
+        <defs>
+        <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
+        </filter>
+        <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FEC400" stop-opacity="0.6"/>
+        <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
+        </linearGradient>
+        </defs>
+      </svg>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
       <svg class="z-[-1] absolute top-0 right-[-200px]" xmlns="http://www.w3.org/2000/svg" width="470" height="396" viewBox="0 0 470 396" fill="none">
         <path d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z" stroke="#FFFFFF" stroke-width="2"/>
       </svg>
