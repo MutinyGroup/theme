08daf8588bd3f16c581ee619636513c281fb04f3
Tomek <TK-54-4B>
2023-07-07 22:28:49 +0200

    page-aboute general fixing

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -33,13 +33,13 @@
     <div class="wp-block-group group cursor-pointer img-clip-path-rounded">
       <!-- wp:buttons -->
       <div
-        class="wp-block-buttons z-[1] desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-white text-center rounded-full border desktop:border-[4px] border-[3px] border-[#D5D5D5] translate-y-[-50%] absolute desktop:top-[50%] top-[300px] desktop:left-[50%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+        class="wp-block-buttons z-[1] desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-white text-center uppercase rounded-full border desktop:border-[4px] border-[3px] border-[#D5D5D5] translate-y-[-50%] absolute desktop:top-[50%] top-[300px] desktop:left-[50%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
       >
         <!-- wp:paragraph -->
-        <p class="uppercase">play</p>
+        <p class="text-[35px] font-extrabold">play</p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="uppercase text-[25px] font-extrabold">video</p>
+        <p class="">video</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:buttons -->
