5b7be13aed2f31f80fb20aa9b19cc86239b28d4d
Tomek <TK-54-4B>
2023-07-07 13:48:23 +0200

    page-realizations banner title

--- patterns/page-about-cta.php
+++ patterns/page-about-cta.php
@@ -9,7 +9,7 @@
 <!-- wp:group -->
 <div class="wp-block-group relative cta desktop:mb-[150px]">
   <!-- wp:group -->
-  <div class="wp-block-group group container mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[120px] wide:h-[1180px] desktop:h-[790px] h-[480px]">
+  <div class="wp-block-group group container mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[120px] wide:h-[1180px] desktop:h-[700px] h-[480px]">
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper relative desktop:pt-[110px] pt-[0px] desktop:mb-[80px] mb-[60px]">
       <!-- wp:group -->
