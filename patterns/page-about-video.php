7504bb2f67d63523cd21ab3c0e8bca861add82a8
Tomek <TK-54-4B>
2023-07-07 22:17:03 +0200

    page-aboute general fixing

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -31,6 +31,18 @@
     <!-- /wp:group -->
     <!-- wp:group -->
     <div class="wp-block-group group cursor-pointer img-clip-path-rounded">
+      <!-- wp:buttons -->
+      <div
+        class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[50%] top-[300px] desktop:left-[50%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+      >
+        <!-- wp:paragraph -->
+        <p>play</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="uppercase text-[25px] font-extrabold">video</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:buttons -->
       <!-- wp:group -->
       <div class="wp-block-group img-clip-path-video overflow-hidden rounded-tr-[22px]">
         <!-- wp:image -->
