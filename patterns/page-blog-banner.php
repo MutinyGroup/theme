3e9d8fd5756415e6e74121ecf198856bce2f8886
Tomek <TK-54-4B>
2023-07-07 14:36:17 +0200

    blog

---
+++ patterns/page-blog-banner.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Title: page-blog-banner
+ * Slug: page-blog-banner
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group -->
+<div class="wp-block-group page-realizations-banner overflow-hidden">
+  <!-- wp:group -->
+  <div
+    class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
+  >
+    <!-- wp:group -->
+    <div
+      class="wp-block-group title-wrapper relative container mx-auto pt-[90px] desktop:mb-[80px] mb-[60px] flex"
+    >
+      <!-- wp:buttons -->
+      <div
+        class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[130px] top-[0px] right-[30px] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+      >
+        <!-- wp:paragraph -->
+        <p>Zobacz</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="uppercase text-[25px] font-extrabold">WIĘCEJ</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:buttons -->
+
+        <!-- wp:group -->
+        <div
+        class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
+        >
+          <!-- wp:paragraph -->
+          <p class="justify-self-end pt-[3px] pr-[15px]">Realizacje</p>
+          <!-- /wp:paragraph -->
+        </div>  
+        <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[24px] desktop:leading-[60px] leading-[30px]">
+        <!-- wp:paragraph -->
+        <p
+          class=""
+        >
+          Projekty z którch <br />
+          jesteśmy
+          <span
+            class="relative italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[3px] before:bg-black"
+          >
+            dumni.</span
+          >
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+    
+    <!-- wp:group -->
+    <div class="wp-block-group paragrapth-wrapper ml-[230px]">
+      <!-- wp:paragraph -->
+      <p class="text text-[20px] leading-[30px] max-w-[445px]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
+      </p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
