7672d754012bee8b6e2d0e1580f5b77e5e34ead8
Tomek <TK-54-4B>
2023-07-08 12:17:09 +0200

    page-about wptags

--- patterns/page-about-companies-logos.php
+++ patterns/page-about-companies-logos.php
@@ -107,44 +107,39 @@
       <!-- /wp:core/image -->
     </div>
     <!-- /wp:group -->
-
-
-
   </div>
   <!-- /wp:group -->
 
+  <!-- wp:group -->
+  <div class="wp-block-group sport-logos relative overflow-hidden mt-[100px] w-full h-[120px]">
+    <!-- wp:group -->
+    <div class="wp-block-group h-[120px] w-full px-[50px] bg-white flex items-center absolute bottom-0 desktop:left-[11%] left-[20px] is-layout-flow">
+      <!-- wp:paragraph -->
+      <p class="text-[22px] font-medium">Dumnie wspieramy lokalny sport</p>
+      <!-- /wp:paragraph -->
       <!-- wp:group -->
-      <div class="wp-block-group sport-logos relative overflow-hidden mt-[100px] w-full h-[120px]">
-      <!-- wp:group -->
-      <div class="wp-block-group h-[120px] w-full px-[50px] bg-white flex items-center absolute bottom-0 desktop:left-[11%] left-[20px] is-layout-flow">
-        <!-- wp:paragraph -->
-        <p class="text-[22px] font-medium">Dumnie wspieramy lokalny sport</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:group -->
-        <div class="wp-block-group logos-sport-wrapper flex items-center gap-[100px]">
-          <!-- wp:image -->
-          <figure class="wp-block-image">
-            <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logos-companies/logo-leniwce.png" alt="leniwce-logo">
-          </figure>
-          <!-- /wp:image -->
-          <!-- wp:image -->
-          <figure class="wp-block-image">
-            <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logos-companies/logo-sport.png" alt="company-logo">
-          </figure>
-          <!-- /wp:image -->
-          <!-- wp:image -->
-          <figure class="wp-block-image">
-            <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logos-companies/logo-leniwce.png" alt="leniwce-logo">
-          </figure>
-          <!-- /wp:image -->
-        </div>
-        <!-- /wp:group -->
+      <div class="wp-block-group logos-sport-wrapper flex items-center gap-[100px]">
+        <!-- wp:image -->
+        <figure class="wp-block-image">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logos-companies/logo-leniwce.png" alt="leniwce-logo">
+        </figure>
+        <!-- /wp:image -->
+        <!-- wp:image -->
+        <figure class="wp-block-image">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logos-companies/logo-sport.png" alt="company-logo">
+        </figure>
+        <!-- /wp:image -->
+        <!-- wp:image -->
+        <figure class="wp-block-image">
+          <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/logos-companies/logo-leniwce.png" alt="leniwce-logo">
+        </figure>
+        <!-- /wp:image -->
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-
   </div>
   <!-- /wp:group -->
+
 </div>
 <!-- /wp:group -->
