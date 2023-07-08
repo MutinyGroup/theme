90a51c8f1663cccedc198f36adfa61e2f8dfd4e9
Tomek <TK-54-4B>
2023-07-08 12:31:08 +0200

    wp:tags

--- patterns/page-contact-company-details.php
+++ patterns/page-contact-company-details.php
@@ -33,48 +33,73 @@
   <div
   class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
   >
-  <div class="cols-wrapper flex justify-between">
-    <div class="col1 relative address text-[20px] leading-[29px]">
-      <svg class="shape opacity-[60%] absolute top-[-110px] left-[-60px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="236" height="317" viewBox="0 0 236 317" fill="none">
-        <path d="M1.62088 314.924L234.242 236.649V1.91746L1.62088 80.1919V314.924Z" stroke="white" stroke-width="2"/>
-      </svg>
-      <svg class="flash absolute top-[-300px] left-[-210px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
-        <g filter="url(#filter0_f_1787_1988)">
-        <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
-        </g>
-        <defs>
-        <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-        <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
-        </filter>
-        <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#FEC400" stop-opacity="0.6"/>
-        <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
-        </linearGradient>
-        </defs>
-      </svg>
-      <p class="font-semibold">Mutiny Group sp. z o.o.</p>
-      <p class="font-light">ul. Jaśkowa Dolina 11A/1A,</p>
-      <p class="font-light">80-252 Gdańsk | Polska</p>
-      <p class="font-light">kontakt@mutiny.pl</p>
+    <!-- wp:group -->
+    <div class="cols-wrapper flex justify-between">
+      <!-- wp:group -->
+      <div class="col1 relative address text-[20px] leading-[29px]">
+        <!-- wp:paragraph -->
+        <svg class="shape opacity-[60%] absolute top-[-110px] left-[-60px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="236" height="317" viewBox="0 0 236 317" fill="none">
+          <path d="M1.62088 314.924L234.242 236.649V1.91746L1.62088 80.1919V314.924Z" stroke="white" stroke-width="2"/>
+        </svg>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <svg class="flash absolute top-[-300px] left-[-210px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+          <g filter="url(#filter0_f_1787_1988)">
+          <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
+          </g>
+          <defs>
+          <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
+          </filter>
+          <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FEC400" stop-opacity="0.6"/>
+          <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
+          </linearGradient>
+          </defs>
+        </svg>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="font-semibold">Mutiny Group sp. z o.o.</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="font-light">ul. Jaśkowa Dolina 11A/1A,</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="font-light">80-252 Gdańsk | Polska</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="font-light">kontakt@mutiny.pl</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <div class="col2 numbers font-light leading-[21px]">
+        <!-- wp:list -->
+        <ul>
+          <!-- wp:list-item -->
+          <li><span class="font-medium">NIP:</span> 9571133012</li>
+          <!-- /wp:list-item -->
+          <!-- wp:list-item -->
+          <li><span class="font-medium">REGON:</span> 388790205</li>
+          <!-- /wp:list-item -->
+          <!-- wp:list-item -->
+          <li><span class="font-medium">KRS:</span> 0000898283</li>
+          <!-- /wp:list-item -->
+        </ul>
+        <!-- /wp:list -->
+      </div>
+      <!-- /wp:group -->
+      
+      <!-- wp:group -->
+      <div class="col3 details text-[14px] font-light leading-[19px] max-w-[444px]">
+        <!-- wp:paragraph -->
+        <p>Spółka Mutiny Group sp. z o.o. z siedzibą w Gdańsku przy ul. Jaśkowa Dolina 11A/1A, kod pocztowy: 80-252 jest wpisana do rejestru przedsiębiorców, prowadzonego przez Sąd Rejonowy Gdańsk - Wydział Gospodarczy Krajowego Rejestru Sądowego pod nr KRS 0000898283, NIP: 9571133012, REGON: 388790205. Kapitał zakładowy: 5.000 PLN (opłacony w całości).</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
-    <div class="col2 numbers font-light leading-[21px]">
-      <ul>
-        <li><span class="font-medium">NIP:</span> 9571133012</li>
-        <li><span class="font-medium">REGON:</span> 388790205</li>
-        <li><span class="font-medium">KRS:</span> 0000898283</li>
-      </ul>
-    </div>
-    <div class="col3 details text-[14px] font-light leading-[19px] max-w-[444px]">
-
-
-
-
-
-      <p>Spółka Mutiny Group sp. z o.o. z siedzibą w Gdańsku przy ul. Jaśkowa Dolina 11A/1A, kod pocztowy: 80-252 jest wpisana do rejestru przedsiębiorców, prowadzonego przez Sąd Rejonowy Gdańsk - Wydział Gospodarczy Krajowego Rejestru Sądowego pod nr KRS 0000898283, NIP: 9571133012, REGON: 388790205. Kapitał zakładowy: 5.000 PLN (opłacony w całości).</p>
-    </div>
-  </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
