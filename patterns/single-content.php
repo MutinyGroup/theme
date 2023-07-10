72a56c8eed007b2c3379cb61141aa05e3ab99607
Tomek <TK-54-4B>
2023-07-10 09:13:14 +0200

    blog single post page

--- patterns/single-content.php
+++ patterns/single-content.php
@@ -18,22 +18,20 @@
         the_post(); 
         $avatarUrl = get_avatar_url(get_the_author_id());
     ?>
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h1 class="my-[40px] text-[36px] leading-[50px]"><?php the_title(); ?></h1>
-        <div class="post-author-wrapper mb-[20px] flex gap-[10px] items-center [&>img]:rounded-full [&>img]:w-[55px] [&>img]:h-[55px]">
-          <img src="<?php echo $avatarUrl; ?>" alt="avatar-image">
-          <span class="mb-0">Autor: <?php the_author(); ?>,</span>
-          <span class="mb-0 text-neutral-500"><?php the_time('j F Y'); ?></span>
-        </div>
-        <?php the_content(); ?>
-      </article>
-      <?php
-        if ( comments_open() || get_comments_number() ) :
-          comments_template();
-        endif;
+    <!-- wp:group -->
+    <div class="content">
+      <!-- wp:post-content /-->
+      <?php the_content(); ?>
+    </div>
+    <!-- /wp:group -->
+      
+    <?php
       endwhile;
     ?>
   </div>
+  <!-- /wp:group -->
 </div>
+<!-- /wp:group -->
+
 
 
