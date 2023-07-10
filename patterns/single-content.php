86957b2b224f5dc91ec9c44c90e230ece5b5606f
Tomek <TK-54-4B>
2023-07-10 12:44:17 +0200

    single post wp:tags

--- patterns/single-content.php
+++ patterns/single-content.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group -->
-<div class="wp-block-group page-about-services mb-[150px]">
+<div class="wp-block-group single-page-post mb-[150px]">
   <!-- wp:group -->
   <div
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
