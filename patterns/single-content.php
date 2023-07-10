2cfdda05675e8ee2f9782928af7883379f4b5ca3
Tomek <TK-54-4B>
2023-07-10 16:34:17 +0200

    blog single post page

--- patterns/single-content.php
+++ patterns/single-content.php
@@ -175,11 +175,13 @@
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-    
-    <!-- wp:paragraph -->
-    <a href="https://mutiny.webo.design/blog/" class="group mt-[60px] mb-[40px] w-fit font-semibold flex items-center gap-[10px]">
-      <!-- wp:group -->
-      <div class="wp-block-group group w-[56px] h-[56px]">
+
+    <!-- wp:group -->
+    <div class="border-wrapper pt-[30px] border-[#D5D5D5] border-t-[2px]">
+      <!-- wp:paragraph -->
+      <a href="https://mutiny.webo.design/blog/" class="group mt-[60px] mb-[40px] w-fit font-semibold flex items-center gap-[10px]">
+        <!-- wp:group -->
+        <div class="wp-block-group group w-[56px] h-[56px]">
         <!-- wp:paragraph -->
         <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
           <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:fill-[#FEC400] group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
@@ -187,13 +189,15 @@
           <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
         </svg>
         <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="uppercase">WRÓĆ DO LISTY</p>
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:paragraph -->
+        <p class="uppercase">WRÓĆ DO LISTY</p>
+        <!-- /wp:paragraph -->
+      </a>
       <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
