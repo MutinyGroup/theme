38f0c764864641d9ee4cb63ae420c3052c5e3572
Tomek <TK-54-4B>
2023-07-10 17:22:32 +0200

    blog single post pageblog single post page

--- patterns/page-blog-more-swiper.php
+++ patterns/page-blog-more-swiper.php
@@ -344,10 +344,10 @@
           <!-- wp:group -->
           <div id="blog-prev" class="wp-block-group group swiper-button-prev desktop:inline-block hidden w-[56px] h-[56px] absolute desktop:top-[90px] top-[35px] desktop:left-[800px] left-[190px]">
             <!-- wp:paragraph -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
-              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
-              <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-              <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-primaryYellow">
+              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAA" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
+              <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+              <path d="M20 28L31 28" stroke="black" stroke-width="1.5"/>
             </svg>
             <!-- /wp:paragraph -->
           </div>
