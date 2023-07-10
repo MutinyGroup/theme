f233467b5dfcc341cf8700694f935c29d9cd3d14
Tomek <TK-54-4B>
2023-07-10 14:00:59 +0200

    blog single post page

--- patterns/single-content.php
+++ patterns/single-content.php
@@ -40,6 +40,20 @@
     </div>
     <!-- /wp:group -->
 
+    <!-- wp:paragraph -->
+    <a href="https://mutiny.webo.design/blog/" class="mb-[60px] font-semibold flex gap-[10px]">
+    <!-- wp:paragraph -->
+      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none"          class="rotate-180 group fill-transparent group-hover:fill-[#FEC400]">
+        <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+        <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+        <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+      </svg>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="uppercase">WRÓĆ DO LISTY</p>
+      <!-- /wp:paragraph -->
+    </a>
+    <!-- /wp:paragraph -->
 
 
     <!-- wp:group -->
