69b6d06dba89331f27e9ceb22fb89a01ca5fbe70
Tomek <TK-54-4B>
2023-07-10 14:23:44 +0200

    blog single post page

--- patterns/single-content.php
+++ patterns/single-content.php
@@ -121,18 +121,20 @@
         <ul class="wp-block-list flex gap-[20px]">
           <!-- wp:list-item -->
           <li class="cursor-pointer group w-[56px] h-[56px]">
+            <!-- wp:paragraph -->
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" class="group">
               <path d="M24.0088 21.125L24.5087 17.8674H21.383V15.7535C21.383 14.8623 21.8196 13.9936 23.2195 13.9936H24.6406V11.2201C24.6406 11.2201 23.351 11 22.1181 11C19.5439 11 17.8614 12.5602 17.8614 15.3847V17.8674H15V21.125H17.8614V29H21.383V21.125H24.0088Z" fill="black" class="group-hover:fill-primaryYellow transition-stroke ease-out duration-500"/>
               <rect x="1" y="1" width="38" height="38" rx="19" stroke="#D5D5D5" stroke-width="2" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
             </svg>
+            <!-- /wp:paragraph -->
           </li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
           <li class="cursor-pointer">
             <!-- wp:paragraph -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" class="group scale-[0.55]">
-              <path d="M21.3484 39H24.8323V26.5125H21.3484V39ZM23.0903 21C21.929 21 21 21.9 21 23.025C21 24.15 21.929 25.05 23.0903 25.05C24.2516 25.05 25.1806 24.15 25.1806 23.025C25.1806 21.9 24.2516 21 23.0903 21ZM30.7548 28.425V26.5125H27.271V39H30.7548V32.5875C30.7548 28.9875 35.5161 28.7625 35.5161 32.5875V39H39V31.35C39 25.275 32.3806 25.5 30.7548 28.425Z" fill="black" class="group-hover:fill-primaryYellow transition-stroke ease-out duration-500"/>
-              <rect x="1" y="1" width="58" height="58" rx="29" stroke="#D5D5D5" stroke-width="2" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+              <path d="M11.3484 29H14.8323V16.5125H11.3484V29ZM13.0903 11C11.929 11 11 11.9 11 13.025C11 14.15 11.929 15.05 13.0903 15.05C14.2516 15.05 15.1806 14.15 15.1806 13.025C15.1806 11.9 14.2516 11 13.0903 11ZM20.7548 18.425V16.5125H17.271V29H20.7548V22.5875C20.7548 18.9875 25.5161 18.7625 25.5161 22.5875V29H29V21.35C29 15.275 22.3806 15.5 20.7548 18.425Z" fill="black"/>
+              <rect x="1" y="1" width="38" height="38" rx="19" stroke="#D5D5D5" stroke-width="2"/>
             </svg>
             <!-- /wp:paragraph -->
           </li>
