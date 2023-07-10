3b8a30393d209d1aeb06ce31b0bb08872f0a9c26
Tomek <TK-54-4B>
2023-07-10 11:51:20 +0200

    blog single post page

--- patterns/single-content.php
+++ patterns/single-content.php
@@ -36,20 +36,21 @@
     ?>
   </div>
 
+  <!-- wp:link -->
+  <a href="https://mutiny.webo.design/blog/" class="wp-block-link mb-[60px] flex gap-[10px]">
+  <!-- wp:paragraph -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none"          class="rotate-180 group fill-transparent group-hover:fill-[#FEC400]">
+      <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+      <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+      <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+    </svg>
+    <!-- /wp:paragraph -->
+    WRÓĆ DO LISTY
+  </a>
+  <!-- /wp:link -->
+
   <!-- wp:group -->
   <div class="wp-block-group above-img-banner mb-[50px] flex gap-[50px]">
-    <!-- wp:link -->
-    <a href="https://mutiny.webo.design/blog/" class="wp-block-link mb-[60px] flex gap-[10px]">
-      <!-- wp:paragraph -->
-      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none"          class="rotate-180 group fill-transparent group-hover:fill-[#FEC400]">
-        <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
-        <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-        <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-      </svg>
-      <!-- /wp:paragraph -->
-      WRÓĆ DO LISTY
-    </a>
-    <!-- /wp:link -->
     <!-- wp:group -->
     <div class="wp-block-group post-title w-[70%] text-[50px] leading-[55px] font-normal">
       <!-- wp:post-title /-->
