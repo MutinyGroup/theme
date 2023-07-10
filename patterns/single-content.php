7d1ea52bf69623c6cbb253e401abcef0c7aec5b9
Tomek <TK-54-4B>
2023-07-10 10:17:26 +0200

    blog single post page

--- patterns/single-content.php
+++ patterns/single-content.php
@@ -13,15 +13,28 @@
   <div
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
   >    
-  <?php print_r(get_post_thumbnail_id()); ?>
-  <div class="above-img-banner flex">
-    <div class="post-title w-[70%] text-[50px] leading-[55px] font-medium">
+
+  <?php 
+    if ( have_posts() ) {
+      while ( have_posts() ) {
+        the_post(); 
+        print_r(get_post_thumbnail_id()); 
+        the_time('j F Y'); 
+        //
+        // Post Content here
+        //
+      } // end while
+    } // end if
+    ?>
+
+  <div class="above-img-banner mb-[50px] flex">
+    <div class="post-title w-[70%] text-[50px] leading-[55px] font-normal">
       <!-- wp:post-title /-->
     </div>
 
 
     <div class="post-author-wrapper w-[30%]">
-      <div class="post-date">
+      <div class="post-date before:content-[''] before:inline-block desktop:before:mr-[5px] before:mb-[5px] desktop:before:w-[20px] desktop:before:h-[2px] before:bg-primaryYellow">
         <!-- wp:post-date /-->
       </div>
       <div class="author-name">
