be94bf6de552e9f09249e1c5936c26f0163e17e1
Tomek <TK-54-4B>
2023-07-11 12:28:20 +0200

    page-about mobile fixes

--- patterns/page-about-meet-us.php
+++ patterns/page-about-meet-us.php
@@ -192,19 +192,20 @@
             <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/about-gallery1.jpg" alt="image">
           </figure>
           <!-- /wp:image -->
+          <!-- wp:paragraph -->
+          <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <filter id="flt_tag">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+                <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+              </filter>
+            </defs>
+          </svg>
+          <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
-        <!-- wp:paragraph -->
-        <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <filter id="flt_tag">
-              <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
-              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
-              <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-            </filter>
-          </defs>
-        </svg>
-        <!-- /wp:paragraph -->
+
       </div>
       <!-- /wp:group -->
     </div>
