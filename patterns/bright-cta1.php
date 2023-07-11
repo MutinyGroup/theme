8ca5787da55f55beeb63884449354ce35a39a961
Tomek <TK-54-4B>
2023-07-11 11:23:00 +0200

    cta mobile

--- patterns/bright-cta1.php
+++ patterns/bright-cta1.php
@@ -39,7 +39,7 @@
     </div>
     <!-- /wp:group -->
     <!-- wp:buttons -->
-    <div class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] desktop:ml-[120px] mx-auto text-center rounded-full border border-[4px] border-[#AAA] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
+    <div class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] desktop:ml-[120px] mx-auto text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAA] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
       <!-- wp:paragraph -->
       <p>Poznaj</p>
       <!-- /wp:paragraph -->
