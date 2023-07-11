08b7d3f58a1ea9f5e7eb613b827ada54c50a32c4
Tomek <TK-54-4B>
2023-07-11 16:18:15 +0200

    cta test

--- patterns/bright-cta1.php
+++ patterns/bright-cta1.php
@@ -16,7 +16,7 @@
       <div class="wp-block-group bg-flash top-[80px] z-[-1]"></div>
       <!-- /wp:group -->
       <!-- wp:paragraph -->
-      <svg xmlns="http://www.w3.org/2000/svg" width="1201" height="917" viewBox="0 0 1201 917" fill="none" class="desktop:scale-[1] scale-[0.3] absolute top-[-430px] left-[-4300px] z-[-1]">
+      <svg xmlns="http://www.w3.org/2000/svg" width="1201" height="917" viewBox="0 0 1201 917" fill="none" class="absolute top-[-190px] left-[-260px] z-[-1]">
         <path d="M-91 2.02805L553.6 283.595L554 283.77L554.4 283.595L751.232 197.617L876 191.674V352.568L554 493.221L69.9003 281.762L68.5 281.15V282.678V711.805L-91 642.134V2.02805Z" stroke="white" stroke-width="2"></path>
         <path d="M1040.07 494.916L1040.67 494.654V494L1040.67 262.321L1200 192.695L1200 634.346L1039.27 704.584L555.667 915.909L234 775.346V566.028L555.266 706.416L555.667 706.591L556.067 706.416L1040.07 494.916Z" stroke="white" stroke-width="2"></path>
       </svg>
