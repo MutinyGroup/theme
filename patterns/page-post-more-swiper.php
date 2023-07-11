a434a9cef8edd78e72df7627837204288fb1e9aa
Tomek <TK-54-4B>
2023-07-11 10:55:42 +0200

    single post page mobile

--- patterns/page-post-more-swiper.php
+++ patterns/page-post-more-swiper.php
@@ -54,7 +54,7 @@
                   $counter++;
                   if ($counter == 1){ ?>
                     <!-- wp:group -->
-                    <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:w-[444px] w-[300px] desktop:h-[675px] h-[400px]">
+                    <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:w-[444px] w-[300px] desktop:h-[675px] h-[500px]">
                       <!-- wp:paragraph -->
                       <a href="<?php the_permalink(); ?>">                      
                         <!-- wp:group -->
@@ -98,13 +98,13 @@
                   <?php 
                   }else{ ?>                    
                     <!-- wp:group -->
-                    <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:w-[444px] w-[280px] desktop:h-[675px] h-[380px]">
+                    <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:w-[444px] w-[280px] desktop:h-[675px] h-[500px]">
                       <!-- wp:paragraph -->
                       <a href="<?php the_permalink(); ?>">                        
                         <!-- wp:group -->
                         <div class="wp-block-group img-wrapper mb-[20px]">
                           <!-- wp:group -->
-                          <div class="wp-block-group overflow-hidden h-[320px] desktop:h-[460px] rounded-[20px]">
+                          <div class="wp-block-group overflow-hidden h-[300px] desktop:h-[460px] rounded-[20px]">
                             <!-- wp:image -->
                             <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
                               <img src="<?php echo $postImageUrl[0]; ?>" alt="post-image">
