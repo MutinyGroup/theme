fe040d4d6d36ed2832e6ed7720d593280cc85263
Tomek <TK-54-4B>
2023-07-12 09:41:00 +0200

    page services-marketing process

--- patterns/page-services-marketing-process.php
+++ patterns/page-services-marketing-process.php
@@ -14,9 +14,35 @@
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
   >
 
-        <!-- wp:paragraph -->
-    <p>process</p>
-    <!-- /wp:paragraph -->
+  <!-- wp:group -->
+  <div class="wp-block-group cols-wrapper flex gap-[75px]">
+    <!-- wp:group -->
+    <div class=" wp-block-groupcol1 w-[50%]">
+      <!-- wp:image -->
+      <figure class="wp-block-image object-cover h-full">
+        <img
+          src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-marketing/process-img.jpg"
+          alt="bg-image"
+        />
+      </figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:group -->
+    <!-- wp:group -->
+    <div class="wp-block-group col2 w-[50%]">
+      <!-- wp:paragraph -->
+      <p class="text-[35px] font-semibold mb-[50px]">Jak wygląda proces realizacji</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="text-[16px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="text-[16px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
 
   </div>
   <!-- /wp:group -->
