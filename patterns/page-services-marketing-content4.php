a363345722f28c516cc681c3ae821927246abc8c
Tomek <TK-54-4B>
2023-07-12 15:09:18 +0200

    page services-marketing second swiper

--- patterns/page-services-marketing-content4.php
+++ patterns/page-services-marketing-content4.php
@@ -147,10 +147,10 @@
           <!-- wp:group -->
           <div id="marketing2-next" class="wp-block-group group swiper-button-next w-[56px] h-[56px] absolute top-[50%] desktop:right-[0px]">
             <!-- wp:paragraph -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-primaryYellow">
-              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-primaryYellow">
+              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#FEC400" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
               <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-              <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+              <path d="M20 28L31 28" stroke="black" stroke-width="1.5"/>
             </svg>
             <!-- /wp:paragraph -->
           </div>
