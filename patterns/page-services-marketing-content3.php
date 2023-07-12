b410d7cafc1d5a6cd2d47c49e0772b6ae542148f
Tomek <TK-54-4B>
2023-07-12 15:48:48 +0200

    page services-marketing mobile

--- patterns/page-services-marketing-content3.php
+++ patterns/page-services-marketing-content3.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group -->
-<div class="wp-block-group page-marketing-info my-[150px]">
+<div class="wp-block-group page-marketing-info">
   <!-- wp:group -->
   <div
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
