5db2fd39f9add22480cc117a4ea6b04e4cf778e4
Tomek <TK-54-4B>
2023-07-12 15:16:09 +0200

    page services-marketing cta

--- patterns/page-services-marketing-banner.php
+++ patterns/page-services-marketing-banner.php
@@ -76,7 +76,7 @@
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group paragraph-wrapper container mx-auto desktop:px-0 px-[20px] relative w-full desktop:mb-[150px] mb-[50px] pt-[100px]">
+  <div class="wp-block-group paragraph-wrapper container mx-auto desktop:px-0 px-[20px] relative w-full desktop:mb-[150px] mb-[50px] desktop:pt-[100px] pt-[50px]">
     <!-- wp:paragraph -->
     <svg
       class="shape scale-[0.6] desktop:scale-[1] z-[-1] absolute top-0 desktop:right-[0px] right-auto left-[-40px] desktop:left-auto"
@@ -95,7 +95,7 @@
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
     <p
-    class="ml-[145px] desktop:text-[35px] text-[22px] font-medium desktop:leading-[42px] leading-[32px] desktop:w-[897px]"
+    class="desktop:ml-[145px] desktop:text-[35px] text-[22px] font-medium desktop:leading-[42px] leading-[32px] desktop:w-[897px]"
     >
       Jesteśmy ekspertami od komunikacji i marketingu. Pracujemy dla topowych firm, stawiających przed nami duże wyzwania.
     </p>
