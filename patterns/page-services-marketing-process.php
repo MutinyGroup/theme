ef4fbae395996e57211d2aec7a9856a951a03492
Tomek <TK-54-4B>
2023-07-12 09:33:15 +0200

    page services-marketing process

--- patterns/page-services-marketing-process.php
+++ patterns/page-services-marketing-process.php
@@ -1,21 +1,23 @@
 <?php
 /**
- * Title: page-services-marketing-process
- * Slug: page-services-marketing-process
+ * Title: page-services-marketing-info-process
+ * Slug: page-services-marketing-info-process
  * Categories: text, banner
  * Viewport Width: 1280
  */
 ?>
 
 <!-- wp:group -->
-<div class="wp-block-group page-services-marketing-process my-[150px] overflow-hidden">
+<div class="wp-block-group page-marketing-info my-[150px]">
   <!-- wp:group -->
   <div
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
   >
-    <!-- wp:paragraph -->
+
+        <!-- wp:paragraph -->
     <p>process</p>
     <!-- /wp:paragraph -->
+
   </div>
   <!-- /wp:group -->
 </div>
