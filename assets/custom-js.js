fc6b0068d3476935a08f83ae6dbcf84bf09552fc
Tomek <TK-54-4B>
2023-07-12 17:27:20 +0200

    page services-marketing mobile

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -25,7 +25,7 @@ window.addEventListener("load", function () {
   const iconArrow = document.querySelectorAll("#icon-arrow");
 
   clickBar.forEach(function(item,index) {
-    item.addEventListener("mouseover", () => {
+    item.addEventListener("click", () => {
       if (!stepParagraph[index].classList.contains('active')) { 
         stepParagraph[index].classList.replace('in-active','active')
         iconArrow[index].classList.replace('rotate-90', 'rotate-[-90deg]')
