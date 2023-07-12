2d93b2482668f6d949ae83f64841851237497b50
Tomek <TK-54-4B>
2023-07-12 09:15:07 +0200

    page services-marketing

--- patterns/page-services-marketing-info.php
+++ patterns/page-services-marketing-info.php
@@ -39,7 +39,7 @@
           </defs>
         </svg>
         <!-- /wp:paragraph -->
-        
+
         <!-- wp:group -->
         <div
         class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
@@ -107,83 +107,7 @@
     </div>
     <!-- /wp:group -->
     
-    <!-- wp:group -->
-    <div class="wp-block-group contact-info-cols-wrapper flex desktop:gap-[80px] gap-[20px]">
-      <!-- wp:group -->
-      <div class="wp-block-group col1 w-[30%] img-clip-path-rounded">
-        <!-- wp:group -->
-        <div
-          class="wp-block-group img-clip-path-wrapper img-clip-path-realizations w-full h-full-[20px] rounded-br-[20px] overflow-hidden"
-        >
-          <!-- wp:image -->
-          <figure
-            class="wp-block-image group-hover:grayscale transition ease-in-out duration-[400ms]"
-          >
-            <img
-              src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-contact/co-founder-img.jpg"
-              alt="services-backgroun-image"
-            />
-          </figure>
-          <!-- /wp:image -->
-          <!-- wp:paragraph -->
-          <svg
-            class="flt_svg"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <filter id="flt_tag">
-                <feGaussianBlur
-                  in="SourceGraphic"
-                  stdDeviation="8"
-                  result="blur"
-                />
-                <feColorMatrix
-                  in="blur"
-                  mode="matrix"
-                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
-                  result="flt_tag"
-                />
-                <feComposite
-                  in="SourceGraphic"
-                  in2="flt_tag"
-                  operator="atop"
-                />
-              </filter>
-            </defs>
-          </svg>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:group -->
-      
-      <!-- wp:group -->
-      <div class="wp-block-group col2 self-center text-wrapper">
-        <!-- wp:group -->
-        <div class="wp-block-group mb-[30px] name-wrapper leading-[28px]">
-          <!-- wp:paragraph -->
-          <p class="desktop:text-[35px] text-[24px] font-medium">Karol Anusiewicz</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="desktop:text-[20px] text-[15px] font-light">Co-founder & Project Manager</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
 
-        <!-- wp:group -->
-        <div class="wp-block-group mail-phone-wrapper flex flex-col gap-y-[14px] desktop:leading-[38px] leading-[24px]">
-          <!-- wp:paragraph -->
-          <p class="w-fit desktop:text-[25px] text-[18px] leading-[24px] inline font-medium border-b-[1px] border-black hover:border-primaryYellow hover:text-primaryYellow transition ease-out duration-300 cursor-pointer">kontakt@mutiny.pl</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="w-fit desktop:text-[20px] text-[15px] leading-[24px] inline font-medium border-b-[1px] border-black hover:border-primaryYellow hover:text-primaryYellow transition ease-out duration-300 cursor-pointer">+48 795 123 584</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
