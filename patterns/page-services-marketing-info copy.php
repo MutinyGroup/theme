677fc9279f92af3b4e3a88ec590dd6075a2c79a7
Tomek <TK-54-4B>
2023-07-12 09:31:16 +0200

    page services-marketing process

---
+++ patterns/page-services-marketing-info copy.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: page-services-marketing-info-copy
+ * Slug: page-services-marketing-info-copy
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group -->
+<div class="wp-block-group page-marketing-info my-[150px]">
+  <!-- wp:group -->
+  <div
+    class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
+  >
+
+        <!-- wp:paragraph -->
+    <p>process</p>
+    <!-- /wp:paragraph -->
+
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
