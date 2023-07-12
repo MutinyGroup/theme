2567fa377f5684826e2b8f0c2d3753ba01a47d76
Tomek <TK-54-4B>
2023-07-12 16:58:48 +0200

    page services-marketing mobile

--- patterns/page-services-marketing-content2.php
+++ patterns/page-services-marketing-content2.php
@@ -57,8 +57,7 @@
           <p
             class="uppercase"
           >
-            content <br />
-            Marketing
+            content
             <span
               class="relative inline desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
               >Marketingowych</span
