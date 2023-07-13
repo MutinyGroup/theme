b9fd9971175746d6bb599b404170a18bd3ef8c6d
Tomek <TK-54-4B>
2023-07-13 09:57:11 +0200

    bg yellow flash

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -14,9 +14,27 @@
     <!-- wp:group -->
     <div class="wp-block-group banner-title relative desktop:mb-[89px] mb-[45px] desktop:pt-[50px] pt-0 uppercase fullhd:text-[85px] desktop:text-[85px] text-[32px] font-normal desktop:w-[940px] wide:leading-[85px] desktop:leading-[80px] leading-[35px]">
       <!-- wp:group -->
-      <div class="wp-block-group desktop:block hidden z-[-1] bg-flash desktop:top-[40px] top-[20px] left-[-20px]">
+      <div class="wp-block-group hidden desktop:block hidden z-[-1] bg-flash desktop:top-[40px] top-[20px] left-[-20px]">
       </div>
       <!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <svg class="flash desktop:scale-[1] scale-[0.4] absolute desktop:top-[-330px] top-[-360px] desktop:left-[-140px] right-[-100px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+        <g filter="url(#filter0_f_1787_1988)">
+        <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
+        </g>
+        <defs>
+        <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
+        </filter>
+        <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FEC400" stop-opacity="0.6"/>
+        <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
+        </linearGradient>
+        </defs>
+      </svg>
+      <!-- /wp:paragraph -->
       <!-- wp:group -->
       <div class="wp-block-group w-[300px] h-[300px] desktop:hidden block z-[-1] bg-flash-png absolute top-[-40px] left-[-20px] bg-[url('https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/bg-flash.png')] bg-center bg-no-repeat bg-cover scale-[2]">
       </div>
