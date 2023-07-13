2d270accbc3ba37573a3ec36922a431c0e3d0a14
Tomek <TK-54-4B>
2023-07-13 15:52:19 +0200

    swiper posts

--- patterns/blog.php
+++ patterns/blog.php
@@ -328,149 +328,128 @@
 
     <!-- wp:group -->
     <div class="wp-block-group relative">
-      
       <!-- wp:group -->
       <div class="wp-block-group desktop:mt-[-120px] w-full overflow-hidden">
-
         <!-- wp:group -->
         <div id="blog" class="wp-block-group swiper swiperBlog relative desktop:pt-[180px] absolute top-0 desktop:left-[150px] left-[20px]">
           <!-- wp:group -->
           <div class="wp-block-group swiper-wrapper">
+            <?php
+              $args = array(
+                  'post_type' => 'post',
+                  'posts_per_page' => 4,
+                  'order' => 'ASC'
+              );
+              $post_query = new WP_Query($args);
+              $counter = 0;
 
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:min-w-[444px] w-[280px] desktop:h-[675px] h-[520px]">
-              <!-- wp:group -->
-              <div class="wp-block-group img-clip-path-rounded mb-[20px]">
-                <!-- wp:group -->
-                <div class="wp-block-group img-clip-path-blog overflow-hidden rounded-[20px]">
-                  <!-- wp:image -->
-                  <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
-                    <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/blog-post-img/blog-post-img1.png" alt="blog-post-img">
-                  </figure>
-                  <!-- /wp:image -->
-                </div>
-                <!-- /wp:group -->
-                <!-- wp:paragraph -->
-                <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <filter id="flt_tag">
-                      <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
-                      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
-                      <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-                    </filter>
-                  </defs>
-                </svg>
-                <!-- /wp:paragraph -->
-              </div>
-              <!-- /wp:group -->
+              if($post_query->have_posts() ) {
+                while($post_query->have_posts() ) {
+                  $post_query->the_post();
+                  $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(),'portrait');
+                  $counter++;
+                  if ($counter == 1){ ?>
+                    <!-- wp:group -->
+                    <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:w-[444px] w-[300px] desktop:h-[675px] h-[500px]">
+                      <!-- wp:paragraph -->
+                      <a href="<?php the_permalink(); ?>">                      
+                        <!-- wp:group -->
+                        <div class="wp-block-group img-clip-path-rounded mb-[20px]">
+                          <!-- wp:group -->
+                          <div class="wp-block-group img-clip-path-blog h-[320px] desktop:h-[460px] overflow-hidden rounded-[20px]">
+                            <!-- wp:image -->
+                            <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
+                              <img src="<?php echo $postImageUrl[0]; ?>" alt="post-image">
+                            </figure>
+                            <!-- /wp:image -->
+                          </div>
+                          <!-- /wp:group -->
+                          <!-- wp:paragraph -->
+                          <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
+                              <filter id="flt_tag">
+                                <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+                                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+                                <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+                              </filter>
+                            </defs>
+                          </svg>
+                          <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
 
+                        <!-- wp:paragraph -->
+                        <p class="slide-date font-light flex items-center before:content-[''] before:w-[20px] before:h-[1px] before:bg-primaryYellow before:inline-block before:mr-[10px] mb-[20px]"><?php the_time('d / m / Y'); ?></p>
+                        <!-- /wp:paragraph -->
 
+                        <!-- wp:paragraph -->
+                        <p class="subtitle inline text-[23px] font-medium leading-[30px] border-b-[1px] border-transparent group-hover:border-primaryYellow group-hover:text-primaryYellow transition ease-out duration-[800ms]">
+                          <?php the_title(); ?>
+                        </p>
+                        <!-- /wp:paragraph -->
+                      </a>
+                      <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:group -->                
+                  <?php 
+                  }else{ ?>                    
+                    <!-- wp:group -->
+                    <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:w-[444px] w-[280px] desktop:h-[675px] h-[500px]">
+                      <!-- wp:paragraph -->
+                      <a href="<?php the_permalink(); ?>">                        
+                        <!-- wp:group -->
+                        <div class="wp-block-group img-wrapper mb-[20px]">
+                          <!-- wp:group -->
+                          <div class="wp-block-group overflow-hidden h-[300px] desktop:h-[460px] rounded-[20px]">
+                            <!-- wp:image -->
+                            <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
+                              <img src="<?php echo $postImageUrl[0]; ?>" alt="post-image">
+                            </figure>
+                            <!-- /wp:image -->
+                          </div>
+                          <!-- /wp:group -->
+                          <!-- wp:paragraph -->
+                          <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
+                              <filter id="flt_tag">
+                                <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+                                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+                                <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+                              </filter>
+                            </defs>
+                          </svg>
+                          <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
 
+                        <!-- wp:paragraph -->
+                        <p class="slide-date font-light flex items-center before:content-[''] before:w-[20px] before:h-[1px] before:bg-primaryYellow before:inline-block before:mr-[10px] mb-[20px]"><?php the_time('d / m / Y'); ?></p>
+                        <!-- /wp:paragraph -->
 
-              <!-- wp:paragraph -->
-              <p class="slide-date font-light flex items-center before:content-[''] before:w-[20px] before:h-[1px] before:bg-primaryYellow before:inline-block before:mr-[10px] mb-[20px]">10 / 03 / 2023</p>
-              <!-- /wp:paragraph -->
-
-              <!-- wp:paragraph -->
-              <p class="subtitle inline text-[23px] font-medium leading-[30px] border-b-[1px] border-transparent group-hover:border-primaryYellow group-hover:text-primaryYellow transition ease-out duration-[800ms]">
-                5 urodziny Galerii Navigator - zajrzyjmy na backstage
-              </p>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:min-w-[444px] w-[280px] desktop:h-[675px] h-[520px]">
-              <!-- wp:group -->
-              <div class="wp-block-group img-wrapper mb-[20px]">
-                <!-- wp:group -->
-                <div class="wp-block-group overflow-hidden desktop:h-[420px] rounded-[20px]">
-                  <!-- wp:image -->
-                  <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
-                    <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/blog-post-img/blog-post-img2.png" alt="blog-post-img">
-                  </figure>
-                  <!-- /wp:image -->
-                </div>
-                <!-- /wp:group -->
-                <!-- wp:paragraph -->
-                <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <filter id="flt_tag">
-                      <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
-                      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
-                      <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-                    </filter>
-                  </defs>
-                </svg>
-                <!-- /wp:paragraph -->
-              </div>
-              <!-- /wp:group -->
-
-              <!-- wp:paragraph -->
-              <p class="slide-date font-light flex items-center before:content-[''] before:w-[20px] before:h-[1px] before:bg-primaryYellow before:inline-block before:mr-[10px] mb-[20px]">25 / 02 / 2023</p>
-              <!-- /wp:paragraph -->
-
-              <!-- wp:paragraph -->
-              <p class="subtitle inline text-[23px] font-medium leading-[30px] border-b-[1px] border-transparent group-hover:border-primaryYellow group-hover:text-primaryYellow transition ease-out duration-[800ms]">
-              Podsumowanie akcji WOŚP 2023 z Mutiny Group
-              </p>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide desktop:mr-[80px] mr-[20px] relative group desktop:min-w-[444px] w-[280px] desktop:h-[675px] h-[520px]">
-              <!-- wp:group -->
-              <div class="wp-block-group img-wrapper mb-[20px]">
-                <!-- wp:group -->
-                <div class="wp-block-group overflow-hidden desktop:h-[420px] rounded-[20px]">
-                  <!-- wp:image -->
-                  <figure class="wp-block-image post-img scale-[1.12] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
-                    <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/blog-post-img/blog-post-img3.jpg" alt="blog-post-img">
-                  </figure>
-                  <!-- /wp:image -->
-                </div>
-                <!-- /wp:group -->
-                <!-- wp:paragraph -->
-                <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <filter id="flt_tag">
-                      <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
-                      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
-                      <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-                    </filter>
-                  </defs>
-                </svg>
-                <!-- /wp:paragraph -->
-              </div>
-              <!-- /wp:group -->
-
-
-
-
-              <!-- wp:paragraph -->
-              <p class="slide-date font-light flex items-center before:content-[''] before:w-[20px] before:h-[1px] before:bg-primaryYellow before:inline-block before:mr-[10px] mb-[20px]">10 / 03 / 2023</p>
-              <!-- /wp:paragraph -->
-
-              <!-- wp:paragraph -->
-              <p class="subtitle inline text-[23px] font-medium leading-[30px] border-b-[1px] border-transparent group-hover:border-primaryYellow group-hover:text-primaryYellow transition ease-out duration-[800ms]">
-                5 urodziny Galerii Navigator - zajrzyjmy na backstage
-              </p>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-
+                        <!-- wp:paragraph -->
+                        <p class="subtitle inline text-[23px] font-medium leading-[30px] border-b-[1px] border-transparent group-hover:border-primaryYellow group-hover:text-primaryYellow transition ease-out duration-[800ms]">
+                          <?php the_title(); ?>
+                        </p>
+                        <!-- /wp:paragraph -->
+                      </a>
+                      <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:group -->                      
+                  <?php
+                  };
+                };
+              };
+            ?>
           </div>
           <!-- /wp:group -->
 
           <!-- wp:group -->
           <div id="blog-prev" class="wp-block-group group swiper-button-prev desktop:inline-block hidden w-[56px] h-[56px] absolute desktop:top-[90px] top-[35px] desktop:left-[800px] left-[190px]">
             <!-- wp:paragraph -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
-              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
-              <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-              <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-primaryYellow">
+              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAA" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+              <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+              <path d="M20 28L31 28" stroke="black" stroke-width="1.5"/>
             </svg>
             <!-- /wp:paragraph -->
           </div>
@@ -478,16 +457,14 @@
           <!-- wp:group -->
           <div id="blog-next" class="wp-block-group group swiper-button-next desktop:inline-block hidden w-[56px] h-[56px] absolute desktop:top-[90px] top-[35px] desktop:left-[870px] left-[260px]">
             <!-- wp:paragraph -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-white">
-              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
-              <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-              <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-primaryYellow">
+              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAA" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+              <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+              <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
             </svg>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
-
-
         </div>
         <!-- /wp:group -->
       </div>
