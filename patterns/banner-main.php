c6fa0bd4765836fe5cbb56da28df5058d09cf21f
Tomek <TK-54-4B>
2023-07-13 10:40:57 +0200

    bg yellow flash

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -13,12 +13,8 @@
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] wide:h-[1180px] desktop:h-[790px] h-[700px]">
     <!-- wp:group -->
     <div class="wp-block-group banner-title relative desktop:mb-[89px] mb-[45px] desktop:pt-[50px] pt-0 uppercase fullhd:text-[85px] desktop:text-[85px] text-[32px] font-normal desktop:w-[940px] wide:leading-[85px] desktop:leading-[80px] leading-[35px]">
-      <!-- wp:group -->
-      <div class="wp-block-group hidden desktop:block hidden z-[-1] bg-flash desktop:top-[40px] top-[20px] left-[-20px]">
-      </div>
-      <!-- /wp:group -->
       <!-- wp:paragraph -->
-      <svg class="flash desktop:scale-[1] scale-[0.5] absolute desktop:top-[-330px] top-[-350px] desktop:left-[-140px] left-[-250px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+      <svg class="flash desktop:scale-[1] scale-[0.5] absolute desktop:top-[-330px] top-[-350px] desktop:left-[-140px] right-[-90px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
         <g filter="url(#filter0_f_1787_1988)">
         <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
         </g>
@@ -36,14 +32,6 @@
       </svg>
       <!-- /wp:paragraph -->
 
-      <!-- wp:image -->
-      <figure class="wp-block-image absolute desktop:left-[-140px] desktop:scale-[1] desktop:top-[-170px] flash hidden left-[-110px] scale-[0.8] top-[-140px] z-[-1]">
-        <img
-          src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/bg-yellow-flash.png"
-          alt="background image"
-        />
-      </figure>
-      <!-- /wp:image -->
       <!-- wp:group -->
       <div class="wp-block-group bg-shape w-[1220px] h-[750px] z-[-1] opacity-[0.25] absolute top-[130px] left-[-320px] bg-[url('https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/banner-bg-shape.png')] bg-center bg-no-repeat bg-cover">
       </div>
