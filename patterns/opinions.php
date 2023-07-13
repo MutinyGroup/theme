d803c8f66a5c2994bb6d07fd7263fea8d3be53df
Tomek <TK-54-4B>
2023-07-13 14:40:43 +0200

    swiper test

--- patterns/opinions.php
+++ patterns/opinions.php
@@ -31,7 +31,7 @@
     <div class="wp-block-group swiper-section-wrapper relative desktop:flex">
 
       <!-- wp:group -->
-      <div class="wp-block-group swiper-nav-wrapper desktop:block hidden relative basis-[40%] z-0">
+      <div class="wp-block-group swiper-nav-wrapper desktop:block hidden relative w-[40%] z-0">
         <!-- wp:group -->
         <div id="opinions-prev" class="wp-block-group group swiper-button-prev w-[56px] h-[56px] absolute desktop:top-[60px] top-[0px] desktop:left-[200px] left-[130px]">
           <!-- wp:paragraph -->
@@ -64,7 +64,7 @@
       <!-- /wp:group -->
 
       <!-- wp:group -->
-      <div id="opinions" class="wp-block-group swiper swiperOpinions basis-[60%]">
+      <div id="opinions" class="wp-block-group swiper swiperOpinions desktop:max-w-[500px]">
         <!-- wp:group -->
         <div class="wp-block-group swiper-wrapper">
           <!-- wp:group -->
