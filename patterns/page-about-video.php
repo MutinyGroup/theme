04cb29c9d90764a64101b9f30b6cb35f02a2c560
Tomek <TK-54-4B>
2023-07-14 05:11:03 +0200

    video test

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -94,7 +94,9 @@
 
 
     <!-- wp:core/video -->
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/HaJAtHhVzC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <figure>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/HaJAtHhVzC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </figure>
     <!-- /wp:core/video -->
     </div>
   <!-- /wp:group -->
