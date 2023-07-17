3dd36d43d2db9fc2f2aa4955d13dbe02c9a9a8fa
Tomek <TK-54-4B>
2023-07-17 18:51:39 +0200

    tests

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -107,10 +107,7 @@
     </div></figure>
     <!-- /wp:embed --> 
     
-    
-    <iframe width="420" height="315"
-    src="https://www.youtube.com/embed/tgbNymZ7vqY">
-    </iframe>
+
 
   </div>
   <!-- /wp:group -->
