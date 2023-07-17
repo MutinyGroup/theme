63e6f600ceb42b46924b9b391023a8e267fb9d6b
Tomek <TK-54-4B>
2023-07-17 18:45:47 +0200

    tests

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -104,6 +104,13 @@
     </figure>
     <!-- /wp:embed -->    
 
+
+    <!-- wp:embed {"url":"https://www.youtube.com/watch?v=QZXc39hT8t4","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+    <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+    https://www.youtube.com/watch?v=QZXc39hT8t4
+    </div></figure>
+    <!-- /wp:embed -->    
+
   </div>
   <!-- /wp:group -->
 </div>
