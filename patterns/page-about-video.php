2f12ca10be096e5cfbd5317391f63435a7e128ed
Tomek <TK-54-4B>
2023-07-17 18:55:54 +0200

    tests

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -71,41 +71,11 @@
     <!-- /wp:parapgraph -->
 
 
-    <!-- wp:embed {"url":"https://youtu.be/QZXc39hT8t4","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-    <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
-      <div class="wp-block-embed__wrapper">https://youtu.be/QZXc39hT8t4</div>
-    <figcaption class="wp-element-caption">podpis</figcaption></figure>
-    <!-- /wp:embed -->
-
-
-    <!-- wp:embed {"url":"https://www.youtube.com/watch?v=QZXc39hT8t4","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-    <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
-      <div class="wp-block-embed__wrapper">https://www.youtube.com/watch?v=QZXc39hT8t4</div>
-    <figcaption class="wp-element-caption">podpis2 watch</figcaption></figure>
-    <!-- /wp:embed -->
-
-
-    <!-- wp:embed {"url":"https://www.youtube.com/embed/QZXc39hT8t4","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-    <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
-      <div class="wp-block-embed__wrapper">https://www.youtube.com/embed/QZXc39hT8t4</div>
-    <figcaption class="wp-element-caption">podpis2 watch</figcaption></figure>
-    <!-- /wp:embed -->
-
-    <!-- wp:embed {"url":"https://www.youtube.com/watch?v=QZXc39hT8t4","type":"video","providerNameSlug":"youtube"} -->
-    <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube">
-        <div class="wp-block-embed__wrapper">
-        https://www.youtube.com/watch?v=QZXc39hT8t4
-        </div>
-        <figcaption class="wp-element-caption">Embedded content from an example URL</figcaption>
-    </figure>
-    <!-- /wp:embed -->    
-
-
     <!-- wp:embed {"url":"https://www.youtube.com/watch?v=QZXc39hT8t4","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
     <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
     https://www.youtube.com/watch?v=QZXc39hT8t4
     </div></figure>
-    <!-- /wp:embed --> 
+    <!-- /wp:embed -->
     
 
 
