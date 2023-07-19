4ffcf2a1ccdf6d65e4ae5b6cde130ce0bd4ce214
Tomek <TK-54-4B>
2023-07-19 09:19:53 +0200

    about us list mobile

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -83,7 +83,7 @@
             <!-- wp:group -->
             <div class="wp-block-group text-wrapper">
               <!-- wp:paragraph -->
-              <p class="target-title mb-[15px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
+              <p class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
               <!-- /wp:paragraph -->
               <!-- wp:paragraph -->
               <p class="target-paragraph font-light leading-[22px]">
@@ -105,7 +105,7 @@
               <!-- /wp:group -->
 
               <!-- wp:paragraph -->
-              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">01</p>
+              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">02</p>
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
@@ -113,37 +113,7 @@
             <!-- wp:group -->
             <div class="wp-block-group text-wrapper">
               <!-- wp:paragraph -->
-              <p class="target-title mb-[15px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
-              <!-- /wp:paragraph -->
-              <!-- wp:paragraph -->
-              <p class="target-paragraph font-light leading-[22px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              </p>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-          </li>
-          <!-- /wp:list-item -->
-          
-          <!-- wp:list-item -->
-          <li class="relative mb-[20px] desktop:mb-0 desktop:max-w-[264px] max-w-full flex desktop:list-item gap-[20px]">
-            <!-- wp:group -->
-            <div class="wp-block-group counter-index-wrapper inline relative">
-            <!-- wp:group -->
-              <div class="wp-block-group circle-counter-index z-[-1] opacity-[15%] w-[80px] h-[80px] scale-[1] bg-[url('https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/about-us-circle-index.png')] bg-center bg-no-repaet bg-contain absolute top-[-85px] right-[-40px]">
-              </div>
-              <!-- /wp:group -->
-
-              <!-- wp:paragraph -->
-              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">01</p>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:group -->
-            <div class="wp-block-group text-wrapper">
-              <!-- wp:paragraph -->
-              <p class="target-title mb-[15px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
+              <p class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
               <!-- /wp:paragraph -->
               <!-- wp:paragraph -->
               <p class="target-paragraph font-light leading-[22px]">
@@ -165,7 +135,7 @@
               <!-- /wp:group -->
 
               <!-- wp:paragraph -->
-              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">01</p>
+              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">03</p>
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
@@ -173,7 +143,7 @@
             <!-- wp:group -->
             <div class="wp-block-group text-wrapper">
               <!-- wp:paragraph -->
-              <p class="target-title mb-[15px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
+              <p class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
               <!-- /wp:paragraph -->
               <!-- wp:paragraph -->
               <p class="target-paragraph font-light leading-[22px]">
@@ -183,7 +153,37 @@
             </div>
             <!-- /wp:group -->
           </li>
-          <!-- /wp:list-item -->                                    
+          <!-- /wp:list-item -->
+          
+          <!-- wp:list-item -->
+          <li class="relative mb-[20px] desktop:mb-0 desktop:max-w-[264px] max-w-full flex desktop:list-item gap-[20px]">
+            <!-- wp:group -->
+            <div class="wp-block-group counter-index-wrapper inline relative">
+            <!-- wp:group -->
+              <div class="wp-block-group circle-counter-index z-[-1] opacity-[15%] w-[80px] h-[80px] scale-[1] bg-[url('https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/about-us-circle-index.png')] bg-center bg-no-repaet bg-contain absolute top-[-85px] right-[-40px]">
+              </div>
+              <!-- /wp:group -->
+
+              <!-- wp:paragraph -->
+              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">04</p>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group -->
+            <div class="wp-block-group text-wrapper">
+              <!-- wp:paragraph -->
+              <p class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
+              <!-- /wp:paragraph -->
+              <!-- wp:paragraph -->
+              <p class="target-paragraph font-light leading-[22px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              </p>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+          </li>
+          <!-- /wp:list-item -->          
         </ul>
         <!-- /wp:list -->
 
