f1de73c07947d777bb86574127a80c710c0e2fc2
Tomek <TK-54-4B>
2023-07-19 15:59:38 +0200

    swiper test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -75,10 +75,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     direction: "horizontal",
     slidesPerView: 2,
     slidesPerGroup: 2,
-    // autoplay: {
-    //   delay: 2000,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
   });
 
   new Swiper('#opinions-dark', {
