b5e976fc6330b2d42753a3186299173b5ebf782e
Tomek <TK-54-4B>
2023-07-19 16:15:09 +0200

    swiper fix

--- patterns/companies-logos.php
+++ patterns/companies-logos.php
@@ -64,7 +64,7 @@
     <!-- /wp:group -->
 
     <!-- wp:group -->
-    <div class="wp-block-group companies-wrapper hidden desktop:flex gap-x-[110px] gap-y-[40px] flex-wrap items-center justify-center">
+    <div class="wp-block-group companies-wrapper-dark hidden desktop:flex gap-x-[110px] gap-y-[40px] flex-wrap items-center justify-center">
       <!-- wp:core/image -->
       <figure class="swiper-slide wp-block-image desktop:w-[165px] w-[125px] h-auto self-center">
         <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/logo companies/logo-serenada.png" alt="logo-serenada">
