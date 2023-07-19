2ca75d415fe6be50c69059df57c3b4564c8cdaae
Tomek <TK-54-4B>
2023-07-19 17:28:09 +0200

    wp:tags query test

--- patterns/blog-layout-test.php
+++ patterns/blog-layout-test.php
@@ -12,8 +12,8 @@
 <!-- wp:query {"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude"} } -->
 <div class="wp-block-query alignwide jace-blog-layout">
   <!-- wp:post-template {"align":"wide"} -->
-    <!-- wp:group {"style":{"border":{"radius":"4px"},"color":{"background":"#f2f2f203"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","verticalAlignment":"top"}} -->
-    <div class="wp-block-group has-background" style="background-color:#f2f2f203;border-radius:4px">
+    <!-- wp:group -->
+    <div class="wp-block-group">
       <!-- wp:post-featured-image {"isLink":true,"width":"200px","height":"149px","className":"is-style-jace-zoom-image is-style-jace-image-border-radius"} /-->
       <!-- wp:group {"className":"jace-post-meta"} -->
       <div class="wp-block-group jace-post-meta">
