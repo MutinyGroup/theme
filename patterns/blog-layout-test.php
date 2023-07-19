0ab1dce4da541277149f80dd910b2b2e77a4bcac
Tomek <TK-54-4B>
2023-07-19 17:26:07 +0200

    wp:tags query test

--- patterns/blog-layout-test.php
+++ patterns/blog-layout-test.php
@@ -10,22 +10,22 @@
 
 
 <!-- wp:query {"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude"} } -->
-<div class="wp-block-query">
-  <!-- wp:post-template -->
-    <!-- wp:group -->
-    <div class="wp-block-group has-background">
-      <!-- wp:post-featured-image {"isLink":true"} /-->
-      <!-- wp:group -->
-      <div class="wp-block-group post-meta">
+<div class="wp-block-query alignwide jace-blog-layout">
+  <!-- wp:post-template {"align":"wide"} -->
+    <!-- wp:group {"style":{"border":{"radius":"4px"},"color":{"background":"#f2f2f203"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","verticalAlignment":"top"}} -->
+    <div class="wp-block-group has-background" style="background-color:#f2f2f203;border-radius:4px">
+      <!-- wp:post-featured-image {"isLink":true,"width":"200px","height":"149px","className":"is-style-jace-zoom-image is-style-jace-image-border-radius"} /-->
+      <!-- wp:group {"className":"jace-post-meta"} -->
+      <div class="wp-block-group jace-post-meta">
       <!-- wp:post-terms {"term":"category","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} /-->
-      <!-- wp:post-title {"isLink":true} /-->
+      <!-- wp:post-title {"isLink":true,"fontSize":"medium"} /-->
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   <!-- /wp:post-template -->
 
-  <!-- wp:query-pagination -->
+  <!-- wp:query-pagination {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
   <!-- wp:query-pagination-previous {"label":"Previous"} /-->
 
   <!-- wp:query-pagination-next {"label":"Next"} /-->
