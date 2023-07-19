73d760e6a9ee52dbb5052b88060258ba37b0bd98
Tomek <TK-54-4B>
2023-07-19 12:11:10 +0200

    paragraph fix

--- patterns/page-about-team.php
+++ patterns/page-about-team.php
@@ -55,7 +55,7 @@
     <!-- wp:group -->
     <div class="wp-block-group cols desktop:flex">
       <!-- wp:group -->
-      <div class="wp-block-group col1 relative desktop:pl-[105px] mb-[50px] desktop:w-[45%]">
+      <div class="wp-block-group col1 relative desktop:pl-[150px] mb-[50px] desktop:w-[45%]">
         <!-- wp:paragraph -->
         <svg
           class="shape opacity-[60%] absolute top-[270px] left-[30px]"
