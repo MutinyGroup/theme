362dd815aeb5a051a9590036e0b5fa417ea8b1cc
Tomek <TK-54-4B>
2023-07-20 14:40:24 +0200

    test

--- patterns/page-about-video.php
+++ patterns/page-about-video.php
@@ -9,10 +9,7 @@
 
 <!-- wp:group -->
 <div class="wp-block-group hidden desktop:block video overflow-hidden">
-  <!-- wp:paragraph -->
-  <p>test</p>
-  <!-- /wp:paragraph -->
-<!-- wp:group -->
+  <!-- wp:group -->
   <div class="wp-block-group video relative container mx-auto desktop:px-0 px-[20px] mb-[150px]">
     <!-- wp:group -->
     <div class="wp-block-group paragraph-wrapper relative w-full pt-[100px]">
@@ -20,7 +17,7 @@
     </div>
     <!-- /wp:group -->
     <!-- wp:group -->
-    <div class="wp-block-group hidden only-video-bg group cursor-pointer img-clip-path-rounded">
+    <div class="wp-block-group hidden only-bg-video group cursor-pointer img-clip-path-rounded">
       <!-- wp:buttons -->
       <div
         class="wp-block-buttons z-[1] desktop:w-[175px] w-[80px] desktop:h-[175px] h-[80px] mx-0 text-white text-center uppercase rounded-full border desktop:border-[4px] border-[3px] border-[#D5D5D5] translate-y-[-50%] absolute desktop:top-[50%] top-[300px] desktop:left-[50%] left-[50%] translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
