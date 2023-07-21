1d6faefbbeb7c0aa2317e43bb6c501e03f66ed8e
Tomek <TK-54-4B>
2023-07-21 15:28:52 +0200

    realizacje

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -74,7 +74,7 @@
       ?>
 
     <!-- wp:list -->
-    <ul class="wp-block-list hidden old events-list mb-[150px] flex gap-[10px]">
+    <ul class="wp-block-list  old events-list mb-[150px] flex gap-[10px]">
       <!-- wp:list-item -->
       <li class="event-tile group desktop:flex gap-[75px]">
         <!-- wp:group -->
@@ -181,6 +181,112 @@
         <!-- /wp:group -->
       </li>
       <!-- /wp:list-item -->
+      <!-- wp:list-item -->
+      <li class="event-tile group desktop:flex gap-[75px]">
+        <!-- wp:group -->
+        <div
+          class="wp-block-group event-image-wrapper desktop:mb-0 mb-[20px] w-[40%] max-h-[400px] overflow-hidden w-full rounded-[20px]"
+        >
+          <!-- wp:image -->
+          <figure
+            class="wp-block-image event-image grayscale scale-[1] group-hover:scale-[1.04] group-hover:grayscale-0 transition ease-out duration-[500ms]"
+          >
+            <img
+              src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-realizations/realizations-event-img1.jpg"
+              alt=""
+            />
+          </figure>
+          <!-- /wp:image -->
+        </div>
+        <!-- /wp:group -->
+        
+        <!-- wp:group -->
+        <div class="wp-block-group content-wrapper desktop:w-[60%]">
+          <!-- wp:image -->
+          <figure
+            class="wp-block-image event-logo-color mb-[25px] w-[60px]"
+          >
+            <img
+              src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-realizations/logo-efc-color.png"
+              alt=""
+            />
+          </figure>
+          <!-- /wp:image -->
+
+          <!-- wp:list -->
+          <ul
+            class="wp-block-list categories-under-logo mb-[20px] flex gap-[10px]"
+          >
+            <!-- wp:list-item -->
+            <li
+              class="w-fit p-[0px_8px] flex justify-center items-center bg-[#F2F2F2] leading-[20px] border border-[#D5D5D5] border-[2px] rounded-full uppercase text-[13px] font-semibold hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer"
+            >
+              <!-- wp:paragraph -->
+              <a href="#">eventy</a>
+              <!-- /wp:paragraph -->
+            </li>
+            <!-- /wp:list-item -->
+            <!-- wp:list-item -->
+            <li
+              class="w-fit p-[0px_8px] flex justify-center items-center bg-[#F2F2F2] leading-[20px] border border-[#D5D5D5] border-[2px] rounded-full uppercase text-[13px] font-semibold hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer"
+            >
+              <!-- wp:paragraph -->
+              <a href="#">scenografia</a>
+              <!-- /wp:paragraph -->
+            </li>
+            <!-- /wp:list-item -->
+          </ul>
+          <!-- /wp:list -->
+
+          <!-- wp:title -->
+          <h2 class="mb-[50px] text-[35px] font-semibold leading-[42px]">
+            Organizacja 3-dniowego wydarzenia dla branży finansowej
+          </h2>
+          <!-- /wp:title -->
+          <!-- wp:group -->
+          <div
+            id="opinions-next"
+            class="wp-block-group w-[56px] h-[56px]"
+          >
+            <!-- wp:paragraph -->
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="56"
+              height="56"
+              viewBox="0 0 56 56"
+              fill="none"
+              class="group fill-transparent group-hover:fill-primaryYellow"
+            >
+              <rect
+                x="1.5"
+                y="1.5"
+                width="53"
+                height="53"
+                rx="26.5"
+                stroke="#AAAAAA"
+                stroke-width="3"
+                class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"
+              />
+              <path
+                d="M28 35L35 28.0189L28.0379 21"
+                stroke="black"
+                stroke-width="1.5"
+                class="group-hover:stroke-black transition-stroke ease-out duration-500"
+              />
+              <path
+                d="M20 28L31 28"
+                stroke="black"
+                stroke-width="1.5"
+                class="group-hover:stroke-black transition-stroke ease-out duration-500"
+              />
+            </svg>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+      </li>
+      <!-- /wp:list-item -->      
     </ul>
     <!-- /wp:list -->
 
