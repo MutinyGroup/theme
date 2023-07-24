bd84d10563917b306159ae0d2b79c81f22039f7b
Tomek <TK-54-4B>
2023-07-24 15:56:27 +0200

    single ulsugi test

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -542,7 +542,7 @@
               $counter++;
               if (true){ ?>
                 <!-- wp:list-item -->
-                <li class="wp-block-group hidden bigger swiper-slide relative group desktop:w-[520px] w-fit desktop:h-[675px] h-[320px] <?php if (!($counter % 2)) echo ''; else echo 'desktop:translate-y-[120px]'; ?>">
+                <li class="wp-block-group hidden bigger swiper-slide relative group desktop:w-[520px] w-fit desktop:h-[675px] h-[320px] <?php if (!($counter % 2)) echo 'desktop:translate-y-[120px]'; else echo ''; ?>">
                   <!-- wp:paragraph -->
                   <a href="<?php the_permalink(); ?>">
                     <!-- wp:group -->
