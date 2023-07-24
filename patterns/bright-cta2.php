01fbfaf172bb393dd7ef9100c66c99b1927ce56b
Tomek <TK-54-4B>
2023-07-24 18:45:14 +0200

    block names

--- patterns/bright-cta2.php
+++ patterns/bright-cta2.php
@@ -7,7 +7,7 @@
  */
 ?>
 <!-- wp:group -->
-<div class="wp-block-group relative cta desktop:mb-[150px] overflow-hidden desktop:overflow-visible">
+<div id="CTA2" class="wp-block-group relative cta desktop:mb-[150px] overflow-hidden desktop:overflow-visible">
   <!-- wp:group -->
   <div class="wp-block-group container group mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[120px] desktop:h-[700px] h-[480px]">
     <!-- wp:group -->
--- patterns/page-services-why.php
+++ patterns/page-services-why.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group -->
-<div class="wp-block-group why pt-[150px] overflow-hidden">
+<div id="Dlaczego" class="wp-block-group why pt-[150px] overflow-hidden">
   <!-- wp:group -->
   <div
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
