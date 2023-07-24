5ad9f9889e3459e4afc21166e5cb0790a9110142
Tomek <TK-54-4B>
2023-07-24 12:11:42 +0200

    single ulsugi test

---
+++ patterns/single-uslugi.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Title: single-content
+ * Slug: single-content
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group -->
+<div class="wp-block-group single-page-post mb-[150px] overflow-hidden">
+  <!-- wp:group -->
+  <div
+    class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
+  >    
+    <!-- wp:paragraph -->
+    <a href="https://mutiny.webo.design/blog/" class="group mt-[60px] mb-[40px] w-fit font-semibold flex items-center gap-[8px]">
+      <!-- wp:group -->
+      <div class="wp-block-group group w-[56px] h-[56px]">
+        <!-- wp:paragraph -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
+          <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:fill-[#FEC400] group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+          <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+          <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+        </svg>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <p class="uppercase text-[14px]">WRÓĆ DO LISTY</p>
+      <!-- /wp:paragraph -->
+    </a>
+    <!-- /wp:paragraph -->
+
+
+    <!-- wp:group -->
+    <div class="wp-block-group above-img-banner mb-[50px] desktop:flex gap-[50px]">
+      <!-- wp:group -->
+      <div class="wp-block-group post-title desktop:w-[70%] text-[50px] leading-[55px] font-normal">
+        <!-- wp:post-title /-->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+    
+    <!-- wp:group -->
+    <div class="wp-block-group featured-image mb-[100px] w-[1300px]">
+      <!-- wp:post-featured-image /-->
+    </div>
+    <!-- /wp:group -->
+    
+    <!-- wp:group -->
+    <div class="wp-block-group layout-two-cols mb-[50px] desktop:flex gap-[50px]">
+      <!-- wp:group -->
+      <div class="wp-block-group post-content desktop:w-[70%]">
+        <!-- wp:post-content /-->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group border-wrapper pt-[20px] border-opacity-[40%] border-[#D5D5D5] border-t-[2px]">
+      <!-- wp:paragraph -->
+      <a href="https://mutiny.webo.design/blog/" class="group w-fit font-semibold flex items-center gap-[8px]">
+        <!-- wp:group -->
+        <div class="wp-block-group group w-[56px] h-[56px]">
+        <!-- wp:paragraph -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
+            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:fill-[#FEC400] group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
+            <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+            <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+          </svg>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:paragraph -->
+        <p class="uppercase text-[14px]">WRÓĆ DO LISTY</p>
+        <!-- /wp:paragraph -->
+      </a>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+
+
+
