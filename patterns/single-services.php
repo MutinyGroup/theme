94375870b8f970b20a696c541d2f6680555f5a15
Tomek <TK-54-4B>
2023-07-24 14:08:35 +0200

    single ulsugi test

--- patterns/single-services.php
+++ patterns/single-services.php
@@ -11,33 +11,23 @@
 <div class="wp-block-group single-page-post mb-[150px] overflow-hidden">
   <!-- wp:group -->
   <div
-    class="wp-block-group mx-auto relative"
+    class="wp-block-group relative"
   >    
     <!-- wp:group -->
-    <div class="wp-block-group above-img-banner mb-[50px] desktop:flex gap-[50px]">
+    <div
+      class="wp-block-group title-wrapper relative container mx-auto desktop:px-0 px-[20px] flex"
+    >
       <!-- wp:group -->
       <div
-        class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] pt-[90px]"
+      class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
       >
-        <!-- wp:group -->
-        <div
-          class="wp-block-group title-wrapper relative container mx-auto flex"
-        >
-          <!-- wp:group -->
-          <div
-          class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
-          >
-            <!-- wp:paragraph -->
-            <p class="justify-self-end pt-[3px] pr-[15px]">Usługi</p>
-            <!-- /wp:paragraph -->
-          </div>  
-          <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-      </div>
+        <!-- wp:paragraph -->
+        <p class="justify-self-end pt-[3px] pr-[15px]">Usługi</p>
+        <!-- /wp:paragraph -->
+      </div>  
       <!-- /wp:group -->
     </div>
-    <!-- /wp:group -->
+    <!-- /wp:group --> 
     
     <!-- wp:group -->
     <div class="wp-block-group featured-image mb-[100px] w-[1300px]">
@@ -46,7 +36,7 @@
     <!-- /wp:group -->
     
     <!-- wp:group -->
-    <div class="wp-block-group post-content">
+    <div class="wp-block-group post-content container mx-auto desktop:px-0 px-[20px]">
       <!-- wp:post-content /-->
     </div>
     <!-- /wp:group -->
