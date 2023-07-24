2bff71e7f66187dfdea2229f5dacd4caa2f71083
Tomek <TK-54-4B>
2023-07-24 10:25:54 +0200

    swiper test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -80,10 +80,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
       disableOnInteraction: false,
     },
     breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
       768: {
         slidesPerView: 4,
         spaceBetween: 40,
