4a04d65318fe02e0ecd26d8ade18880df66296cb
Tomek <TK-54-4B>
2023-07-24 14:12:49 +0200

    single ulsugi test

--- patterns/single-services.php
+++ patterns/single-services.php
@@ -14,22 +14,6 @@
     class="wp-block-group relative"
   >    
     <!-- wp:group -->
-    <div
-      class="wp-block-group title-wrapper relative container mx-auto desktop:px-0 px-[20px] flex"
-    >
-      <!-- wp:group -->
-      <div
-      class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
-      >
-        <!-- wp:paragraph -->
-        <p class="justify-self-end pt-[3px] pr-[15px]">Usługi</p>
-        <!-- /wp:paragraph -->
-      </div>  
-      <!-- /wp:group -->
-    </div>
-    <!-- /wp:group --> 
-    
-    <!-- wp:group -->
     <div class="wp-block-group featured-image mb-[100px] w-[1300px]">
       <!-- wp:post-featured-image /-->
     </div>
