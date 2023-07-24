152a48f6948c5dd71c44dc305815439a7415c6ca
Tomek <TK-54-4B>
2023-07-24 09:45:13 +0200

    swiper test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -79,6 +79,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
       delay: 2000,
       disableOnInteraction: false,
     },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        noSwiping: true,
+        slidesPerView: 'auto'
+        // slidesPerGroup: 2,
+      },
+    },
   });
 
   new Swiper('#opinions-dark', {
