dd71c78b2bd3d91bcc755c6134378704e7a00e4e
Tomek <TK-54-4B>
2023-07-24 13:58:44 +0200

    single ulsugi test

--- patterns/single-services.php
+++ patterns/single-services.php
@@ -16,12 +16,6 @@
     <!-- wp:group -->
     <div class="wp-block-group above-img-banner mb-[50px] desktop:flex gap-[50px]">
       <!-- wp:group -->
-      <div class="wp-block-group post-title desktop:w-[70%] text-[50px] leading-[55px] font-normal">
-        <!-- wp:post-title /-->
-      </div>
-      <!-- /wp:group -->
-
-      <!-- wp:group -->
       <div
         class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] pt-[90px]"
       >
@@ -29,7 +23,6 @@
         <div
           class="wp-block-group title-wrapper relative container mx-auto flex"
         >
-
           <!-- wp:group -->
           <div
           class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
@@ -39,8 +32,7 @@
             <!-- /wp:paragraph -->
           </div>  
           <!-- /wp:group -->
-
-
+          
           <!-- wp:group -->
           <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[24px] desktop:leading-[60px] leading-[30px]">
             <!-- wp:paragraph -->
@@ -56,7 +48,6 @@
         <!-- /wp:group -->
       </div>
       <!-- /wp:group -->
-
     </div>
     <!-- /wp:group -->
     
@@ -69,29 +60,6 @@
     <!-- wp:group -->
     <div class="wp-block-group post-content">
       <!-- wp:post-content /-->
-    </div>
-    <!-- /wp:group -->
-
-    <!-- wp:group -->
-    <div class="wp-block-group border-wrapper pt-[20px] border-opacity-[40%] border-[#D5D5D5] border-t-[2px]">
-      <!-- wp:paragraph -->
-      <a href="https://mutiny.webo.design/blog/" class="group w-fit font-semibold flex items-center gap-[8px]">
-        <!-- wp:group -->
-        <div class="wp-block-group group w-[56px] h-[56px]">
-        <!-- wp:paragraph -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
-            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:fill-[#FEC400] group-hover:stroke-primaryYellow transition-stroke ease-out duration-500"/>
-            <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-            <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-          </svg>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:paragraph -->
-        <p class="uppercase text-[14px]">WRÓĆ DO LISTY</p>
-        <!-- /wp:paragraph -->
-      </a>
-      <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
   </div>
