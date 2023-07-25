cca029f4e5d1c648dc488c17ee6d7505e71b8855
Tomek <TK-54-4B>
2023-07-25 12:25:10 +0200

    bg shape fix

--- patterns/page-services-marketing-content4.php
+++ patterns/page-services-marketing-content4.php
@@ -112,10 +112,10 @@
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
         <p class="desktop:block text-[16px] leading-[21px]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
         </p>
         <!-- /wp:paragraph -->
       </div>
