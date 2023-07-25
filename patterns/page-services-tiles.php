60a5f4c09a3f2af84a1db7822ed8134b62005a73
Tomek <TK-54-4B>
2023-07-25 16:01:28 +0200

    services mobile view

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -167,7 +167,7 @@
                     >
                       <!-- wp:paragraph -->
                       <a href="<?php the_permalink(); ?>"
-                        class="mr-[20px] desktop:text-[35px] text-[18px] font-semibold mb-[15px] underline-offset-[9px] group-hover:underline group-hover:decoration-primaryYellow w-[470px] absolute bottom-[130px]"
+                        class="mr-[20px] desktop:text-[35px] text-[18px] font-semibold mb-[15px] underline-offset-[9px] group-hover:underline group-hover:decoration-primaryYellow w-[470px] desktop:block absolute bottom-[140px]"
                       >
                         <?php the_title(); ?>
                       </a>
