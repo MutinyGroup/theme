37e7c6ab85a7bb7c8c265aa8be69224913ae71f8
Tomek <TK-54-4B>
2023-07-25 16:27:41 +0200

    services mobile view

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -218,7 +218,6 @@
                       class="inline-block flex desktop:block"
                       >
                         Dowiedz się więcej
-                        <!-- /wp:paragraph -->
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           width="36"
