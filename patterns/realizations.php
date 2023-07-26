31f9ba7995b99112a90f5dd0dfad60261f20f026
Tomek <TK-54-4B>
2023-07-26 15:17:43 +0200

    front page realizations

--- patterns/realizations.php
+++ patterns/realizations.php
@@ -104,7 +104,9 @@
                 <!-- wp:list-item -->
                 <li class="wp-block-group swiper-slide img-clip-path-rounded relative group desktop:w-[545px] w-[100%] desktop:h-[600px] h-[320px] <?php if (($counter % 2)) echo ''; else echo 'desktop:translate-y-[50px]'; ?>">                
                   <!-- wp:group -->
+                  <!-- wp:paragraph -->
                   <?php the_title(); ?>
+                  <!-- /wp:paragraph -->
                 </li>
                 <!-- /wp:list-item -->                              
               <?php 
