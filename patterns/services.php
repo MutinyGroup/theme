f71de2da311f357b66dce3645761605ce12ff764
Tomek <TK-54-4B>
2023-07-26 12:54:30 +0200

    services swiper dynamic data

--- patterns/services.php
+++ patterns/services.php
@@ -121,7 +121,7 @@
                     <!-- wp:group -->
                     <div class="wp-block-group img-clip-path-wrapper w-full h-full rounded-tr-[20px] rounded-br-[20px] overflow-hidden absolute left-0 top-0 z-[-1]">
                       <!-- wp:image -->
-                      <figure class="wp-block-image img-clip-path-services">
+                      <figure class="wp-block-image img-clip-path-services h-full">
                         <img src="<?php echo $postImageUrl[0]; ?>" alt="services-backgroun-image">
                       </figure>
                       <!-- /wp:image -->
