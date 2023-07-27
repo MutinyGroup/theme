b0ac941294fef6d16eb5da146e9a1a733e3ed502
Tomek <TK-54-4B>
2023-07-27 11:16:27 +0200

    services slides order

--- patterns/services.php
+++ patterns/services.php
@@ -98,7 +98,7 @@
             $args = array(
                 'post_type' => 'uslugi',
                 'posts_per_page' => 6,
-                'order' => 'ASC'
+                'order' => 'DESC'
             );
             $post_query = new WP_Query($args);
             $counter = 0;
