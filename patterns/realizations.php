995021daa7b94e83805d96c5302482dc36eebfce
Tomek <TK-54-4B>
2023-07-27 10:58:39 +0200

    front page categories fix

--- patterns/realizations.php
+++ patterns/realizations.php
@@ -133,7 +133,7 @@
                   <!-- wp:group -->
                   <div class="wp-block-group tile-title absolute bottom-[50px] desktop:left-[50px] left-[20px]">
                     <!-- wp:paragraph -->
-                    <p class="desktop:text-[35px] text-[30px] desktop:mb-[20px] mb-[10px] leading-[42px] font-semibold group-hover:border-b-[2px] group-hover:border-primaryYellow desktop:group-hover:translate-y-[-50px] group-hover:translate-y-[-70px] transition ease-in-out duration-[400ms] cursor-pointer">
+                    <p class="w-fit desktop:text-[35px] text-[30px] desktop:mb-[20px] mb-[10px] leading-[42px] font-semibold group-hover:border-b-[2px] group-hover:border-primaryYellow desktop:group-hover:translate-y-[-50px] group-hover:translate-y-[-70px] transition ease-in-out duration-[400ms] cursor-pointer">
                       <a href="<?php the_permalink(); ?>">
                         <?php the_title(); ?>
                       </a>
