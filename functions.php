cc38a18f5fdc75f01352855d53529b2e234c67f9
Tomek <TK-54-4B>
2023-07-27 13:26:28 +0200

    test

--- functions.php
+++ functions.php
@@ -369,3 +369,34 @@ function custom_post_types(){
   ));
 };
 add_action('init', 'custom_post_types');
+
+add_filter( 'kdmfi_featured_images', function( $featured_images ) {
+  // Add featured-image-2 to pages and posts
+  $args_1 = array(
+    'id' => 'featured-image-2',
+    'desc' => 'Your description here.',
+    'label_name' => 'Featured Image 2',
+    'label_set' => 'Set featured image 2',
+    'label_remove' => 'Remove featured image 2',
+    'label_use' => 'Set featured image 2',
+    'post_type' => array( 'page', 'post' ),
+  );
+
+  // Add featured-image-2 to pages only
+  $args_2 = array(
+    'id' => 'featured-image-3',
+    'desc' => 'Your description here.',
+    'label_name' => 'Featured Image 3',
+    'label_set' => 'Set featured image 3',
+    'label_remove' => 'Remove featured image 3',
+    'label_use' => 'Set featured image 3',
+    'post_type' => array( 'page' ),
+  );
+
+  // Add the featured images to the array, so that you are not overwriting images that maybe are created in other filter calls
+  $featured_images[] = $args_1;
+  $featured_images[] = $args_2;
+
+  // Important! Return all featured images
+  return $featured_images;
+});
