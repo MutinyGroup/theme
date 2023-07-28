591ae180ee2bab3555d0b8a4df6113d2987df0bc
Tomek <TK-54-4B>
2023-07-28 14:18:44 +0200

    test

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -34,7 +34,7 @@
               class="w-fit h-[40px] px-[10px] bg-[#F2F2F2] border border-[#AAA] border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer"
             >
               <!-- wp:paragraph -->
-              <a href="<?php echo get_term_link($term->slug, 'category'); ?>" class="flex items-center w-full h-full">
+              <a href="<?php echo get_category_link( $cat->term_id ) ?>" class="flex items-center w-full h-full">
                 <?php echo $cat->name; ?>
               </a>
               <!-- /wp:paragraph -->
