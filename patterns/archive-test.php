49a22b08708cb1bffd8f6f7b77f2283ebc0f8d9e
Tomek <TK-54-4B>
2023-07-28 09:23:45 +0200

    test

--- patterns/archive-test.php
+++ patterns/archive-test.php
@@ -10,44 +10,19 @@
 <!-- wp:grour -->
 <div id="content" class="wp-block-group narrowcolumn">
 
-<?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post(); ?>
-    <!-- wp:group -->
-    <div class="wp-block-group post">
-      <!-- wp:paragraph -->
-      <p id="post-<?php the_ID(); ?>">
-        <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-      <p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
-  <?php endwhile; ?>
-</div>
-<!-- /wp:group -->
-
-
-
-<!-- wp:group -->
-<div class="wp-block-group archive-test desktop:mb-[150px]">
-  <!-- wp:group -->
-  <div
-    class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]"
-  >
-  <p>test</p>
-  <h1 class="page-title"><?php
-    printf( __( 'Category Archives: %s', 'twentyten' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-?></h1>
-  <?php
-   echo single_cat_title();
-   echo single_term_title();
-
-    single_cat_title();
-    single_term_title();
-
-    print_r(single_cat_title());
-    print_r(single_term_title());
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <!-- wp:group -->
+      <div class="wp-block-group post">
+        <!-- wp:paragraph -->
+        <p id="post-<?php the_ID(); ?>">
+          <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+        <p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    <?php endwhile; 
   ?>
-  </div>
-  <!-- /wp:group -->
+  
 </div>
 <!-- /wp:group -->
