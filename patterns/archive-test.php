28a797b615908eb3ab262bd4bbf7a021dcfe6f4c
Tomek <TK-54-4B>
2023-07-28 09:56:06 +0200

    test

--- patterns/archive-test.php
+++ patterns/archive-test.php
@@ -16,7 +16,7 @@
       $args = array(
         'posts_per_page' => 6,
         'order' => 'DESC',
-        'cat' => single_cat_title( '', false ),
+        'category' => single_cat_title( '', false ),
       );
       $post_query = new WP_Query($args);
       if($post_query->have_posts() ) {
