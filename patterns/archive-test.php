69b4a51f6363e8f369b55729ae1968632859798d
Tomek <TK-54-4B>
2023-07-28 09:19:09 +0200

    test

--- patterns/archive-test.php
+++ patterns/archive-test.php
@@ -7,29 +7,20 @@
  */
 ?>
 
- <div id="content" class="narrowcolumn">
+<!-- wp:grour -->
+<div id="content" class="narrowcolumn">
 
-  <?php if (have_posts()) : ?>
-   <?php while (have_posts()) : the_post(); ?>
-     <div class="post">
-     <h2 id="post-<?php the_ID(); ?>">
-        <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-     <small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
-      </div>
-    <?php endwhile; ?>
-<div class="navigation">
-<div class="alignleft">
-<?php posts_nav_link('','','&laquo; Previous Entries') ?>
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <div class="post">
+    <h2 id="post-<?php the_ID(); ?>">
+      <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+    <small><?php the_time('F jS, Y') ?></small>
+    </div>
+  <?php endwhile; ?>
 </div>
-<div class="alignright">
-<?php posts_nav_link('','Next Entries &raquo;','') ?>
-</div>
-  </div>
-<?php else : ?>
-  <h2 class="center">Not Found</h2>
- <p class="center"><?php _e("Sorry, but you are looking for something that isn't here."); ?></p>
-  <?php endif; ?>
-</div>
+<!-- /wp:group -->
+
 
 
 <!-- wp:group -->
