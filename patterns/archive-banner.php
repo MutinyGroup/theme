4e37a7e7c1281257f089533613e7f128e83dcaa7
Tomek <TK-54-4B>
2023-07-28 16:24:26 +0200

    test

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -61,7 +61,7 @@
   </div>
   <!-- /wp:group -->
   <!-- wp:group -->
-  <div class="wp-block-group about-banner-img relative h-[500px] desktop:ml-[80px]">
+  <div class="wp-block-group banner-img relative h-[500px] desktop:ml-[80px] hidden">
     <!-- wp:group -->
     <div class="wp-block-group overflow-hidden w-full h-full">
       <!-- wp:image -->
