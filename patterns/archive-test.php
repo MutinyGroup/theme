1e66470ebd11c980246e4ee98bd1bd0e6bc1475f
Tomek <TK-54-4B>
2023-07-28 09:20:25 +0200

    test

--- patterns/archive-test.php
+++ patterns/archive-test.php
@@ -8,15 +8,19 @@
 ?>
 
 <!-- wp:grour -->
-<div id="content" class="narrowcolumn">
+<div id="content" class="wp-block-group narrowcolumn">
 
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post(); ?>
-    <div class="post">
-    <h2 id="post-<?php the_ID(); ?>">
-      <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-    <small><?php the_time('F jS, Y') ?></small>
+    <!-- wp:group -->
+    <div class="wp-block-group post">
+      <!-- wp:paragraph -->
+      <p id="post-<?php the_ID(); ?>">
+        <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+      <p>
+      <!-- /wp:paragraph -->
     </div>
+    <!-- /wp:group -->
   <?php endwhile; ?>
 </div>
 <!-- /wp:group -->
