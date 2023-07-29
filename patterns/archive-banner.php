e350474590d81ffa1710917d609e2d93ef12ad7d
Tomek <TK-54-4B>
2023-07-29 17:37:19 +0200

    archrive banner title

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -45,6 +45,7 @@
                 global $wp;
                 $wp->parse_request();
                 $currentCategory = ($wp->query_vars)['category_name'];
+                echo $currentCategory;
               ?>
               <span class="desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></span
             >
