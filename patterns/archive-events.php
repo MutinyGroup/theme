428f783b43e91d38bcfd69c4dccf25d925ae0aea
Tomek <TK-54-4B>
2023-07-29 15:09:24 +0200

    test

--- patterns/archive-events.php
+++ patterns/archive-events.php
@@ -18,7 +18,8 @@
         global $wp;
         $wp->parse_request();
         $current_url = home_url($wp->request);
-        $currentCategory = substr(parse_url($current_url )['path'], 10, 30);
+        $currentCategory = substr(substr(parse_url($current_url )['path'], 10, 30),0,-1);
+        echo $currentCategory;
 
         $args = array(
           'taxonomy' => 'category',
@@ -35,7 +36,7 @@
           }else{ ?>
             <!-- wp:list-item -->
             <li
-              class="w-fit h-[40px] test px-[10px] bg-[#F2F2F2] border border-[#AAA] border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer <?php if ($cat->name == substr($currentCategory,0,-1)) echo 'ok';else echo 'nok' ?>"
+              class="w-fit h-[40px] test px-[10px] bg-[#F2F2F2] border border-[#AAA] border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer <?php if ($cat->name == substr($currentCategory,0,-1)) echo 'ok';else echo 'nok2' ?>"
             >
               <!-- wp:paragraph -->
               <a href="<?php echo get_category_link( $cat->term_id ) ?>" class="flex items-center w-full h-full">
