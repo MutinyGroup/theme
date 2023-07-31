dbd6486354872e14ffd7f73b18430ea663cce14e
Tomek <TK-54-4B>
2023-07-31 12:32:16 +0200

    test

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -63,10 +63,10 @@
                 // $currentCategory = parse_url($current_url)['path'];
                 // echo $currentCategory;
 
+                $current_url4 home_url( add_query_arg( null, null ));
+                echo $current_url4;
+
                 $current_url5 = home_url($_SERVER['REQUEST_URI']);
-
-                echo home_url( add_query_arg( null, null ));
-
                 echo $current_url5;
 
                 $current_url6 = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
