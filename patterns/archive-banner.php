db7f05f9996dab2319ecb59bdbbb25e7198a771f
Tomek <TK-54-4B>
2023-07-31 08:25:48 +0200

    url test

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -45,7 +45,7 @@ echo home_url( $wp->request )
               class="relative desktop:inline-block block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
               >
               <?php
-                global $wp
+                global $wp;
                 $wp->parse_request;
                 $currentCategory = str_replace('-',' ',($wp->query_vars)['category_name']);
                 echo $currentCategory;
