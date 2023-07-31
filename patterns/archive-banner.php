3cfec821a0d3d7a5008062d1b6e36657e9f68fc5
Tomek <TK-54-4B>
2023-07-31 08:15:44 +0200

    url test

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -56,7 +56,7 @@ echo home_url( $wp->request )
                 echo $currentCategory;
                 echo '<br>';
                 global $wp;
-                $current_url = home_url(add_query_arg(array(), $wp->request));
+                $current_url = home_url(add_query_arg($_GET,$wp->request));
                 echo $current_url;
               ?>
               <span class="desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></span
