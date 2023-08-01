2eadbba13858b8af0eb3e854bb25dd19b32eceee
Tomek <TK-54-4B>
2023-08-01 16:27:23 +0200

    all categories test

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -34,7 +34,7 @@
           }else{ ?>
             <!-- wp:list-item -->
             <li
-              class="w-fit h-[40px] px-[10px] bg-[#F2F2F2] border border-[#AAA] border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer"
+              class="w-fit test h-[40px] px-[10px] bg-[#F2F2F2] border border-[#AAA] border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer"
             >
               <!-- wp:paragraph -->
               <a href="<?php echo get_category_link( $cat->term_id ) ?>" class="flex items-center w-full h-full">
