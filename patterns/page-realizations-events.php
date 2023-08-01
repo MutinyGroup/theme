9d28cc09fcf5173bbacacfdbaa94f883af835774
Tomek <TK-54-4B>
2023-08-01 16:56:13 +0200

    cleaning

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -17,7 +17,7 @@
     <ul class="wp-block-list categories-all-1 mb-[108px] flex flex-wrap gap-[10px]">
       <!-- wp:list-item -->
       <li
-        class="w-fit test1 h-[40px] px-[10px] bg-[#F2F2F2] border border-[#AAA] border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center hover:bg-primaryYellow hover:border-primaryYellow transition ease-out duration-300 cursor-pointer"
+        class="w-fit test1 h-[40px] px-[10px] bg-primaryYellow border border-primaryYellow border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center transition ease-out duration-300 cursor-pointer"
       >
         <!-- wp:paragraph -->
         <a href="#" class="flex items-center w-full h-full">
