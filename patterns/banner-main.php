b986485621b05cab12573325cb572cb1ba962baf
Tomek <TK-54-4B>
2023-08-02 11:54:30 +0200

    banner fixing

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -14,7 +14,7 @@
     <!-- wp:group -->
     <div class="wp-block-group content-with-bg-wrapper">
       <!-- wp:group -->
-      <div class="wp-block-group banner-title relative desktop:mb-[89px] mb-[45px] desktop:pt-[130px] pt-0 uppercase fullhd:text-[85px] desktop:text-[85px] text-[38px] font-normal desktop:w-[940px] wide:leading-[85px] desktop:leading-[80px] leading-[38px]">
+      <div class="wp-block-group banner-title relative desktop:mb-[89px] mb-[45px] desktop:pt-[110px] pt-0 uppercase fullhd:text-[85px] desktop:text-[85px] text-[38px] font-normal desktop:w-[940px] wide:leading-[85px] desktop:leading-[80px] leading-[38px]">
         <!-- wp:paragraph -->
         <svg class="flash desktop:scale-[1] scale-[0.5] absolute desktop:top-[-40px] top-[-350px] desktop:left-[-140px] right-[-90px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
           <g filter="url(#filter0_f_1787_1988)">
