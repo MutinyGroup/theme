19d0f9107181310f75be11b74a0894ef71900f1c
Tomek <TK-54-4B>
2023-08-02 10:10:22 +0200

    swiper fix

--- patterns/services.php
+++ patterns/services.php
@@ -90,7 +90,7 @@
     <div class="wp-block-group desktop:mt-[-120px] w-full overflow-hidden">
 
       <!-- wp:group -->
-      <div id="services" class="wp-block-group swiper swiperServices w-[calc(100%-150px)] relative desktop:pt-[180px] absolute top-0 desktop:left-[150px] left-[20px]">
+      <div id="services" class="wp-block-group swiper swiperServices w-[calc(100%-150px)] relative desktop:pt-[180px] absolute top-0 desktop:left-[70px] left-[20px]">
 
         <!-- wp:group -->
         <div class="wp-block-group swiper-wrapper desktop:h-[480px]">
