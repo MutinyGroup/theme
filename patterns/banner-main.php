34753797a107610cdc0c094fe92a95eff6d1a3d4
Tomek <TK-54-4B>
2023-08-02 13:03:39 +0200

    banner fixing

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -77,7 +77,6 @@
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore <span class="text-primaryYellow">magna aliqua.</span>
         </p>
         <!-- /wp:paragraph -->
-
       </div>
       <!-- /wp:group -->
 
@@ -97,7 +96,7 @@
     <!-- /wp:group -->
   
     <!-- wp:paragraph -->
-    <a href="#about" class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#D5D5D5] absolute bottom-[110px] desktop:bottom-auto desktop:top-[550px] wide:top-[65%] desktop:left-[56%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer is-layout-flex">
+    <a href="#about" class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#D5D5D5] absolute bottom-[110px] desktop:bottom-auto desktop:top-[55%] wide:top-[65%] desktop:left-[56%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer is-layout-flex">
       <!-- wp:paragraph -->
       <p>Zobacz</p>
       <!-- /wp:paragraph -->
