f0330ee481d1f54d4f523d39a6acaff60e0e5207
Tomek <TK-54-4B>
2023-08-03 11:19:54 +0200

    link fix

--- patterns/page-realizations-banner.php
+++ patterns/page-realizations-banner.php
@@ -69,8 +69,9 @@
     <!-- /wp:group -->
 
 
-    <!-- wp:buttons -->
-    <div
+    <!-- wp:paragraph -->
+    <a
+      href="#Eventy"
       class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[260px] wide:left-[70%] desktop:left-[80%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
     >
       <!-- wp:paragraph -->
@@ -79,8 +80,8 @@
       <!-- wp:paragraph -->
       <p class="uppercase text-[25px] font-extrabold">WIĘCEJ</p>
       <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:buttons -->
+    </a>
+    <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
 </div>
