77b5a95b9c8cf4f384c85a58ab7ef9dac30410dd
Tomek <TK-54-4B>
2023-08-03 15:18:17 +0200

    test

--- patterns/archive-events.php
+++ patterns/archive-events.php
@@ -104,7 +104,7 @@
                 <!-- /wp:group -->
                 
                 <!-- wp:group -->
-                <div class="wp-block-group content-wrapper desktop:w-[60%]">
+                <div class="wp-block-group content-wrapper desktop:w-[60%] flex flex-col justify-center">
                   <!-- wp:image -->
                   <figure
                     class="wp-block-image event-logo-color mb-[25px] w-[60px]"
