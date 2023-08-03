53c134276e1ab7a491a1fa5ab2cef23903de6917
Tomek <TK-54-4B>
2023-08-03 16:48:11 +0200

    test

--- patterns/page-about-banner-img-full-width.php
+++ patterns/page-about-banner-img-full-width.php
@@ -31,26 +31,18 @@
         </div>  
         <!-- /wp:group -->
         <!-- wp:group -->
-        <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[24px] desktop:leading-[60px] leading-[30px]">
+        <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
           <!-- wp:paragraph -->
           <p
-          class="uppercase"
+            class=""
           >
-            Nie boimy się
-          </p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="inline">
+            Nie boimy sie <br />
+          
             przekraczać
-          </p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-            <p
-              class="relative inline desktop:text-[60px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
+            <span
+              class="relative desktop:inline-block block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
+              >granic<span class="desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></span
             >
-              granic.
-            </p>
-            <!-- /wp:paragraph -->
           </p>
           <!-- /wp:paragraph -->
         </div>
@@ -59,9 +51,36 @@
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-
-
-
-
+  </div>
+  <!-- /wp:group -->
+  <!-- wp:group -->
+  <div class="wp-block-group about-banner-img relative h-[500px] desktop:ml-[80px]">
+    <!-- wp:group -->
+    <div class="wp-block-group overflow-hidden w-full h-full">
+      <!-- wp:image -->
+      <figure class="wp-block-image h-full w-[920px] desktop:w-auto">
+        <img
+          src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-about/about-banner-bg.jpg"
+          alt="bg-image"
+        />
+      </figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:group -->
+    <!-- wp:paragraph -->
+    <a
+      href="#Poznaj nas"
+      class="desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[310px] wide:left-[70%] desktop:left-[80%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+    >
+      <!-- wp:paragraph -->
+      <p>Zobacz</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="uppercase text-[25px] font-extrabold">WIĘCEJ</p>
+      <!-- /wp:paragraph -->
+    </a>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
