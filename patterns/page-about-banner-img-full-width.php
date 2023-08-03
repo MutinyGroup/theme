8257bad01875205cc62e17f2f3ae0363ba4306ca
Tomek <TK-54-4B>
2023-08-03 17:06:34 +0200

    test

--- patterns/page-about-banner-img-full-width.php
+++ patterns/page-about-banner-img-full-width.php
@@ -38,6 +38,7 @@
           <!-- wp:paragraph -->
           <p class="inline">przekraczać</p>
           <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
           <p
             class="relative desktop:inline-block block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
           >
