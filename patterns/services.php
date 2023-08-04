c3a9b745d663148d32b942661eee44192708bffb
Tomek <TK-54-4B>
2023-08-04 17:28:52 +0200

    clip path rename

--- patterns/services.php
+++ patterns/services.php
@@ -120,14 +120,14 @@
                     </div>
                     <!-- /wp:group -->                      
                     <!-- wp:image -->
-                    <figure class="wp-block-image services-swiper-img <?php if($counter == 1) echo 'clipped-services-swiper-start'; elseif($counter == $post_query->post_count) echo 'clipped-services-swiper-end'; ?>  h-full">
+                    <figure class="wp-block-image clip-path-img <?php if($counter == 1) echo 'clipped-services-swiper-start'; elseif($counter == $post_query->post_count) echo 'clipped-services-swiper-end'; ?>  h-full">
                       <img src="<?php echo $postImageUrl[0]; ?>" alt="services-backgroun-image">
                     </figure>
                     <!-- /wp:image -->
                     <!-- wp:paragraph -->
                     <svg width="0" height="0">
                       <defs>
-                        <clipPath id="clipped-services-swiper-start">
+                        <clipPath id="clipped-swiper-serv-1">
                         <path d="M0 88.2843C0 82.9799 2.10714 77.8929 5.85786 74.1421L74.1421 5.85786C77.8929 2.10713 82.9799 0 88.2843 0H334C345.046 0 354 8.95431 354 20V430C354 441.046 345.046 450 334 450H0V88.2843Z"/>
                         </clipPath>
                       </defs>
