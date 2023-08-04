01ec22534ec160b546d58e50a0670e5a656867a0
Tomek <TK-54-4B>
2023-08-04 11:09:11 +0200

    title fix

--- patterns/page-about-banner-img-full-width.php
+++ patterns/page-about-banner-img-full-width.php
@@ -40,7 +40,7 @@
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <p
-            class="relative desktop:inline-block block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
+            class="relative inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
           >
             GRANIC
           </p>
