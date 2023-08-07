a54c8190bdca02ce7b0ebe9fe275adabe522faea
Tomek <TK-54-4B>
2023-08-07 15:09:00 +0200

    hover animation fix

--- patterns/blog.php
+++ patterns/blog.php
@@ -105,7 +105,7 @@
                           <!-- wp:group -->
                           <div class="wp-block-group h-[360px] desktop:h-[550px] overflow-hidden rounded-[20px]">
                             <!-- wp:image -->
-                            <figure class="wp-block-image post-img clipped-blog h-full transition ease-out duration-[700ms]">
+                            <figure class="wp-block-image post-img clipped-blog h-full">
                               <img src="<?php echo $postImageUrl[0]; ?>" alt="post-image">
                             </figure>
                             <!-- /wp:image -->
