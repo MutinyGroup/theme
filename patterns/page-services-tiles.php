20105e1acf8d4ad9a63567969bde4d543f5672c1
Tomek <TK-54-4B>
2023-08-08 12:00:23 +0200

    mobile view fix

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -114,7 +114,7 @@
                 <li class="wp-block-group swiper-slide img-clip-path-rounded relative group desktop:w-[545px] w-[100%] desktop:h-[600px] h-[320px] <?php if (($counter % 2)) echo ''; else echo 'desktop:translate-y-[50px]'; ?>">                
                   <!-- wp:group -->
                   <div
-                    class="wp-block-group group relative gallery-tile <?php if($counter == 1) echo 'clipped-realizations-1 tile-services-1'; elseif($counter == $post_query->post_count) echo 'clipped-realizations-2 tile-services-2'; ?>  z-[1] group desktop:p-[25px] desktop:h-[600px] h-[320px] overflow-hidden rounded-[20px] transition ease-out duration-300 mr-[8px] mb-[30px]"
+                    class="wp-block-group group relative gallery-tile w-full <?php if($counter == 1) echo 'clipped-realizations-1 tile-services-1'; elseif($counter == $post_query->post_count) echo 'clipped-realizations-2 tile-services-2'; ?>  z-[1] group desktop:p-[25px] desktop:h-[600px] h-[320px] overflow-hidden rounded-[20px] transition ease-out duration-300 mr-[8px] mb-[30px]"
                   >
                     <!-- wp:group -->
                     <div
