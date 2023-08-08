a605169bba51c15db6cfde8dc81df6577217a6fe
Tomek <TK-54-4B>
2023-08-08 13:45:10 +0200

    mobile view

--- patterns/cta-bright1.php
+++ patterns/cta-bright1.php
@@ -7,7 +7,7 @@
  */
 ?>
 <!-- wp:group -->
-<div id="CTA" class="wp-block-group relative cta-bright1 desktop:mb-[150px] overflow-hidden desktop:overflow-visible">
+<div id="CTA" class="wp-block-group relative cta-bright1 mb-[120px] desktop:mb-[150px] overflow-hidden desktop:overflow-visible">
   <!-- wp:group -->
   <div class="wp-block-group container group mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[55px] h-[300px] desktop:h-[700px] wide:h-[940px]">
     <!-- wp:group -->
