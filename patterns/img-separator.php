0036559e4526fb974168a1bacf892661c22ddfb5
Tomek <TK-54-4B>
2023-08-10 11:29:49 +0200

    image separator pattern

---
+++ patterns/img-separator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: img-separator
+ * Slug: img-separator
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+<!-- wp:group -->
+<div class="wp-block-image img-separator">
+  <!-- wp:image -->
+  <figure class="wp-block-image img-separator desktop:mb-[150px] mb-[80px] desktop:ml-[80px] desktop:h-auto h-[180px] wide:pt-[100px]">
+    <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/section-separator.jpg" alt="image-page-separator">
+  </figure>
+  <!-- /wp:image -->  
+</div>
+<!-- /wp:group -->
+
--- patterns/services.php
+++ patterns/services.php
@@ -238,13 +238,6 @@
     <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
-
-
-  <!-- wp:image -->
-  <figure class="wp-block-image img-separator desktop:mb-[150px] mb-[80px] desktop:ml-[80px] desktop:h-auto h-[180px] wide:pt-[100px]">
-    <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/section-separator.jpg" alt="image-page-separator">
-  </figure>
-  <!-- /wp:image -->  
 </div>
 <!-- /wp:group -->
 
