c6958da2833966e8e612a1f8c80fe8a76cd78b3d
Tomek <TK-54-4B>
2023-08-10 15:43:25 +0200

    margin removed

--- patterns/opinions.php
+++ patterns/opinions.php
@@ -9,7 +9,7 @@
 <!-- wp:group -->
 <div id="Opinie" class="wp-block-group opinions desktop:mb-[150px] mb-[60px]">
   <!-- wp:group -->
-  <div class="wp-block-group relative mx-auto desktop:ml-[83px] desktop:px-0 px-[20px] overflow-hidden bg-black">
+  <div class="wp-block-group relative mx-auto desktop:px-0 px-[20px] overflow-hidden bg-black">
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper relative container mx-auto desktop:ml-[120px] ml-0 desktop:pt-[100px] desktop:mt-0 mt-[60px] desktop:mb-[80px] flex gap-[20px]">
       <!-- wp:paragraph -->
