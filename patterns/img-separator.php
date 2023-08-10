aa792a93681017657e127100d5fc83748141a036
Tomek <TK-54-4B>
2023-08-10 11:32:18 +0200

    test

--- patterns/img-separator.php
+++ patterns/img-separator.php
@@ -7,9 +7,9 @@
  */
 ?>
 <!-- wp:group -->
-<div class="wp-block-image img-separator">
+<div id="Image separator" class="wp-block-image img-separator">
   <!-- wp:image -->
-  <figure class="wp-block-image img-separator desktop:mb-[150px] mb-[80px] desktop:ml-[80px] desktop:h-auto h-[180px] wide:pt-[100px]">
+  <figure class="wp-block-image img-separator desktop:h-auto h-[180px]">
     <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/front-page/section-separator.jpg" alt="image-page-separator">
   </figure>
   <!-- /wp:image -->  
