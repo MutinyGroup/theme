df755451996aea987a4a9e1e55d4c0f8c6681faa
Tomek <TK-54-4B>
2023-08-11 17:51:40 +0200

    test

--- patterns/page-services-marketing-content3.php
+++ patterns/page-services-marketing-content3.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
-<div class="wp-block-group page-marketing-info">
+<div id="2xKafelki" class="wp-block-group page-marketing-info">
   <!-- wp:group -->
   <div
     class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
