c491112ba4372778fa852622d55a687e4b3908f5
Tomek <TK-54-4B>
2023-08-11 16:38:52 +0200

    clip path adjustments

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -110,7 +110,7 @@
                           </clipPath>
                         </defs>
                       </svg>
-                      <!-- /wp:paragraph -->
+                      <!-- /wp:paragraph --> 
                     </div>
                     <!-- /wp:group -->
 
