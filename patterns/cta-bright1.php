cdc0e4dce2c9d9f6f2b7c2fe4d063afb02fce7e4
Tomek <TK-54-4B>
2023-08-14 11:27:39 +0200

    title fix

--- patterns/cta-bright1.php
+++ patterns/cta-bright1.php
@@ -9,7 +9,7 @@
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
 <div id="CTA" class="wp-block-group relative cta-bright1 mb-[120px] desktop:mb-[150px] overflow-hidden desktop:overflow-visible">
   <!-- wp:group -->
-  <div class="wp-block-group container group mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[55px] h-[300px] desktop:h-[700px] wide:h-[940px]">
+  <div class="wp-block-group container group mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[120px] mt-[55px] h-[300px] desktop:h-[700px] wide:h-[940px]">
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper relative desktop:pt-[110px] pt-[0px] desktop:mb-[80px] mb-[60px]">
       <!-- wp:paragraph -->
