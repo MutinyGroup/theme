7419cb721b6e5757b211a7d3fe31d91a03a5df73
Tomek <TK-54-4B>
2023-08-16 13:21:39 +0200

    cleaning

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -64,21 +64,8 @@
             class="relative inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
           >
             <?php
-              // $currentCategory = str_replace('-',' ',str_replace('/','',substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",40, 30)));
-              // echo ' 1 ' . $currentCategory;
-              // echo '<br>';
-
               $currentCategory2 = str_replace('/','',substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",39, 30));
-
               echo $currentCategory2;
-              // echo '<br>';
-
-              // echo substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",0, 100);
-              // echo '<br>';
-
-              // echo substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",29, 30);
-              // echo '<br>';
-
             ?>
           </p>
           <!-- /wp:paragraph -->
