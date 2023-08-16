2fffcc4f240c31aa757d1f53abdbae7c56298cfb
Tomek <TK-54-4B>
2023-08-16 08:41:17 +0200

    hover fix

--- patterns/page-contact-banner.php
+++ patterns/page-contact-banner.php
@@ -15,7 +15,7 @@
   >
     <!-- wp:group -->
     <div
-      class="wp-block-group title-wrapper relative desktop:mb-[100px] mb-[40px] desktop:pt-[90px]"
+      class="wp-block-group title-wrapper relative group desktop:mb-[100px] mb-[40px] desktop:pt-[90px]"
     >
       <!-- wp:group -->
       <div
@@ -73,7 +73,7 @@
     <!-- wp:paragraph -->
     <a
       href="#Info"
-      class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[260px] wide:left-[70%] desktop:left-[85%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+      class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[260px] wide:left-[70%] desktop:left-[85%] left-[50%] translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
     >
       <!-- wp:paragraph -->
       <p class="font-bold">Skontaktuj się</p>
