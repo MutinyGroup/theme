a25f09d60b12ef095eee50ce2faa3142f1f20d9d
Tomek <TK-54-4B>
2023-08-16 13:18:09 +0200

    test

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -65,12 +65,12 @@
           >
             <?php
               $currentCategory = str_replace('-',' ',str_replace('/','',substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",40, 30)));
-              echo ' 1 ' + $currentCategory;
+              echo ' 1 ' . $currentCategory;
               echo '<br>';
 
               $currentCategory2 = str_replace('/','',substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",39, 30));
 
-              echo ' 2 ' + $currentCategory2;
+              echo ' 2 ' . $currentCategory2;
               echo '<br>';
 
               echo substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",0, 100);
