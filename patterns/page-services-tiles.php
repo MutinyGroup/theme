ef214a6f5a635cef543c1422fa8f558cab798a1d
Tomek <TK-54-4B>
2023-08-16 13:28:48 +0200

    link fix

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -253,10 +253,8 @@
                   <div
                     class="wp-block-buttons btn-swiper-more group z-[2] pr-[20px] desktop:pr-0 text-[14px] font-semibold uppercase absolute desktop:bottom-[40px] desktop:bottom-[75px] bottom-[20px] desktop:left-[25px] left-[20px] transition ease-out duration-300"
                   >
-                    <!-- wp:group -->
-                    <div
-                      class="wp-block-group inline-block flex items-center desktop:block"
-                    >
+                    <!-- wp:paragraph -->
+                    <a href="<?php the_permalink(); ?>">
                       <!-- wp:group -->
                       <div class="wp-block-group inline">
                         <!-- wp:paragraph -->
@@ -296,8 +294,8 @@
                         />
                       </svg>
                       <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:group -->
+                    </a>
+                    <!-- /wp:paragraph -->
                   </div>
                   <!-- /wp:buttons -->                   
                 </li>
