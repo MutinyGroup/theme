f3a6c68d5aa1394ddd730128fac815938e20210f
Tomek <TK-54-4B>
2023-08-17 13:35:07 +0200

    wp tags fix

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -118,10 +118,10 @@
     <!-- /wp:group -->
   
     <!-- wp:group -->
-    <div class="wp-block-group btn-more">
+    <div class="wp-block-group btn-more desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full desktop:border-[4px] border-[3px] border-[#D5D5D5] desktop:absolute bottom-[200px] desktop:bottom-auto desktop:top-[58%] wide:top-[65%] desktop:left-[56%] left-[50%] desktop:translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
       <!-- wp:paragraph -->
       <p>
-        <a href="#O nas" class="desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full desktop:border-[4px] border-[3px] border-[#D5D5D5] desktop:absolute bottom-[200px] desktop:bottom-auto desktop:top-[58%] wide:top-[65%] desktop:left-[56%] left-[50%] desktop:translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer is-layout-flex">
+        <a href="#O nas" class="">
           <!-- wp:paragraph -->
           <p class="font-bold">Zobacz</p>
           <!-- /wp:paragraph -->
