50ba93ff737c4a8338926a1c5765a572bdcb0f5e
Tomek <TK-54-4B>
2023-08-17 09:53:32 +0200

    test

--- patterns/services.php
+++ patterns/services.php
@@ -89,10 +89,10 @@
     <div class="wp-block-group mt-[80px] swiper-container h-[500px] w-full overflow-hidden">
 
       <!-- wp:group -->
-      <div id="services" class="wp-block-group swiper swiperServices desktop:w-[calc(100%-90px)] desktop:h-[450px] absolute top-0 desktop:top-[60px] desktop:left-[45px] left-[20px]">
+      <div id="services" class="wp-block-group swiper swiperServices desktop:w-[calc(100%-90px)] desktop:h-[450px] wide:h-[530px] absolute top-0 desktop:top-[60px] desktop:left-[45px] left-[20px]">
 
         <!-- wp:group -->
-        <div class="wp-block-group swiper-wrapper desktop:h-[480px] wide:h-[530px]">
+        <div class="wp-block-group swiper-wrapper h-full">
           <?php
             $args = array(
               'post_type' => 'uslugi',
