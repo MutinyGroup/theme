b1602556e1894946b49a3533072c90e699450764
Tomek <TK-54-4B>
2023-08-18 14:06:14 +0200

    width fix

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -19,8 +19,10 @@
       <li
         class="w-fit h-[40px] px-[10px] bg-primaryYellow border-primaryYellow border-[2px] rounded-full uppercase text-[14px] font-semibold leading-[20px] flex justify-center items-center transition ease-out duration-300 cursor-pointer"
       >
-        <a href="<?php echo get_home_url() . '/realizations'; ?>" class="flex items-center w-full h-full">
-          Wszystkie realizacje
+        <a href="<?php echo get_home_url() . '/realizations'; ?>" class="flex items-center justify-center w-full h-full">
+          <!-- wp:paragraph -->
+          <p>Wszystkie realizacje</p>
+          <!-- /wp:paragraph -->
         </a>
       </li>
       <!-- /wp:list-item --> 
