0b57c0da1ad3d4d15dbb2bce1b1aa20cfd0790c1
Tomek <TK-54-4B>
2023-08-18 15:53:26 +0200

    style fix

--- patterns/cta.php
+++ patterns/cta.php
@@ -58,7 +58,7 @@
         <p class="uppercase inline desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">o potrzebach</p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph {"templateLock":"contentOnly"} -->
-        <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">Twojej marki</p>
+        <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[130px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">Twojej marki</p>
         <!-- /wp:paragraph -->      
       </div>
       <!-- /wp:group -->
