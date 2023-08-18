ec606f4a5bf00512faf9e2e692132d67cf837015
Tomek <TK-54-4B>
2023-08-18 17:48:30 +0200

    for editor styles

--- patterns/page-blog-banner.php
+++ patterns/page-blog-banner.php
@@ -62,9 +62,7 @@
     <!-- wp:group -->
     <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
       <!-- wp:paragraph -->
-      <p class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px]">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
-      </p>
+      <p class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
