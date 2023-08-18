64026fda166a242c6e0f25b8e366f5f2260faf9e
Tomek <TK-54-4B>
2023-08-18 13:43:50 +0200

    wp:tag fix

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -200,7 +200,11 @@
       <!-- wp:group -->
       <div
         class="wp-block-group gray-border hidden desktop:inline-block w-full h-[2px] bg-[#808080] opacity-[15%]"
-      ></div>
+      >
+        <!-- wp:paragraph -->
+        <p class="hidden">gray separator</p>
+        <!-- /wp:paragraph -->
+      </div>
       <!-- /wp:group -->
 
       <!-- wp:buttons -->
@@ -220,7 +224,11 @@
       <!-- wp:group -->
       <div
         class="wp-block-group gray-border hidden desktop:inline-block w-full h-[2px] bg-[#808080] opacity-[15%]"
-      ></div>
+      >
+        <!-- wp:paragraph -->
+        <p class="hidden">gray separator</p>
+        <!-- /wp:paragraph -->    
+      </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
