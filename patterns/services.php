950ed6b10faeb72eab1c569bdaadbc605501f902
Tomek <TK-54-4B>
2023-08-18 15:26:18 +0200

    title test

--- patterns/services.php
+++ patterns/services.php
@@ -60,13 +60,11 @@
       <!-- wp:group -->
       <div class="wp-block-group uslugi-title desktop:leading-[55px] leading-[30px]">
         <!-- wp:paragraph -->
-        <p class="uppercase desktop:text-[50px] text-[26px]">
-          Kampanie realizujemy
-        </p>
+        <p class="uppercase inline desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">KAMPANIE REALIZUJEMY</p>
         <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p class="relative desktop:text-[50px] text-[26px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[20px] before:mr-[5px] before:bottom-0 before:left-0 desktop:before:w-[120px] before:w-[30px]  desktop:before:h-[3px] before:h-[2px] before:bg-white">Kompleksowo</p>
-        <!-- /wp:paragraph -->
+        <!-- wp:paragraph {"templateLock":"contentOnly"} -->
+        <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">KOMPLEKSOWO</p>
+        <!-- /wp:paragraph -->   
       </div>
       <!-- /wp:group -->
     </div>
