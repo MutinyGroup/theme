3c3e0a520b4a3169a6bfbbeec543a9ccec3174ed
Tomek <TK-54-4B>
2023-08-20 16:55:48 +0200

    banner fix

--- patterns/page-services-banner.php
+++ patterns/page-services-banner.php
@@ -8,42 +8,33 @@
 ?>
 
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
-<div id="Banner" class="wp-block-group services-banner group h-screen desktop:h-auto desktop:mb-[150px]">
+<div id="Banner" class="wp-block-group services-banner group desktop:mb-[150px] pt-[80px] desktop:pt-0">
   <!-- wp:group -->
-  <div
-    class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]"
-  >
+  <div class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]">
     <!-- wp:group -->
-    <div
-      class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] desktop:mt-[90px]"
-    >
+    <div class="wp-block-group title-wrapper relative desktop:mb-[40px] mb-[20px] desktop:mt-[90px]">
       <!-- wp:group -->
-      <div
-        id="title"
-        class="wp-block-group title-wrapper relative container mx-auto flex"
-      >
-
+      <div class="wp-block-group title-wrapper relative container mx-auto flex">
         <!-- wp:group -->
-        <div
-        class="wp-block-group section-title grid desktop:w-[145px] w-[90px] uppercase text-primaryYellow"
-        >
+        <div class="wp-block-group section-title grid desktop:w-[145px] w-[90px] uppercase text-primaryYellow">
           <!-- wp:paragraph -->
-          <p class="justify-self-end pt-[3px] pr-[15px]">Usługi</p>
+          <p class="justify-self-end pt-[3px] pr-[15px]">USŁUGI</p>
           <!-- /wp:paragraph -->
         </div>  
         <!-- /wp:group -->
-
-
         <!-- wp:group -->
-        <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
+        <div class="wp-block-group max-w-[75%] uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
           <!-- wp:paragraph -->
-          <p>Jak możemy</p>
+          <p>JAK MOŻEMY</p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <p class="inline">Ci</p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph {"templateLock":"contentOnly"} -->
           <p class="relative inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">POMÓC</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -54,15 +45,19 @@
   </div>
   <!-- /wp:group -->
   <!-- wp:group -->
-  <div class="wp-block-group about-banner-img23 relative">
-    <!-- wp:image -->
-    <figure class="wp-block-image object-cover h-full">
-      <img
-        src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-services/services-banner-bg.jpg"
-        alt="bg-image"
-      />
-    </figure>
-    <!-- /wp:image -->  
+  <div class="wp-block-group services-banner-img relative h-[500px]">
+    <!-- wp:group -->
+    <div class="wp-block-group overflow-hidden w-full h-full">
+      <!-- wp:image -->
+      <figure class="wp-block-image h-full w-[920px] desktop:w-auto">
+        <img
+          src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-services/services-banner-bg.jpg"
+          alt="bg-image"
+        />
+      </figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:group -->
 
     <!-- wp:group -->
     <div class="wp-block-group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[310px] wide:left-[70%] desktop:left-[85%] left-[50%] translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
@@ -75,9 +70,7 @@
       </p>
       <!-- /wp:paragraph -->
     </div>
-    <!-- /wp:group -->   
-    
-    
+    <!-- /wp:group -->    
   </div>
   <!-- /wp:group -->
 </div>
