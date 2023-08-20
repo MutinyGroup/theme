a49cbb37152d3c292a07c4cd9c10cfa7f2d69027
Tomek <TK-54-4B>
2023-08-20 17:15:28 +0200

    banner fix

--- patterns/page-contact-banner.php
+++ patterns/page-contact-banner.php
@@ -8,46 +8,30 @@
 ?>
 
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
-<div id="Banner" class="wp-block-group contact-banner group h-screen desktop:h-auto desktop:mb-[150px]">
+<div id="Banner" class="wp-block-group contact-banner group desktop:mb-[150px] pt-[80px] desktop:pt-0">
   <!-- wp:group -->
-  <div
-    class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]"
-  >
+  <div class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]">
     <!-- wp:group -->
-    <div
-      class="wp-block-group title-wrapper relative desktop:mb-[100px] mb-[40px] desktop:pt-[90px]"
-    >
+    <div class="wp-block-group title-wrapper relative desktop:mb-[40px] mb-[20px] desktop:mt-[90px]">
       <!-- wp:group -->
-      <div
-        id="title"
-        class="wp-block-group title-wrapper relative container mx-auto flex"
-      >
-
+      <div class="wp-block-group title-wrapper relative container mx-auto flex">
         <!-- wp:group -->
-        <div
-        class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
-        >
+        <div class="wp-block-group section-title grid desktop:w-[145px] w-[90px] uppercase text-primaryYellow">
           <!-- wp:paragraph -->
           <p class="justify-self-end pt-[3px] pr-[15px]">DZIAŁAMY?</p>
           <!-- /wp:paragraph -->
         </div>  
         <!-- /wp:group -->
-
-
         <!-- wp:group -->
-        <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
+        <div class="wp-block-group max-w-[75%] uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
           <!-- wp:paragraph -->
           <p>SPRAWDŹ CO</p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <p class="inline">U NAS</p>
           <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p
-            class="relative inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
-          >
-            SŁYCHAĆ
-          </p>
+          <!-- wp:paragraph {"templateLock":"contentOnly"} -->
+          <p class="relative inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">SŁYCHAĆ</p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span>
@@ -61,28 +45,32 @@
   </div>
   <!-- /wp:group -->
   <!-- wp:group -->
-  <div class="wp-block-group about-banner-img23 relative">
-    <!-- wp:image -->
-    <figure class="wp-block-image object-cover h-full">
-      <img
-        src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-contact/page-contact-banner.jpg"
-        alt="bg-image"
-      />
-    </figure>
-    <!-- /wp:image -->
-    <!-- wp:paragraph -->
-    <a
-      href="#Info"
-      class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[260px] wide:left-[70%] desktop:left-[85%] left-[50%] translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
-    >
+  <div class="wp-block-group contact-banner-img relative h-[500px]">
+    <!-- wp:group -->
+    <div class="wp-block-group overflow-hidden w-full h-full">
+      <!-- wp:image -->
+      <figure class="wp-block-image h-full w-[920px] desktop:w-auto">
+        <img
+          src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-contact/contact-banner-bg.jpg"
+          alt="bg-image"
+        />
+      </figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[310px] wide:left-[70%] desktop:left-[85%] left-[50%] translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
       <!-- wp:paragraph -->
-      <p class="font-bold">Skontaktuj się</p>
+      <p>
+        <a href="#O nas">
+          <span class="font-bold block">Zobacz</span>
+          <span class="font-bold block text-[25px]">WIĘCEJ</span>
+        </a>
+      </p>
       <!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <p class="uppercase text-[25px] font-extrabold">z nami</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->    
   </div>
   <!-- /wp:group -->
 </div>
