88034e90d6d94a628f456fafcc31158faa898327
Tomek <TK-54-4B>
2023-08-20 14:57:26 +0200

    wp:tags fix

--- patterns/page-services-why.php
+++ patterns/page-services-why.php
@@ -18,43 +18,32 @@
       class="wp-block-group title-wrapper relative container mx-auto desktop:mb-[120px] mb-[60px] flex"
     >
       <!-- wp:paragraph -->
-      <svg class="flash desktop:scale-[1] scale-[0.4] absolute desktop:top-[-330px] top-[-360px] desktop:left-[-140px] right-[-100px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
-        <g filter="url(#filter0_f_1787_1988)">
-        <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
-        </g>
-        <defs>
-        <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-        <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
-        </filter>
-        <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#FEC400" stop-opacity="0.6"/>
-        <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
-        </linearGradient>
-        </defs>
-      </svg>
+      <p>
+        <svg class="flash desktop:scale-[1] scale-[0.4] absolute desktop:top-[-330px] top-[-360px] desktop:left-[-140px] right-[-100px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+          <g filter="url(#filter0_f_1787_1988)">
+          <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
+          </g>
+          <defs>
+          <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
+          </filter>
+          <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FEC400" stop-opacity="0.6"/>
+          <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
+          </linearGradient>
+          </defs>
+        </svg>
+      </p>
       <!-- /wp:paragraph -->
       <!-- wp:paragraph -->
-      <p
-        class="section-title desktop:basis-auto desktop:w-[145px] basis-[20%] uppercase text-primaryYellow text-right pr-[20px]"
-      >
-        Nasze <br> podejście
-      </p>
+      <p class="section-title desktop:basis-auto desktop:w-[145px] basis-[20%] uppercase text-primaryYellow text-right pr-[20px]">Nasze <br> podejście</p>
       <!-- /wp:paragraph -->
       <!-- wp:group -->
       <div class="wp-block-group uslugi-title">
         <!-- wp:paragraph -->
-        <p
-          class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"
-        >
-          Dlaczego warto z nami <br />
-          <span
-            class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
-          >
-          Współpracować?</span
-          >
-        </p>
+        <p class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">Dlaczego warto z nami <br /><span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">Współpracować?</span></p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -70,27 +59,26 @@
           <!-- wp:group -->
           <div class="wp-block-group col1 relative w-[40%]">
             <!-- wp:paragraph -->
-            <p class="text-[50px] text-primaryYellow">
-              01 /
-              <span class="text-[20px] text-black font-medium">03</span>
-            </p>
+            <p class="text-[50px] text-primaryYellow">01 / <span class="text-[20px] text-black font-medium">03</span></p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="236"
-              height="317"
-              viewBox="0 0 236 317"
-              fill="none"
-              class="z-[-1] absolute top-[190px] left-[30px] opacity-[80%]"
-            >
-              <path
-                d="M234.242 314.924L1.62109 236.649V1.91746L234.242 80.1919V314.924Z"
-                stroke="white"
-                stroke-width="2"
-              />
-            </svg>
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="236"
+                height="317"
+                viewBox="0 0 236 317"
+                fill="none"
+                class="z-[-1] absolute top-[190px] left-[30px] opacity-[80%]"
+              >
+                <path
+                  d="M234.242 314.924L1.62109 236.649V1.91746L234.242 80.1919V314.924Z"
+                  stroke="white"
+                  stroke-width="2"
+                />
+              </svg>
+            </p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
@@ -101,16 +89,11 @@
               <!-- wp:group -->
               <div class="wp-block-group h-[250px] w-[800px]">
                 <!-- wp:group -->
-                <div
-                  class="wp-block-group img-clip-path-wrapper mb-[50px] w-full h-full rounded-[20px] overflow-hidden"
-                >
+                <div class="wp-block-group img-clip-path-wrapper mb-[50px] w-full h-full rounded-[20px] overflow-hidden">
                   <!-- wp:image -->
-                  <figure
-                    class="wp-block-image h-full clipped-why group-hover:grayscale transition ease-in-out duration-[400ms]"
+                  <figure class="wp-block-image h-full clipped-why group-hover:grayscale transition ease-in-out duration-[400ms]"
                   >
-                    <img
-                      src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-services/why-img1.jpg"
-                      alt="services-background-image"
+                    <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-services/why-img1.jpg" alt="services-background-image"
                     />
                   </figure>
                   <!-- /wp:image -->
@@ -129,50 +112,26 @@
               <!-- /wp:group -->
 
               <!-- wp:paragraph -->
-              <p
-                class="my-[50px] text-[35px] leading-[42px] font-semibold uppercase"
-              >
-                Mamy nieszablonowe <br />
-                podejście
+              <p class="my-[50px] text-[35px] leading-[42px] font-semibold uppercase">Mamy nieszablonowe <br />podejście</p>
+              <!-- /wp:paragraph -->
+              <!-- wp:paragraph -->
+              <p class="mb-[50px] text-[20px] leading-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat.
               </p>
               <!-- /wp:paragraph -->
               <!-- wp:paragraph -->
-              <p class="mb-[50px] text-[20px] leading-[30px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </p>
-              <!-- /wp:paragraph -->
-              <!-- wp:paragraph -->
-              <p class="mb-[50px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniami.
+              <p class="mb-[50px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniami.
               </p>
               <!-- /wp:paragraph -->
               <!-- wp:list -->
               <ul class="wp-block-list leading-[32px] font-semibold">
                 <!-- wp:list-item -->
-                <li
-                  class="before:content-[''] before:inline-block before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow"
-                >
-                  Lorem ipsum dolor sit amet
-                </li>
+                <li class="before:content-[''] before:inline-block before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow">Lorem ipsum dolor sit amet</li>
                 <!-- /wp:list-item -->
                 <!-- wp:list-item -->
-                <li
-                  class="before:content-[''] before:inline-block before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow"
-                >
-                  Consectetur adipiscing elit
-                </li>
+                <li class="before:content-[''] before:inline-block before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow">Consectetur adipiscing elit</li>
                 <!-- /wp:list-item -->
                 <!-- wp:list-item -->
-                <li
-                  class="before:content-[''] before:inline-block before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow"
-                >
-                  Sed do eiusmod tempor incididunt
-                </li>
+                <li class="before:content-[''] before:inline-block before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow">Sed do eiusmod tempor incididunt</li>
                 <!-- /wp:list-item -->
               </ul>
               <!-- /wp:list -->
