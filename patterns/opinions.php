e5861b979afa03fa361ca45d07c58cda0c2229e5
Tomek <TK-54-4B>
2023-08-20 13:58:08 +0200

    wp:tag fix

--- patterns/opinions.php
+++ patterns/opinions.php
@@ -35,11 +35,13 @@
           <!-- wp:group -->
           <div id="opinions-prev" class="wp-block-group group swiper-button-prev w-[56px] h-[56px] ">
             <!-- wp:paragraph -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
-              <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
-              <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-              <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
-            </svg>
+            <p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 group fill-transparent group-hover:fill-white">
+                <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#D5D5D5" stroke-width="3" class="group-hover:stroke-white transition-stroke ease-out duration-500"/>
+                <path d="M28 35L35 28.0189L28.0379 21" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+                <path d="M20 28L31 28" stroke="white" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500"/>
+              </svg>
+            </p>  
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
@@ -63,7 +65,7 @@
         <!-- wp:group -->
         <div id="opinions-pagination" class="wp-block-group swiper-pagination w-fit h-fit text-primaryYellow text-[50px] absolute left-[130px] desktop:left-[200px] wide:left-[190px] top-[-70px] desktop:top-[90px]">
           <!-- wp:paragraph -->
-          <p>Swiper pagination</p>
+          <p class="hidden">Swiper pagination</p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -79,15 +81,16 @@
             <!-- wp:group -->
             <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
               <!-- wp:paragraph -->
-              <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
-                <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
-              </svg>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
+                  <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
+                </svg>
+              </p>
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
             <!-- wp:paragraph -->
-            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <!-- /wp:paragraph -->
             <!-- wp:group -->
             <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
@@ -115,15 +118,16 @@
             <!-- wp:group -->
             <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
               <!-- wp:paragraph -->
-              <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
-                <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
-              </svg>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
+                  <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
+                </svg>
+              </p>
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
             <!-- wp:paragraph -->
-            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <!-- /wp:paragraph -->
             <!-- wp:group -->
             <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
@@ -152,15 +156,16 @@
             <!-- wp:group -->
             <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
               <!-- wp:paragraph -->
-              <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
-                <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
-              </svg>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
+                  <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
+                </svg>
+              </p>
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
             <!-- wp:paragraph -->
-            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <!-- /wp:paragraph -->
             <!-- wp:group -->
             <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
@@ -189,15 +194,16 @@
             <!-- wp:group -->
             <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
               <!-- wp:paragraph -->
-              <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
-                <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
-              </svg>
+              <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="39" height="26" viewBox="0 0 39 26" fill="none">
+                  <path d="M0.08 20.92C5.6 20.36 8.32 17.56 7.92 13H2.96V0.199999H15.92V11C15.92 21.56 10.64 25.8 1.12 25.96L0.08 20.92ZM22.9706 20.92C28.4906 20.36 31.2106 17.56 30.8106 13H25.8506V0.199999H38.8106V11C38.8106 21.56 33.5306 25.8 24.0106 25.96L22.9706 20.92Z" fill="#555555"/>
+                </svg>
+              </p>  
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
             <!-- wp:paragraph -->
-            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <!-- /wp:paragraph -->
             <!-- wp:group -->
             <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
