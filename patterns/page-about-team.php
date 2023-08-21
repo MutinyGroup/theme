cfbf11a283ef4e9922d651e3afee67b45394fbad
Tomek <TK-54-4B>
2023-08-21 12:01:21 +0200

    wide screen tiles adjustments

--- patterns/page-about-team.php
+++ patterns/page-about-team.php
@@ -109,7 +109,7 @@
               <p>
                 <svg width="0" height="0">
                   <defs>
-                    <clipPath id="clipped-swiper-serv-1" class="scale-[0.49] desktop:scale-[0.839]">
+                    <clipPath id="clipped-swiper-serv-1" class="scale-[0.49] desktop:scale-[0.839] wide:scale-[1.2]">
                       <path d="M0 88.2843C0 82.9799 2.10714 77.8929 5.85786 74.1421L74.1421 5.85786C77.8929 2.10713 82.9799 0 88.2843 0H334C345.046 0 354 8.95431 354 20V430C354 441.046 345.046 450 334 450H0V88.2843Z"/>
                     </clipPath>
                   </defs>
@@ -163,10 +163,10 @@
             <!-- /wp:list-item -->
           </ul>
           <!-- /wp:list -->
-          <!-- wp:group -->
-          <div class="wp-block-group inner-col2 w-[49%] absolute top-[60px] right-0">
-            <!-- wp:group -->
-            <div class="wp-block-group tile-wrapper relative mb-[12px] rounded-[20px] overflow-hidden">
+          <!-- wp:list -->
+          <ul class="wp-block-list inner-col2 w-[49%] absolute top-[60px] right-0">
+            <!-- wp:list-item -->
+            <li class="tile-wrapper relative mb-[12px] rounded-[20px] overflow-hidden">
              <!-- wp:group -->
              <div class="wp-block-group overlay-author z-[1] h-full w-full bg-gradient-to-t from-black via-[rgba(0,0,0,0)] opacity-[40%] absolute top-0 left-0">
                 <!-- wp:paragraph -->
@@ -194,10 +194,10 @@
                 <!-- /wp:paragraph -->
               </div>
               <!-- /wp:group -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group tile-wrapper relative mb-[12px] rounded-[20px] overflow-hidden">
+            </li>
+            <!-- /wp:list-item -->
+            <!-- wp:list-item -->
+            <li class="tile-wrapper relative mb-[12px] rounded-[20px] overflow-hidden">
               <!-- wp:group -->
               <div class="wp-block-group overlay-author z-[1] h-full w-full bg-gradient-to-t from-black via-[rgba(0,0,0,0)] opacity-[40%] absolute top-0 left-0 clipped-team">
                 <!-- wp:paragraph -->
@@ -217,7 +217,7 @@
               <p>
                 <svg width="0" height="0">
                   <defs>
-                    <clipPath id="clipped-team" class="scale-[0.49] desktop:scale-[0.839]">
+                    <clipPath id="clipped-team" class="scale-[0.49] desktop:scale-[0.839] wide:scale-[1.2]">
                       <path d="M66.1682 -6.10352e-05H334C345.046 -6.10352e-05 354 8.95424 354 19.9999V396.928C354 407.974 345.046 416.928 334 416.928H233.807C225.792 416.928 218.11 420.136 212.474 425.835L197.386 441.094C191.751 446.793 184.069 450 176.054 450H20.0001C8.95444 450 0.000112129 441.046 0.000109321 430L5.0847e-06 19.9999C2.2765e-06 8.95424 8.95431 -6.10352e-05 20 -6.10352e-05L66.1682 -6.10352e-05Z"/>
                     </clipPath>
                   </defs>
@@ -237,10 +237,10 @@
                 <!-- /wp:paragraph -->
               </div>
               <!-- /wp:group -->
-            </div>
+            </li>
             <!-- /wp:group -->
-          </div>
-          <!-- /wp:group -->
+          </ul>
+          <!-- /wp:list -->
         </div>
         <!-- /wp:group -->
       </div>
