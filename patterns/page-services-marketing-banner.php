d55f94f8cee3f6f39e7b71f4122ee95a48cb27a4
Tomek <TK-54-4B>
2023-08-21 10:58:04 +0200

    wp:tags fix

--- patterns/page-services-marketing-banner.php
+++ patterns/page-services-marketing-banner.php
@@ -10,37 +10,23 @@
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
 <div id="Banner" class="wp-block-group page-marketing-banner desktop:h-[860px] h-auto mb-[150px]">
   <!-- wp:group -->
-  <div
-    class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]"
-  >
+  <div class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]">
     <!-- wp:group -->
-    <div
-      class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] desktop:pt-[90px]"
-    >
+    <div class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] desktop:pt-[90px]">
       <!-- wp:group -->
-      <div
-        class="wp-block-group title-wrapper relative container mx-auto flex"
-      >
-
+      <div class="wp-block-group title-wrapper relative container mx-auto flex">
         <!-- wp:group -->
-        <div
-        class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
-        >
+        <div class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] uppercase text-primaryYellow">
           <!-- wp:paragraph -->
           <p class="m-0 justify-self-end pt-[3px] pr-[15px]">Usługi</p>
           <!-- /wp:paragraph -->
         </div>  
         <!-- /wp:group -->
 
-
         <!-- wp:group -->
         <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[24px] desktop:leading-[60px] leading-[30px]">
           <!-- wp:paragraph -->
-          <p
-            class="m-0 uppercase"
-          >
-            Marketing i PR
-          </p>
+          <p class="m-0 uppercase">Marketing i PR</p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -54,21 +40,20 @@
   <div class="wp-block-group services-marketing-banner-img relative">
     <!-- wp:image -->
     <figure class="wp-block-image object-cover h-full">
-      <img
-        src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-marketing/marketing-banner.jpg"
-        alt="bg-image"
-      />
+      <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-marketing/marketing-banner.jpg" alt="bg-image" />
     </figure>
     <!-- /wp:image -->
     <!-- wp:buttons -->
     <div
-      class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full border desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[350px] desktop:left-[85%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
+      class="wp-block-buttons desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-0 text-center rounded-full desktop:border-[4px] border-[3px] border-[#AAAAAA] absolute desktop:top-[-130px] top-[350px] desktop:left-[85%] left-[50%] translate-x-[-50%] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer"
     >
       <!-- wp:paragraph -->
-      <p class="m-0">Zobacz</p>
-      <!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <p class="m-0 uppercase text-[25px] font-extrabold">więcej</p>
+      <p>
+        <a href="#O nas">
+          <span class="font-bold block">Zobacz</span>
+          <span class="font-bold block text-[25px]">WIĘCEJ</span>
+        </a>
+      </p>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:buttons -->
@@ -78,29 +63,27 @@
   <!-- wp:group -->
   <div class="wp-block-group paragraph-wrapper container mx-auto desktop:px-0 px-[20px] relative w-full desktop:mb-[150px] mb-[50px] desktop:pt-[100px] pt-[50px]">
     <!-- wp:paragraph -->
-    <svg
-      class="shape scale-[0.6] desktop:scale-[1] z-[-1] absolute top-0 desktop:right-[0px] right-auto left-[-40px] desktop:left-auto"
-      xmlns="http://www.w3.org/2000/svg"
-      width="470"
-      height="396"
-      viewBox="0 0 470 396"
-      fill="none"
-    >
-      <path
-        d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z"
-        stroke="#FFFFFF"
-        stroke-width="2"
-      />
-    </svg>
+    <p>
+      <svg
+        class="shape scale-[0.6] desktop:scale-[1] z-[-1] absolute top-0 desktop:right-[0px] right-auto left-[-40px] desktop:left-auto"
+        xmlns="http://www.w3.org/2000/svg"
+        width="470"
+        height="396"
+        viewBox="0 0 470 396"
+        fill="none"
+      >
+        <path
+          d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z"
+          stroke="#FFFFFF"
+          stroke-width="2"
+        />
+      </svg>
+    </p>
     <!-- /wp:paragraph -->
     <!-- wp:group -->
     <div class="wp-block-group paragraph-wrapper desktop:px-0 px-[20px] desktop:ml-[145px]">
       <!-- wp:paragraph -->
-      <p
-      class="desktop:text-[35px] text-[22px] font-medium desktop:leading-[42px] leading-[32px] desktop:w-[897px]"
-      >
-        Jesteśmy ekspertami od komunikacji i marketingu. Pracujemy dla topowych firm, stawiających przed nami duże wyzwania.
-      </p>
+      <p class="desktop:text-[35px] text-[22px] font-medium desktop:leading-[42px] leading-[32px] desktop:w-[897px]">Jesteśmy ekspertami od komunikacji i marketingu. Pracujemy dla topowych firm, stawiających przed nami duże wyzwania.</p>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
