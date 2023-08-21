5428afa6c86e37c186088ebfee4c96cc51ff1f9a
Tomek <TK-54-4B>
2023-08-21 11:13:23 +0200

    wp:tags fix

--- patterns/page-services-marketing-content3.php
+++ patterns/page-services-marketing-content3.php
@@ -10,40 +10,34 @@
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
 <div id="2xKafelki" class="wp-block-group page-marketing-info">
   <!-- wp:group -->
-  <div
-    class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
-  >
+  <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]">
     <!-- wp:group -->
-    <div
-      class="wp-block-group title-wrapper relative desktop:mb-[100px] mb-[40px] desktop:pt-[90px] pt-[20px]"
-    >
+    <div class="wp-block-group title-wrapper relative desktop:mb-[100px] mb-[40px] desktop:pt-[90px] pt-[20px]">
       <!-- wp:group -->
-      <div
-        class="wp-block-group title-wrapper relative desktop:flex"
-      >
+      <div class="wp-block-group title-wrapper relative desktop:flex">
         <!-- wp:paragraph -->
-        <svg class="flash desktop:scale-[1] scale-[0.4] absolute desktop:top-[-330px] top-[-360px] desktop:left-[-140px] right-[-100px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
-          <g filter="url(#filter0_f_1787_1988)">
-          <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
-          </g>
-          <defs>
-          <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-          <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
-          </filter>
-          <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
-          <stop stop-color="#FEC400" stop-opacity="0.6"/>
-          <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
-          </linearGradient>
-          </defs>
-        </svg>
+        <p>
+          <svg class="flash desktop:scale-[1] scale-[0.4] absolute desktop:top-[-330px] top-[-360px] desktop:left-[-140px] right-[-100px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+            <g filter="url(#filter0_f_1787_1988)">
+            <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
+            </g>
+            <defs>
+            <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
+            </filter>
+            <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FEC400" stop-opacity="0.6"/>
+            <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
+            </linearGradient>
+            </defs>
+          </svg>
+        </p>
         <!-- /wp:paragraph -->
 
         <!-- wp:group -->
-        <div
-        class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
-        >
+        <div class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] uppercase text-primaryYellow">
           <!-- wp:paragraph -->
           <p class="desktop:justify-self-end pt-[3px] pr-[15px] text-[20px]">03</p>
           <!-- /wp:paragraph -->
@@ -54,16 +48,7 @@
         <!-- wp:group -->
         <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[50px] text-[24px] desktop:leading-[60px] leading-[30px]">
           <!-- wp:paragraph -->
-          <p
-            class="uppercase"
-          >
-            Opracowanie strategii <br />
-            Twoim
-            <span
-              class="relative inline desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
-              >Marketingowych</span
-            >
-          </p>
+          <p class="uppercase">Opracowanie strategii <br />Twoim<span class="relative inline desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">Marketingowych</span></p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -82,35 +67,24 @@
         </svg>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="max-w-[173px] text-[16px] leading-[21px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor.
-        </p>
+        <p class="max-w-[173px] text-[16px] leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
       <!-- wp:group -->
       <div class="wp-block-group col2 relative basis-[60%]">
         <!-- wp:paragraph -->
-        <svg class="z-[-1] absolute top-[-250px] right-[20px]" xmlns="http://www.w3.org/2000/svg" width="470" height="396" viewBox="0 0 470 396" fill="none">
-          <path d="M0.999573 393.923L468.242 236.702V1.96971L0.999573 159.191V393.923Z" stroke="white" stroke-width="2"/>
-        </svg>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p class="desktop:text-[20px] text-[18px] desktop:leading-[30px] leading-[26px] pb-[50px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
+        <p>
+          <svg class="z-[-1] absolute top-[-250px] right-[20px]" xmlns="http://www.w3.org/2000/svg" width="470" height="396" viewBox="0 0 470 396" fill="none">
+            <path d="M0.999573 393.923L468.242 236.702V1.96971L0.999573 159.191V393.923Z" stroke="white" stroke-width="2"/>
+          </svg>
         </p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="hidden desktop:block text-[16px] leading-[21px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <p class="desktop:text-[20px] text-[18px] desktop:leading-[30px] leading-[26px] pb-[50px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="hidden desktop:block text-[16px] leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -121,53 +95,44 @@
     <!-- wp:group -->
     <div class="wp-block-group page-marketing-content">
       <!-- wp:group -->
-      <div
-        class="wp-block-group"
-      >
-
-      <!-- wp:group -->
-      <div class="wp-block-group gallery desktop:mb-[240px] mb-[150px] relative mb-[100px] flex gap-[8px]">
-        <!-- wp:image -->
-        <figure class="wp-block-image w-[50%] overflow-hidden rounded-tl-[22px] rounded-bl-[22px]">
-          <img  
-          src="	https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-marketing/gallery1.jpg"
-          alt="image"
-          />
-        </figure>
-        <!-- /wp:image -->
-
+      <div class="wp-block-group">
         <!-- wp:group -->
-        <div class="wp-block-group mb-[20px] w-[50%] absolute top-[60px] right-[0px]">
+        <div class="wp-block-group gallery desktop:mb-[240px] relative mb-[100px] flex gap-[8px]">
+          <!-- wp:image -->
+          <figure class="wp-block-image w-[50%] overflow-hidden rounded-tl-[22px] rounded-bl-[22px]">
+            <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-marketing/gallery1.jpg" alt="image" />
+          </figure>
+          <!-- /wp:image -->
+
           <!-- wp:group -->
-          <div class="wp-block-group overflow-hidden ">
-            <!-- wp:image -->
-            <figure class="wp-block-image clipped-meet-us-gallery h-full">
-              <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-marketing/gallery2.jpg" alt="image">
-            </figure>
-            <!-- /wp:image -->
-            <!-- wp:paragraph -->
-            <svg x="0px" y="0px" viewBox="0 0 1374.7 1083.6" class="absolute top-0" >
-              <defs>
-                <clipPath id="clipped-meet-us-gallery" class="scale-[0.00133] scale-x-[0.00160]" clipPathUnits="objectBoundingBox">
-                  <path d="M-0.000204735 0L605 0C616.046 0 625 8.95431 625 20V680C625 691.046 616.046 700 605 700H437.429C429.472 700 421.841 703.161 416.215 708.787L383.789 741.213C378.163 746.839 370.532 750 362.576 750H0L-0.000204735 0Z"/>
-                </clipPath>
-              </defs>
-            </svg>
-            <!-- /wp:paragraph -->
+          <div class="wp-block-group mb-[20px] w-[50%] absolute top-[60px] right-[0px]">
+            <!-- wp:group -->
+            <div class="wp-block-group overflow-hidden ">
+              <!-- wp:image -->
+              <figure class="wp-block-image clipped-meet-us-gallery h-full">
+                <img src="https://mutiny.webo.design/wp-content/themes/mutiny/assets/img/page-marketing/gallery2.jpg" alt="image">
+              </figure>
+              <!-- /wp:image -->
+              <!-- wp:paragraph -->
+              <svg x="0px" y="0px" viewBox="0 0 1374.7 1083.6" class="absolute top-0" >
+                <defs>
+                  <clipPath id="clipped-meet-us-gallery" class="scale-[0.00133] scale-x-[0.00160]" clipPathUnits="objectBoundingBox">
+                    <path d="M-0.000204735 0L605 0C616.046 0 625 8.95431 625 20V680C625 691.046 616.046 700 605 700H437.429C429.472 700 421.841 703.161 416.215 708.787L383.789 741.213C378.163 746.839 370.532 750 362.576 750H0L-0.000204735 0Z"/>
+                  </clipPath>
+                </defs>
+              </svg>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
           </div>
           <!-- /wp:group -->
-
         </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:grup -->
-
+        <!-- /wp:grup -->
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-    
-
   </div>
   <!-- /wp:group -->
 </div>
