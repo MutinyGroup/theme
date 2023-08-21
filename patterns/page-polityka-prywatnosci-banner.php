441853e3f2e0d31e5ede4d3b6646a344334775c2
Tomek <TK-54-4B>
2023-08-21 09:01:04 +0200

    added page private policy

---
+++ patterns/page-polityka-prywatnosci-banner.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Title: page-polityka-prywatnosci-banner
+ * Slug: page-polityka-prywatnosci-banner
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
+<div id="Banner" class="wp-block-group polityka-prywatnosci-banner group h-auto desktop:mb-[150px] pt-[80px] desktop:pt-0">
+  <!-- wp:group -->
+  <div class="wp-block-group container relative mx-auto desktop:px-0 px-[20px]">
+    <!-- wp:group -->
+    <div class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] desktop:mt-[90px]">
+      <!-- wp:group -->
+      <div id="title" class="wp-block-group title-wrapper relative container mx-auto desktop:flex">
+        <!-- wp:group -->
+        <div class="wp-block-group section-title grid desktop:w-[145px] desktop:basis-auto uppercase text-primaryYellow">
+          <!-- wp:paragraph -->
+          <p class="justify-self-start desktop:justify-self-end pt-[3px] pr-[15px]">Info</p>
+          <!-- /wp:paragraph -->
+        </div>  
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
+          <!-- wp:paragraph -->
+          <p>POLITYKA</p>
+          <!-- /wp:paragraph -->
+         <!-- wp:paragraph {"templateLock":"contentOnly"} -->
+         <p class="relative inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">PRYWATNOŚCI</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+  <!-- wp:group -->
+  <div class="wp-block-group relative">
+    <!-- wp:group -->
+    <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+      <!-- wp:paragraph -->
+      <p class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px] mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] desktop:mx-0 mb-[80px] dekstop:mb-0 text-center rounded-full desktop:border-[4px] border-[3px] border-[#AAAAAA] mx-auto desktop:absolute desktop:top-[-130px] top-[310px] wide:left-[70%] desktop:left-[85%] left-[50%] desktop:translate-x-[-50%] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
+      <!-- wp:paragraph -->
+      <p>
+        <a href="#Eventy">
+          <span class="font-bold block">Zobacz</span>
+          <span class="font-bold block text-[25px]">WIĘCEJ</span>
+        </a>
+      </p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->       
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
