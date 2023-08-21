520ae6075686e7509b11fb6ecbd630d8e2313b41
Tomek <TK-54-4B>
2023-08-21 22:24:17 +0200

    editor styles

--- patterns/page-about-meet-us.php
+++ patterns/page-about-meet-us.php
@@ -47,10 +47,7 @@
       <!-- wp:group -->
       <div class="wp-block-group uslugi-title font-medium">
         <!-- wp:paragraph -->
-        <p class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">
-          Kampanie realizujemy <br>
-          <span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">kompleksowo</span>
-        </p>
+        <p class="uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">Kampanie realizujemy <br><span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[16px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">kompleksowo</span></p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -210,7 +207,6 @@
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
-
       </div>
       <!-- /wp:group -->
     </div>
