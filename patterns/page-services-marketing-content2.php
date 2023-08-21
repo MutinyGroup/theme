b6665a846511aec8795e6a782707bbadc5b245ac
Tomek <TK-54-4B>
2023-08-21 11:22:19 +0200

    test

--- patterns/page-services-marketing-content2.php
+++ patterns/page-services-marketing-content2.php
@@ -10,44 +10,59 @@
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
 <div id="Galeria Swiper" class="wp-block-group page-marketing-info mb-[100px]">
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]">
+  <div
+    class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]"
+  >
     <!-- wp:group -->
-    <div class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] pt-[90px]">
+    <div
+      class="wp-block-group title-wrapper relative desktop:mb-[58px] mb-[40px] pt-[90px]"
+    >
       <!-- wp:group -->
-      <div class="wp-block-group title-wrapper relative desktop:flex">
+      <div
+        class="wp-block-group title-wrapper relative desktop:flex"
+      >
         <!-- wp:paragraph -->
-        <p>
-          <svg class="flash desktop:scale-[1] scale-[0.4] absolute desktop:top-[-330px] top-[-360px] desktop:left-[-140px] right-[-100px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
-            <g filter="url(#filter0_f_1787_1988)">
-            <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
-            </g>
-            <defs>
-            <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-            <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
-            </filter>
-            <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#FEC400" stop-opacity="0.6"/>
-            <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
-            </linearGradient>
-            </defs>
-          </svg>
-        </p>
+        <svg class="flash desktop:scale-[1] scale-[0.4] absolute desktop:top-[-330px] top-[-360px] desktop:left-[-140px] right-[-100px] z-[-1]" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+          <g filter="url(#filter0_f_1787_1988)">
+          <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5"/>
+          </g>
+          <defs>
+          <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988"/>
+          </filter>
+          <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FEC400" stop-opacity="0.6"/>
+          <stop offset="1" stop-color="#FEC400" stop-opacity="0"/>
+          </linearGradient>
+          </defs>
+        </svg>
         <!-- /wp:paragraph -->
 
         <!-- wp:group -->
-        <div class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] uppercase text-primaryYellow">
+        <div
+        class="wp-block-group section-title grid desktop:basis-auto desktop:w-[145px] desktop:basis-[80%] uppercase text-primaryYellow"
+        >
           <!-- wp:paragraph -->
           <p class="desktop:justify-self-end pt-[3px] pr-[15px] text-[20px]">02</p>
           <!-- /wp:paragraph -->
         </div>  
         <!-- /wp:group -->
 
+
         <!-- wp:group -->
         <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[50px] text-[24px] desktop:leading-[60px] leading-[30px]">
           <!-- wp:paragraph -->
-          <p class="uppercase">content<span class="relative inline desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">Marketing</span></p>
+          <p
+            class="uppercase"
+          >
+            content
+            <span
+              class="relative inline desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"
+              >Marketing</span
+            >
+          </p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -61,45 +76,66 @@
       <!-- wp:group -->
       <div class="wp-block-group col1 desktop:pl-[155px] basis-[40%]">
         <!-- wp:paragraph -->
-        <p class="max-w-[173px] text-[16px] leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+        <p class="max-w-[173px] text-[16px] leading-[21px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor.
+        </p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
       <!-- wp:group -->
       <div class="wp-block-group col2 relative basis-[60%]">
         <!-- wp:paragraph -->
-        <p>
-          <svg class="hidden desktop:block z-[-1] absolute top-[-250px] left-[-250px]" xmlns="http://www.w3.org/2000/svg" width="1051" height="810" viewBox="0 0 1051 810" fill="none">
-            <path d="M1 1.45224L938.266 329.349L938.596 329.465L938.927 329.349L1406.89 165.633V409.787L938.596 573.618L234.979 327.462L233.649 326.996V328.405V807.609L1 726.219V1.45224Z" stroke="white" stroke-width="2"/>
-          </svg>
+        <svg class="hidden desktop:block z-[-1] absolute top-[-250px] left-[-250px]" xmlns="http://www.w3.org/2000/svg" width="1051" height="810" viewBox="0 0 1051 810" fill="none">
+          <path d="M1 1.45224L938.266 329.349L938.596 329.465L938.927 329.349L1406.89 165.633V409.787L938.596 573.618L234.979 327.462L233.649 326.996V328.405V807.609L1 726.219V1.45224Z" stroke="white" stroke-width="2"/>
+        </svg>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="desktop:text-[20px] text-[18px] desktop:leading-[30px] leading-[26px] pb-[50px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
         </p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="desktop:text-[20px] text-[18px] desktop:leading-[30px] leading-[26px] pb-[50px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat.</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p class="hidden desktop:block pb-[50px] text-[16px] leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p class="hidden desktop:block pb-[50px] text-[16px] leading-[21px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
+        </p>
         <!-- /wp:paragraph -->
         
         <!-- wp:paragraph -->
         <p class="pb-[30px] text-[20px] font-medium">Co dostaniesz w ramach usługi?</p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="desktop:block text-[16px] leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p class="desktop:block text-[16px] leading-[21px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+        </p>
         <!-- /wp:paragraph -->
+
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
 
+
     <!-- wp:group -->
     <div class="wp-block-group page-marketing-content">
       <!-- wp:group -->
-      <div class="wp-block-group">
+      <div
+        class="wp-block-group"
+      >
         <!-- wp:group -->
         <div class="wp-block-group desktop:mt-[-120px] w-full">
+
           <!-- wp:group -->
-          <div id="marketing" class="wp-block-group swiper swiperMarketing desktop:pt-[180px] absolute top-0 desktop:left-[150px] left-[20px]">
+          <div id="marketing" class="wp-block-group swiper swiperMarketing relative desktop:pt-[180px] absolute top-0 desktop:left-[150px] left-[20px]">
             <!-- wp:group -->
             <div class="wp-block-group swiper-wrapper">
               <!-- wp:group -->
@@ -173,10 +209,13 @@
           <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
+
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
+    
+
   </div>
   <!-- /wp:group -->
 </div>
