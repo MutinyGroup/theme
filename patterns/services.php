84a2cea5bd663e258a458fc5ddab5070b77b59cb
Tomek <TK-54-4B>
2023-08-21 14:07:50 +0200

    test

--- patterns/services.php
+++ patterns/services.php
@@ -104,7 +104,7 @@
                 $counter++;
               ?>
                 <!-- wp:group -->
-                <div class="wp-block-group swiper-slide swiper-slide-services relative z-[2] group p-[25px] wide:max-w-[420px] desktop:max-w-[354px] max-w-[310px] h-[380px] desktop:h-[450px] wide:h-[530px] rounded-[20px] transition ease-out duration-300 mr-[8px] <?php if($counter == 1) echo 'clip-path-url-["#clipped-swiper-serv-1"]'; elseif($counter == $post_query->post_count) echo 'clip-path-url-["#clipped-swiper-serv-2"]'; ?>">
+                <div class="wp-block-group swiper-slide swiper-slide-services relative z-[2] group p-[25px] wide:max-w-[420px] desktop:max-w-[354px] max-w-[310px] h-[380px] desktop:h-[450px] wide:h-[530px] rounded-[20px] transition ease-out duration-300 mr-[8px] <?php if($counter == 1) echo "clip-path-url-['#clipped-swiper-serv-1']"; elseif($counter == $post_query->post_count) echo 'clip-path-url-["#clipped-swiper-serv-2"]'; ?>">
                   <!-- wp:group -->
                   <div class="wp-block-group img-clip-path-wrapper w-full h-full rounded-[20px] rounded-br-[20px] overflow-hidden absolute left-0 top-0 z-[-1] group-hover:grayscale">
                     <!-- wp:group -->
