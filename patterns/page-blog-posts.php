1fd93a8b825fc7f840a1101cd211014585b3f1e8
Tomek <TK-54-4B>
2023-08-22 12:43:24 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -132,31 +132,10 @@
                 </a>
               </li>
               <!-- /wp:list-item -->
-          <?php
+        <?php
               $i++;
             };
-          }; ?>
-          <!-- wp:group -->
-          <div class="wp-block-group pagination">
-            <?php
-            echo paginate_links(array(
-              'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-              'current'      => max(1, get_query_var('paged')),
-              'format'       => '?paged=%#%',
-              'show_all'     => false,
-              'type'         => 'plain',
-              'end_size'     => 2,
-              'mid_size'     => 1,
-              'prev_next'    => true,
-              'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-              'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-              'add_args'     => false,
-              'add_fragment' => '',
-            ));
-            ?>
-          </div>
-          <!-- /wp:group -->
-        <?php
+          };
         };
         ?>
       </ul>
