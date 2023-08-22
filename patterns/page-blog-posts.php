3dc2cc6c2f0ae2151a7a03d5af7694f9a03b528b
Tomek <TK-54-4B>
2023-08-22 12:17:35 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -132,10 +132,13 @@
                 </a>
               </li>
               <!-- /wp:list-item -->
-        <?php
+          <?php
               $i++;
             };
-          };
+          }; ?>
+          <div class="nav-previous alignleft"><?php next_posts_link('Older posts'); ?></div>
+          <div class="nav-next alignright"><?php previous_posts_link('Newer posts'); ?></div>
+        <?php
         };
         ?>
       </ul>
