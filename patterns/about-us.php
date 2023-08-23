3d45affe5163a023fa940a600c7e97812dd882c0
Tomek <TK-54-4B>
2023-08-23 15:25:07 +0200

    code cleaning

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -231,7 +231,7 @@
           <!-- wp:buttons -->
           <div class="wp-block-buttons ">
             <!-- wp:paragraph -->
-            <p><a href="<?php echo get_home_url() . '/about'; ?>" class="uppercase text-[14px] font-bold min-w-[225px] flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] uppercase border-[2px] border-white rounded-full hover:bg-white hover:text-black transition ease-out duration-300">Dowiedz się więcej</a></p>
+            <p><a href="<?php echo get_home_url() . '/about'; ?>" class="uppercase text-[14px] font-bold min-w-[225px] flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] border-white rounded-full hover:bg-white hover:text-black transition ease-out duration-300">Dowiedz się więcej</a></p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:buttons -->
