4c4b9138aa4627dd93d6e7340c9afe460cbaac7f
Tomek <TK-54-4B>
2023-08-23 18:30:04 +0200

    test

--- patterns/page-services-marketing-content4.php
+++ patterns/page-services-marketing-content4.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="Swiper index" class="wp-block-group page-marketing ">
+<div id="Swiper index" class="wp-block-group page-marketing overflow-x-hidden">
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]">
     <!-- wp:group -->
