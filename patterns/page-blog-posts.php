4e5b94f327e29c384501fe70014da5c8001e9d27
Tomek <TK-54-4B>
2023-08-23 11:51:08 +0200

    bg shape fix

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -16,7 +16,7 @@
     <div class="wp-block-group new gallery-cols relative flex justify-between desktop:pb-[150px] pb-[80px]">
       <!-- wp:paragraph -->
       <p>
-        <svg class="wp-block-image z-[-1] absolute top-[0px] right-[0px]" xmlns="http://www.w3.org/2000/svg" width="470" height="396" viewBox="0 0 470 396" fill="none">
+        <svg class="bg-shape z-[0] absolute top-[0px] right-[0px]" xmlns="http://www.w3.org/2000/svg" width="470" height="396" viewBox="0 0 470 396" fill="none">
           <path d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z" stroke="white" stroke-width="2" />
         </svg>
       </p>
