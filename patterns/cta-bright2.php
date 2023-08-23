9455f82c166c07c6b7aecb4200f9d7db80de21ec
Tomek <TK-54-4B>
2023-08-23 11:38:07 +0200

    styles fix

--- patterns/cta-bright2.php
+++ patterns/cta-bright2.php
@@ -8,30 +8,40 @@
  */
 ?>
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
-<div id="CTA" class="wp-block-group relative cta-bright2 mb-[150px]">
+<div id="CTA" class="wp-block-group relative cta-bright1 mb-[150px]">
+  <!-- wp:paragraph -->
+  <p class="scale-[0.3] desktop:scale-[1] absolute left-0">
+    <svg class="bg-shape z-[0] absolute top-[-145px] left-[-80px]" width="1201" height="917" viewBox="0 0 1201 917" fill="none">
+      <path d="M-91 2.02805L553.6 283.595L554 283.77L554.4 283.595L751.232 197.617L876 191.674V352.568L554 493.221L69.9003 281.762L68.5 281.15V282.678V711.805L-91 642.134V2.02805Z" stroke="white" stroke-width="2" />
+      <path d="M1040.07 494.916L1040.67 494.654V494L1040.67 262.321L1200 192.695L1200 634.346L1039.27 704.584L555.667 915.909L234 775.346V566.028L555.266 706.416L555.667 706.591L556.067 706.416L1040.07 494.916Z" stroke="white" stroke-width="2" />
+    </svg>
+  </p>
+  <!-- /wp:paragraph -->
   <!-- wp:group -->
   <div class="wp-block-group container group mx-auto desktop:px-0 px-[20px] desktop:mt-0 mt-[120px] desktop:p-[110px] h-[300px] desktop:h-[700px] wide:h-[940px] ">
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper relative desktop:mb-[80px] mb-[60px]">
       <!-- wp:group -->
-      <div class="wp-block-group flash-wrapper wide:scale-[1] desktop:scale-[1] scale-[0.5] absolute top-[-350px] desktop:top-[-300px] wide:top-[-270px] left-[-200px] wide:left-[-230px] z-[-1]">
+      <div class="wp-block-group flash-wrapper wide:scale-[1] desktop:scale-[1] scale-[0.5] absolute top-[-350px] desktop:top-[-300px] wide:top-[-270px] left-[-200px] wide:left-[-230px] z-[0]">
         <!-- wp:paragraph -->
-        <svg class="flash" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
-          <g filter="url(#filter0_f_1787_1988)">
-            <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5" />
-          </g>
-          <defs>
-            <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988" />
-            </filter>
-            <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
-              <stop stop-color="#FEC400" stop-opacity="0.6" />
-              <stop offset="1" stop-color="#FEC400" stop-opacity="0" />
-            </linearGradient>
-          </defs>
-        </svg>
+        <p>
+          <svg class="flash" xmlns="http://www.w3.org/2000/svg" width="688" height="801" viewBox="0 0 688 801" fill="none">
+            <g filter="url(#filter0_f_1787_1988)">
+              <circle cx="287.132" cy="400.132" r="150" transform="rotate(-45 287.132 400.132)" fill="url(#paint0_linear_1787_1988)" fill-opacity="0.5" />
+            </g>
+            <defs>
+              <filter id="filter0_f_1787_1988" x="-112.868" y="0.131836" width="800" height="800" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                <feGaussianBlur stdDeviation="125" result="effect1_foregroundBlur_1787_1988" />
+              </filter>
+              <linearGradient id="paint0_linear_1787_1988" x1="287.132" y1="250.132" x2="287.132" y2="550.132" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FEC400" stop-opacity="0.6" />
+                <stop offset="1" stop-color="#FEC400" stop-opacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </p>
         <!-- /wp:paragraph -->
 
         <!-- wp:image -->
@@ -45,7 +55,7 @@
       <!-- wp:paragraph -->
       <p class="section-title text-[20px] text-primaryYellow">Wymagaj więcej</p>
       <!-- /wp:paragraph -->
-      <!-- wp:group {"templateLock":"all"} -->
+      <!-- wp:group -->
       <div class="wp-block-group uslugi-title font-normal desktop:text-[65px] text-[32px] desktop:leading-[70px] leading-[36px]">
         <!-- wp:paragraph {"templateLock":"all"} -->
         <p class="uppercase">Porozmawiajmy</p>
@@ -62,12 +72,12 @@
     <!-- /wp:group -->
 
     <!-- wp:group -->
-    <div class="wp-block-group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] desktop:ml-[120px] mx-auto text-center rounded-full border-[4px] border-[#D5D5D5] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
+    <div class="wp-block-group relative desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] desktop:ml-[120px] mx-auto text-center rounded-full border-[4px] border-[#D5D5D5] group-hover:border-none group-hover:bg-primaryYellow group-hover:text-white transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer">
       <!-- wp:paragraph -->
       <p>
         <a href="<?php echo get_home_url() . '/contact'; ?>">
-          <span class="font-bold block">Skontaktuj się</span>
-          <span class="font-bold block text-[25px] uppercase">TERAZ</span>
+          <span class="font-bold block">Poznaj</span>
+          <span class="font-bold block text-[25px] uppercase">OFERTĘ</span>
         </a>
       </p>
       <!-- /wp:paragraph -->
@@ -77,10 +87,10 @@
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group img-clip-path-wrapper w-[58%] h-full rounded-tl-[20px] overflow-hidden absolute right-0 desktop:top-0 top-[-40px] z-[-1]">
+  <div class="wp-block-group img-clip-path-wrapper w-[58%] h-full rounded-tl-[20px] overflow-hidden absolute right-0 desktop:top-0 top-[-40px] z-[0]">
     <!-- wp:image -->
     <figure class="wp-block-image clip-path-url-['#clipped-cta'] h-full before:content-[''] before:w-full before:h-full before:bg-white before:z-[1] before:opacity-[60%] before:absolute before:top-0 before:left-0">
-      <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/cta-bright-bg1.jpg'; ?>" alt="cta-backgroun-image">
+      <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/cta-bright-bg2.jpg'; ?>" alt="cta-backgroun-image">
     </figure>
     <!-- /wp:image -->
     <!-- wp:paragraph -->
