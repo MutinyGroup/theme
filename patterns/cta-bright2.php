9e2f2e28a730ba08bf0910258477ff28b3d92dbb
Tomek <TK-54-4B>
2023-08-23 11:39:43 +0200

    img fix

--- patterns/cta-bright2.php
+++ patterns/cta-bright2.php
@@ -90,7 +90,7 @@
   <div class="wp-block-group img-clip-path-wrapper w-[58%] h-full rounded-tl-[20px] overflow-hidden absolute right-0 desktop:top-0 top-[-40px] z-[0]">
     <!-- wp:image -->
     <figure class="wp-block-image clip-path-url-['#clipped-cta'] h-full before:content-[''] before:w-full before:h-full before:bg-white before:z-[1] before:opacity-[60%] before:absolute before:top-0 before:left-0">
-      <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/cta-bright-bg2.jpg'; ?>" alt="cta-backgroun-image">
+      <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/cta-bright-bg2.jpeg'; ?>" alt="cta-backgroun-image">
     </figure>
     <!-- /wp:image -->
     <!-- wp:paragraph -->
