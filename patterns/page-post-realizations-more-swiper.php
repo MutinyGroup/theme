c9a66605ae4769a162b49aec4b2554f04966656b
Tomek <TK-54-4B>
2023-08-23 17:05:00 +0200

    style fix

--- patterns/page-post-realizations-more-swiper.php
+++ patterns/page-post-realizations-more-swiper.php
@@ -23,7 +23,7 @@
         <p class="uppercase desktop:text-[50px] text-[26px]">Zobacz więcej</p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="relative desktop:text-[50px] text-[26px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[5px] before:bottom-0 before:left-0 desktop:before:w-[120px] before:w-[30px]  desktop:before:h-[3px] before:h-[2px] before:bg-black">Realizacji</p>
+        <p class="relative desktop:text-[50px] text-[26px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[5px] before:bottom-0 before:left-0 desktop:before:w-[80px] before:w-[30px]  desktop:before:h-[3px] before:h-[2px] before:bg-black">Realizacji</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
