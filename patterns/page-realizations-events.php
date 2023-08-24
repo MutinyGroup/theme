cf2efb5b1db9574358c35e77afc1f1b5be52f1f3
Tomek <TK-54-4B>
2023-08-24 17:40:29 +0200

    test

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -56,7 +56,7 @@
       <?php
       $args = array(
         'post_type' => 'realizacje',
-        'posts_per_page' => 6,
+        'posts_per_page' => 2,
         'order' => 'DESC'
       );
       $post_query = new WP_Query($args);
