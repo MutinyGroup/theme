868cbfd05779770db3cd22aaa0afd9e209709bdc
Tomek <TK-54-4B>
2023-08-24 18:10:22 +0200

    test

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -161,7 +161,7 @@
       <!-- wp:group -->
       <div class="wp-block-group btn-more min-w-fit flex gap-[20px]">
         <?php
-        $currentPageID = substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 44, 1);
+        $currentPageID = (int)substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 44, 1);
 
         ?>
         <!-- wp:paragraph -->
@@ -180,11 +180,24 @@
             <!-- wp:paragraph -->
             <p><a href="<?php echo home_url() . '/realizations/page/' . ($currentPageID - 1) ?>" class="min-w-fit ml-[0px] p-[15px_30px] font-bold uppercase border-[2px] border-[#D5D5D5] rounded-full hover:border-primaryYellow hover:text-black transition ease-out duration-300 cursor-pointer text-[14px]">Pokaz poprzednie</a></p>
             <!-- /wp:paragraph -->
-        <?php
+          <?php
           };
+          ?>
+          <!-- wp:paragraph -->
+          <p><a href="<?php echo home_url() . '/blog/page/' . ($currentPageID + 1) ?>" class="min-w-fit ml-[0px] p-[15px_30px] font-bold uppercase border-[2px] border-[#D5D5D5] rounded-full hover:border-primaryYellow hover:text-black transition ease-out duration-300 cursor-pointer text-[14px]">Pokaz następne</a></p>
+          <!-- /wp:paragraph -->
+
+
+        <?php
+        } elseif ($currentPageID = $post_query->max_num_pages) {
+        ?>
+          <!-- wp:paragraph -->
+          <p><a class="min-w-fit ml-[0px] p-[15px_30px] font-bold uppercase border-[2px] border-[#D5D5D5] rounded-full hover:border-primaryYellow hover:text-black transition ease-out duration-300 cursor-pointer" href="<?php echo home_url() . '/blog/page/' . ($currentPageID - 1) ?>" class="uppercase text-[14px] font-bold">Pokaz poprzednie</a></p>
+          <!-- /wp:paragraph -->
+
+
+        <?php
         };
-
-
         ?>
       </div>
       <!-- /wp:group -->
