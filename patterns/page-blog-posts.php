dd43686d24fab279a565a5af170ca34067ac0642
Tomek <TK-54-4B>
2023-08-24 17:10:14 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -51,7 +51,7 @@
             $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(), 'portrait');
             $counter++;
             if ($tab[$i] == 0) {
-              array_push($tab2, $rand) ?>
+              array_push($tab, $rand); ?>
               <!-- wp:list-item -->
               <li class="wp-block-group bigger swiper-slide relative group w-full desktop:w-[535px] wide:w-[580px] h-fit desktop:h-[545px] wide:h-[650px] <?php echo (!($counter % 2)) ? 'desktop:translate-y-[120px]' : ''; ?>">
                 <a href="<?php the_permalink(); ?>">
@@ -96,7 +96,8 @@
               <!-- /wp:list-item -->
             <?php
               $i++;
-            } else { ?>
+            } else {
+              array_push($tab, $rand); ?>
               <!-- wp:list-item -->
               <li class="wp-block-group smaller swiper-slide relative group w-full desktop:w-[444px] wide:w-[490px] h-fit desktop:h-[500px] <?php echo (!($counter % 2)) ? 'desktop:translate-y-[120px]' : ''; ?>">
                 <a href="<?php the_permalink(); ?>">
@@ -142,7 +143,7 @@
           <?php
               $i++;
               echo '<pre>';
-              print_r($tab2);
+              print_r($tab);
               echo '</pre>';
             };
           };
