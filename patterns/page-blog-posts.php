9797d54f9dfa6f79bc6bd67d19c11b261cf5ae1f
Tomek <TK-54-4B>
2023-08-24 17:32:20 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -34,7 +34,7 @@
         $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
         $args = array(
           'post_type' => 'post',
-          'posts_per_page' => 6,
+          'posts_per_page' => 10,
           'paged' => $currentPageID,
           'order' => 'DESC'
         );
