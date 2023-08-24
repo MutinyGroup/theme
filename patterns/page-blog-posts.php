6705a1929cdbbcc98b8a60b5f435a85234e543f0
Tomek <TK-54-4B>
2023-08-24 12:55:49 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -203,7 +203,7 @@
   </ul>
   <!-- /wp:list -->
   <?php
-  $currentUrl = substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 31, 3);
+  $currentUrl = substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 20, 3);
   ?>
   <!-- wp:paragraph -->
   <p><?php echo $currentUrl; ?></p>
