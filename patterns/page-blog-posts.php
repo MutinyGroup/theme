0b33351fa127232b6e9294b56c63a9540aba56bc
Tomek <TK-54-4B>
2023-08-24 17:03:37 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -43,7 +43,7 @@
         $tab = [0, 1, 1, 0, 0, 1];
         $tab2 = [1, 2];
         $i = 0;
-        echo rand(0, 1);
+        $rand = rand(0, 1);
 
         if ($post_query->have_posts()) {
           while ($post_query->have_posts()) {
@@ -51,7 +51,7 @@
             $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(), 'portrait');
             $counter++;
             if ($tab[$i] == 0) {
-              array_push($tab2, 2) ?>
+              array_push($tab2, $rand) ?>
               <!-- wp:list-item -->
               <li class="wp-block-group bigger swiper-slide relative group w-full desktop:w-[535px] wide:w-[580px] h-fit desktop:h-[545px] wide:h-[650px] <?php echo (!($counter % 2)) ? 'desktop:translate-y-[120px]' : ''; ?>">
                 <a href="<?php the_permalink(); ?>">
