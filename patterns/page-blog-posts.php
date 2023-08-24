43e914337e454586484dab7b4e0ae7f35403f893
Tomek <TK-54-4B>
2023-08-24 15:12:56 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -164,6 +164,21 @@
 
     <!-- wp:buttons -->
     <div class="wp-block-buttons btn-more min-w-fit ml-[0px] p-[15px_30px] uppercase border-[2px] border-[#D5D5D5] rounded-full hover:border-primaryYellow hover:text-black transition ease-out duration-300 cursor-pointer">
+      <?php
+      if ($currentPageID < ($post_query->max_num_pages)) {
+      ?>
+        <!-- wp:paragraph -->
+        <p><a href="<?php echo home_url() . '/blog/page/' . ($currentPageID + 1) ?>" class="uppercase text-[14px] font-bold">Pokaz więcej</p>
+        <!-- /wp:paragraph -->
+      <?php
+      } else {
+      ?>
+        <!-- wp:paragraph -->
+        <p><a href="<?php echo home_url() . '/blog/page/' . ($currentPageID - 1) ?>" class="uppercase text-[14px] font-bold">Pokaz poprzednie</p>
+        <!-- /wp:paragraph -->
+      <?php
+      }
+      ?>
       <!-- wp:paragraph -->
       <p><a href="<?php echo home_url() . '/blog/page/' . ($currentPageID + 1) ?>" class="uppercase text-[14px] font-bold"><?php echo ($currentPageID < ($post_query->max_num_pages)) ?  'Pokaż więcej' :  'Pokaż poprzednie'; ?></a></p>
       <!-- /wp:paragraph -->
