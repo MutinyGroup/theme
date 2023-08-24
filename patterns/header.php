0a04bfd3d0d2d5dcc6ae8997dd167bb47ca6b7de
Tomek <TK-54-4B>
2023-08-24 15:52:54 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -25,10 +25,10 @@
   <!-- wp:navigation /-->
 
   <!-- wp:list-item -->
-  <li class="wp-block-group lang-btn hidden desktop:block p-[10px_20px] uppercase font-medium border-[2px] border-[#AAAAAA] rounded-full absolute top-[20px] right-[20px] cursor-pointer hover:bg-white hover:border-white hover:text-black transition ease-out duration-300">
+  <li class="wp-block-group lang-btn hidden desktop:flex p-[10px_20px] uppercase font-medium border-[2px] border-[#AAAAAA] rounded-full absolute top-[20px] right-[20px] cursor-pointer hover:bg-white hover:border-white hover:text-black transition ease-out duration-300">
     <a href="https://mutiny.webo.design/se/">PL</a>
-    <a href="https://mutiny.webo.design/eng/">ENG</a>
-    <a href="https://mutiny.webo.design/se/">SE</a>
+    <a class="max-h-0 group-hover:max-h-fit transition ease-out duration-300" href="https://mutiny.webo.design/eng/">ENG</a>
+    <a class="max-h-0 group-hover:max-h-fit transition ease-out duration-300" href="https://mutiny.webo.design/se/">SE</a>
   </li>
   <!-- /wp:list-item -->
 </ul>
