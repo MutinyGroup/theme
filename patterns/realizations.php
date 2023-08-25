dca7a1cec4811f5b11382da817aeecfde39e5558
Tomek <TK-54-4B>
2023-08-25 19:00:06 +0200

    wp:tag fix

--- patterns/realizations.php
+++ patterns/realizations.php
@@ -54,6 +54,7 @@
       </div>
       <!-- /wp:group -->
 
+      <!-- wp:group -->
       <div class="wp-block-group overflow-hidden">
         <!-- wp:paragraph -->
         <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="130" class="section-title uppercase text-primaryYellow">Realizacje</p>
