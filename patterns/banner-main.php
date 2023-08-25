a6a7a15dbb0c90238a953a38d9cf87d5e83bca6d
Tomek <TK-54-4B>
2023-08-25 11:48:59 +0200

    data attr test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -61,8 +61,8 @@
 
 
 
-        <!-- wp:group {"attributes":"data-header"} -->
-        <div data-header class="wp-block-group group title-text-wrapper mb-[100px] desktop:mb-[70px] uppercase relative z-[1]">
+        <!-- wp:group {"attributes":"[data-type="header"]"} -->
+        <div data-type="header" class="wp-block-group group title-text-wrapper mb-[100px] desktop:mb-[70px] uppercase relative z-[1]">
           <!-- wp:paragraph -->
           <p data-test class="company-title group font-normal text-[16px] leading-[125%] overflow-hidden">
             <span data-test class="block 1translate-y-[100%] 1group-hover:translate-y-0 transition ease-out 1duration-300">
