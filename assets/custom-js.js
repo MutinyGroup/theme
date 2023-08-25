0f92fdd9fd1d95e64de453a565770d4eebed2d2a
Tomek <TK-54-4B>
2023-08-25 22:54:15 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -5,13 +5,16 @@ window.addEventListener("load", function () {
       function scrollPage() {
         let scrollPos = window.scrollY;
 
-        if (scrollPos > 1000 + (i*100)) {
+        if (scrollPos > 1000)  {
+
+          if (scrollPos > 1000 + (i*100)) {
           console.log('>1200');
           for(let i = 0 ; i < 10 ; i++){
             servicesTile[i].classList.add('test01');
             servicesTile[i].classList.replace('opacity-0', 'opacity-100');
             console.log(i);
           };
+        };
       
         // if (scrollPos > 1000) {
         //   console.log('>1200');
