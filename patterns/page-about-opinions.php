4446b1a5984eea1826800d5096c3c118c2f006fa
Tomek <TK-54-4B>
2023-08-25 20:27:50 +0200

    test

--- patterns/page-about-opinions.php
+++ patterns/page-about-opinions.php
@@ -107,10 +107,14 @@
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
-            <!-- wp:paragraph -->
-            <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
-            <!-- /wp:paragraph -->
+            <!-- wp:group -->
+            <div class="wp-block-group">
+              <!-- wp:paragraph -->
+              <p class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
             <!-- wp:group -->
             <div class="wp-block-group opinion-author my-[50px] flex gap-[20px]">
               <!-- wp:image -->
