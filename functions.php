5f820ecb8d638738e06dd59af45725bffaff7b10
Tomek <TK-54-4B>
2023-08-25 16:36:44 +0200

    test

--- functions.php
+++ functions.php
@@ -308,7 +308,7 @@ function mutinyblocks_register_styles()
 	wp_enqueue_script('swiperMain', get_theme_file_uri('/assets/swiper-main.js'), '1.0', array('swiper'));
 
 	wp_enqueue_style('custom-styles', get_theme_file_uri('/assets/styles.min.css'), '1.0', array('swiper-css'));
-	wp_enqueue_style('aos-styles', get_theme_file_uri('//unpkg.com/aos@2.3.1/dist/aos.css'), '1.0', array('aos-css'));
+	wp_enqueue_style('aos-styles', '//unpkg.com/aos@2.3.1/dist/aos.css', '1.0');
 
 	wp_enqueue_style('google-fonts-api', '//fonts.googleapis.com', '1.0');
 	wp_enqueue_style('google-fonts-gstatic', '//fonts.gstatic.com', '1.0');
