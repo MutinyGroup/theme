cdedc6366d8bc6ba1527367e0db7d95ffba2fd72
Tomek <TK-54-4B>
2023-08-25 11:11:55 +0200

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -72,7 +72,7 @@
           <!-- /wp:paragraph -->
 
           <!-- wp:paragraph -->
-          <p class="company-title group font-medium text-[16px] leading-[125%] overflow-hidden">
+          <p class="company-title group font-medium overflow-hidden">
             <span class="block 1translate-y-[100%] 1group-hover:translate-y-0 transition ease-out 1duration-300">
               Wchodzimy
             </span>
