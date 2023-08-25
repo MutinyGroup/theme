146443cd88727564487c2abfc5a414f7963cfe77
Tomek <TK-54-4B>
2023-08-25 18:32:04 +0200

    test

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -36,21 +36,21 @@
           <!-- wp:group -->
           <div class="wp-block-group overflow-hidden">
             <!-- wp:paragraph -->
-            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="130" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">(v2) Holistyczne podejście do działań</p>
+            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="100" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">(v2) Holistyczne podejście do działań</p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
           <!-- wp:group -->
           <div class="wp-block-group overflow-hidden">
             <!-- wp:paragraph -->
-            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="100" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">reklamowych sprawia, że nasza firma </p>
+            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="85" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">reklamowych sprawia, że nasza firma </p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
           <!-- wp:group -->
           <div class="wp-block-group overflow-hidden">
             <!-- wp:paragraph -->
-            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">działa<span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">jak jeden organizm</span></p>
+            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="70" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">działa<span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">jak jeden organizm</span></p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
