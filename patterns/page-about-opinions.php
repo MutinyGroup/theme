f10b6339c5ab4f150e6f15cce3322f259e0afa84
Tomek <TK-54-4B>
2023-08-25 20:20:51 +0200

    test data attr on li

--- patterns/page-about-opinions.php
+++ patterns/page-about-opinions.php
@@ -93,7 +93,7 @@
         <!-- wp:list -->
         <ul class="wp-block-list swiper-wrapper">
           <!-- wp:list-item -->
-          <li class="swiper-slide relative z-[2] group desktop:max-w-[444px] max-w-[80%] desktop:h-[430px] max-h-[530px] mr-[40px] desktop:mr-[90px]">
+          <li data-aos="fade-left" class="swiper-slide relative z-[2] group desktop:max-w-[444px] max-w-[80%] desktop:h-[430px] max-h-[530px] mr-[40px] desktop:mr-[90px]">
             <!-- wp:group -->
             <div class="wp-block-group quote-sign desktop:mb-[35px] mb-[20px] desktop:mt-0 mt-[40px]">
               <!-- wp:paragraph -->
