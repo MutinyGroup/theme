75dcd48dcd0b76e97feb084a2ccd2a873cada475
Tomek <TK-54-4B>
2023-08-25 13:49:08 +0200

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -91,7 +91,7 @@
           <div class="wp-block-group relative italic font-medium  company-title group overflow-hidden">
             <!-- wp:paragraph -->
             <p class="test01  transition ease-out 1duration-300">
-              <span class="before:content-[''] before:inline desktop:before:mr-[70px] before:mr-[5px]  desktop:before:w-[280px] before:w-[30px] desktop:before:h-[6px] before:h-[2px] before:bg-white"></span>
+              <span class="before:content-[''] before:inline-block desktop:before:mr-[70px] before:mr-[5px]  desktop:before:w-[280px] before:w-[30px] desktop:before:h-[6px] before:h-[2px] before:bg-white"></span>
               UDERZENIEM.
             </p>
             <!-- /wp:paragraph -->
