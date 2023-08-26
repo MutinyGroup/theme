e69181c7580ee654619d2916ce631054bca6779f
Tomek <TK-54-4B>
2023-08-26 16:34:53 +0200

    removed old title

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -54,30 +54,13 @@
             $currentCategory2 = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 35, 30));
             ?>
             <!-- wp:paragraph -->
-            <p class="uppercase test01 inline-block ">PRZEKARCZAĆ<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"><?php echo $currentCategory2; ?></span></p>
+            <p class="uppercase test01 inline-block "><span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"><?php echo $currentCategory2; ?></span></p>
             <!-- /wp:paragraph -->
             <!-- wp:paragraph -->
             <p class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
-
-
-
-          <!-- wp:paragraph -->
-          <p>REALIZACJE Z KATEGORII</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="relative inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">
-            <?php
-            $currentCategory2 = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 35, 30));
-            echo $currentCategory2;
-            ?>
-          </p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span>
-          <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
       </div>
