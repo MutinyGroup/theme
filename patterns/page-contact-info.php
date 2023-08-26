4c6da3787ccef062ec6b4d9e40f6a19ee9f78d31
Tomek <TK-54-4B>
2023-08-26 13:40:58 +0200

    new title addded

--- patterns/page-contact-info.php
+++ patterns/page-contact-info.php
@@ -46,9 +46,31 @@
         <!-- /wp:group -->
 
         <!-- wp:group -->
-        <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[50px] text-[24px] desktop:leading-[60px] leading-[30px]">
+        <div class="wp-block-group uslugi-title desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">
+          <!-- wp:group -->
+          <div class="wp-block-group overflow-hidden">
+            <!-- wp:paragraph -->
+            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="130" class="company-title overflow-hidden">
+              <span class="block uppercase">
+                POROZMAWIAJMY O
+              </span>
+            </p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group overflow-hidden">
+            <!-- wp:paragraph -->
+            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="85" class="uppercase test01 inline-block ">TWOIM<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">BIZNESIE</span></p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+
+
           <!-- wp:paragraph -->
-          <p class="uppercase">Porozmawiajmy o <br />Twoim<span class="relative inline desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">biznesie</span></p>
+          <p class="hidden uppercase desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">Wybierz usługę którą <br />jesteś<span class="relative desktop:text-[50px] text-[24px] uppercase italic before:content-[''] before:inline-block desktop:before:mr-[8px] before:mr-[4px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">zainteresowany</span></p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
