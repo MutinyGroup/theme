c034b9d70bcb0c0d38fa35ff84efdc20bed42c29
Tomek <TK-54-4B>
2023-08-26 13:08:26 +0200

    btn fix

--- patterns/page-realizations-banner.php
+++ patterns/page-realizations-banner.php
@@ -79,7 +79,7 @@
 
 
     <!-- wp:group -->
-    <div class="wp-block-group btn-more group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full z-[1] desktop:absolute desktop:top-[-130px] top-[310px] wide:left-[70%] desktop:left-[85%] left-[50%] desktop:translate-x-[-50%] transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer  overflow-hidden">
+    <div class="wp-block-group btn-more group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full z-[1] absolute desktop:top-[-130px] top-[320px] wide:left-[70%] desktop:left-[85%] left-[50%] translate-x-[-50%] transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer overflow-hidden">
       <!-- wp:group -->
       <div class="wp-block-group yellow-bg w-full h-full rounded-full bg-primaryYellow scale-100 group-hover:scale-[85%] absolute transition ease-in-out duration-[700ms] z-0">
         <!-- wp:paragraph -->
