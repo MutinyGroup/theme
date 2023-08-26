c0c0877b1469b1515ca66ed10a59f1e20b3573fd
Tomek <TK-54-4B>
2023-08-26 16:27:55 +0200

    wp:tag fix

--- patterns/archive-events.php
+++ patterns/archive-events.php
@@ -51,7 +51,101 @@
     </ul>
     <!-- /wp:list -->
 
+    <!-- wp:list -->
+    <ul class="wp-block-list events-list mb-[150px]">
+      <?php
+      $args = array(
+        'post_type' => 'realizacje',
+        'posts_per_page' => 10,
+        'order' => 'DESC',
+        'category_name' => $currentCategory2
+      );
+      $post_query = new WP_Query($args);
 
+      if ($post_query->have_posts()) {
+        while ($post_query->have_posts()) {
+          $post_query->the_post();
+          $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(), 'portrait');
+          if (true) { ?>
+            <!-- wp:list-item -->
+            <li class="event-tile group desktop:flex gap-[75px] mb-[50px]">
+              <!-- wp:group -->
+              <div class="wp-block-group event-image-wrapper desktop:mb-0 mb-[20px] max-h-[400px] overflow-hidden w-full rounded-[20px]">
+                <!-- wp:image -->
+                <figure class="wp-block-image event-image grayscale scale-[1] group-hover:scale-[1.04] group-hover:grayscale-0 transition ease-out duration-[500ms]">
+                  <img src="<?php echo $postImageUrl[0]; ?>" alt="realization-post-img" />
+                </figure>
+                <!-- /wp:image -->
+              </div>
+              <!-- /wp:group -->
+
+              <!-- wp:group -->
+              <div class="wp-block-group content-wrapper desktop:w-[60%] flex flex-col justify-center">
+                <!-- wp:image -->
+                <figure class="wp-block-image event-logo-color mb-[25px] w-[60px]">
+                  <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/page-realizations/logo-efc-color.png'; ?>" alt="" />
+                </figure>
+                <!-- /wp:image -->
+
+                <!-- wp:list -->
+                <ul class="wp-block-list categories-under-logo mb-[20px] flex flex-wrap gap-[10px]">
+                  <?php
+                  $categoriesArray = get_the_category();
+                  foreach ($categoriesArray as $category) {
+                    $category_link = get_category_link($category->term_id);
+                  ?>
+                    <?php
+                    if (($category->name) == 'glowna') {
+                      echo '';
+                    } else { ?>
+                      <!-- wp:list-item -->
+                      <li class="w-fit p-[0px_8px] flex justify-center items-center bg-[#F2F2F2] leading-[20px] border-[#D5D5D5] border-[2px] rounded-full uppercase text-[13px] font-semibold">
+                        <!-- wp:paragraph -->
+                        <p><?php echo $category->name ?></p>
+                        <!-- /wp:paragraph -->
+                      </li>
+                      <!-- /wp:list-item -->
+                    <?php
+                    };
+                    ?>
+
+                  <?php }; ?>
+                </ul>
+                <!-- /wp:list -->
+
+                <!-- wp:paragraph -->
+                <p class="mb-[50px] text-[35px] font-semibold leading-[42px]"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+                <!-- /wp:paragraph -->
+                <!-- wp:buttons -->
+                <div class="wp-block-buttons w-[56px] h-[56px]">
+                  <!-- wp:paragraph -->
+                  <p>
+                    <a href="<?php the_permalink(); ?>">
+                      <!-- wp:paragraph -->
+                      <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="group fill-transparent group-hover:fill-primaryYellow">
+                          <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500" />
+                          <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
+                          <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
+                        </svg>
+                      </p>
+                      <!-- /wp:paragraph -->
+                    </a>
+                  </p>
+                  <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:group -->
+              </div>
+              <!-- /wp:group -->
+            </li>
+            <!-- /wp:list-item -->
+      <?php
+          };
+        };
+      };
+      ?>
+    </ul>
+    <!-- /wp:list -->
 
     <!-- wp:group -->
     <div class="wp-block-group button-wrapper desktop:mt-[80px] mt-[40px] flex items-center justify-center gap-[64px] cursor-pointer">
