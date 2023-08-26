256481defb2d5335b5f6c6169c351c8cdd42b7ab
Tomek <TK-54-4B>
2023-08-26 16:33:00 +0200

    new title added

--- patterns/archive-banner.php
+++ patterns/archive-banner.php
@@ -6,18 +6,6 @@
  * Categories: text, banner
  * Viewport Width: 1280
  */
-
-// global $wp;
-// $wp->parse_request();
-// $currentCategory = str_replace('-',' ',($wp->query_vars)['category_name']);
-// print_r($wp->parse_request());
-// $current_url = home_url($wp->request);
-// $currentCategory = parse_url($current_url)['path'];
-// $currentCategory = substr((home_url($_SERVER['REQUEST_URI'])),36, 30);
-// $currentCategory = str_replace('-',' ',str_replace('/','',substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",35, 30)));
-// $currentCategory = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
-// $currentCategory = str_replace('/','',substr(home_url( add_query_arg( null, null )),36, 30));
-// echo $currentCategory;
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
@@ -30,25 +18,52 @@
       <div class="wp-block-group title-wrapper relative container mx-auto flex">
         <!-- wp:group -->
         <div class="wp-block-group section-title grid desktop:w-[145px] desktop:basis-auto uppercase text-primaryYellow">
-          <!-- wp:paragraph -->
-          <p class="justify-self-end pt-[3px] pr-[15px]">Archiwum</p>
-          <!-- /wp:paragraph -->
+
+
+          <!-- wp:group -->
+          <div class="wp-block-group h-fit overflow-hidden flex justify-end">
+            <!-- wp:paragraph -->
+            <p class="test01 justify-self-end pt-[3px] pr-[15px]">Archiwum</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
 
-        <!-- wp:group -->
-        <div class="wp-block-group hidden test">
-          <?php
-          // global $wp;
-          // $wp->parse_request();
-          // $currentCategory = str_replace('-',' ',($wp->query_vars)['category_name']);
-          // echo $currentCategory;
-          ?>
-        </div>
-        <!-- /wp:group -->
 
         <!-- wp:group -->
         <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
+
+
+
+          <!-- wp:group -->
+          <div class="wp-block-group overflow-hidden">
+            <!-- wp:paragraph -->
+            <p class="test01 company-title font-medium overflow-hidden">
+              <span class="block">
+                REALIZACJE Z KATEGORII
+              </span>
+            </p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group overflow-hidden">
+            <?php
+            $currentCategory2 = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 35, 30));
+            ?>
+            <!-- wp:paragraph -->
+            <p class="uppercase test01 inline-block ">PRZEKARCZAĆ<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black"><?php echo $currentCategory2; ?></span></p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+
+
           <!-- wp:paragraph -->
           <p>REALIZACJE Z KATEGORII</p>
           <!-- /wp:paragraph -->
