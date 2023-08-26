a2af83449a68260170817e510ed7295e4472ce69
Tomek <TK-54-4B>
2023-08-26 12:45:23 +0200

    test title removed

--- patterns/page-realizations-banner.php
+++ patterns/page-realizations-banner.php
@@ -34,28 +34,7 @@
         <!-- wp:group -->
         <div class="wp-block-group uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
 
-          <!-- wp:group -->
-          <div class="wp-block-group overflow-hidden">
-            <!-- wp:paragraph -->
-            <p class="test01 company-title font-medium overflow-hidden">
-              <span class="block">
-                PROJEKTY Z KTÓRYCH
-              </span>
-            </p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
 
-          <!-- wp:group -->
-          <div class="wp-block-group overflow-hidden">
-            <!-- wp:paragraph -->
-            <p class="uppercase test01 inline-block ">JESTEŚMY<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">DUMNI</span></p>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <p class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
 
 
         </div>
