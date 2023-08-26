5eae0f24635f97211310d83cb6225f012c9b085c
Tomek <TK-54-4B>
2023-08-26 12:34:48 +0200

    title fix

--- patterns/page-realizations-banner.php
+++ patterns/page-realizations-banner.php
@@ -51,15 +51,8 @@
             <!-- wp:paragraph -->
             <p class="uppercase test01 inline-block ">JESTEŚMY<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">DUMNI</span></p>
             <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-
-          <!-- wp:group -->
-          <div class="wp-block-group overflow-hidden">
-            <!-- wp:paragraph -->
-            <p class="test01 company-title font-medium overflow-hidden">
-              <span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span>
-            </p>
+            <!-- /wp:paragraph -->
+            <p class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
