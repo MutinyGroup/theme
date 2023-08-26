93085987111942e9392c0c342ff4aa426c7f82ba
Tomek <TK-54-4B>
2023-08-26 15:35:40 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -34,7 +34,6 @@ window.addEventListener("load", function () {
         if (scrollPos >= 0 + ((i*1.5)*150)) {
         eventTile[i].classList.add('test02');
         eventTile[i].classList.remove('opacity-0');
-        i++;
         console.log('i ',i);
         console.log('i*270',i*270);
         };
