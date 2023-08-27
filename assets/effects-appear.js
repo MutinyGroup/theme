fd2fb9b26db81c386800dc7f51fd1a8edb555b7c
Tomek <TK-54-4B>
2023-08-27 13:31:52 +0200

    test

--- assets/effects-appear.js
+++ assets/effects-appear.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function () {
   function scrollPage() {
     if (servicesTile && scrollPos > 1000)  {
       for(let i = 0 ; i < servicesTile.length ; i++){
-        if (scrollPos > 1000 + ((i*1.3)*180)) {
+        if (scrollPos > 1100 + ((i*1.3)*180)) {
         servicesTile[i].classList.add('test02');
         servicesTile[i].classList.remove('opacity-0');
         servicesTile[i+1].classList.add('test02');
