da15c81584761c1ec764ca80ae652a526f772f17
Tomek <TK-54-4B>
2023-08-27 13:21:33 +0200

    link fix

--- patterns/cta-bright2.php
+++ patterns/cta-bright2.php
@@ -108,7 +108,7 @@
 
       <!-- wp:paragraph -->
       <p class="z-[2] text-white group-hover:text-black transition ease-in-out duration-[550ms]">
-        <a href="#<?php echo get_home_url() . '/contact'; ?>" class="">
+        <a href="<?php echo get_home_url() . '/contact'; ?>">
           <span class="font-bold block">Poznaj</span>
           <span class="font-bold block text-[25px] uppercase">OFERTĘ</span>
         </a>
