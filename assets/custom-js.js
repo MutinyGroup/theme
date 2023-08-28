bfd16203a1e4b95bf2bc53cfee008a02ba91c0e3
Tomek <TK-54-4B>
2023-08-28 14:21:31 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -40,12 +40,14 @@ window.addEventListener("load", function () {
 
   function isInViewport(element) {
     const rect = element.getBoundingClientRect();
+    if(rect!=null){
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
+  };
   }
 
   document.addEventListener("scroll", (event) => {
