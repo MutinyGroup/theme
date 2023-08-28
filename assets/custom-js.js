50f553288c095da4b726e89f511366cc3ade9162
Tomek <TK-54-4B>
2023-08-28 14:55:09 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -50,7 +50,7 @@ window.addEventListener("load", function () {
   }
 
   document.addEventListener("scroll", (event) => {
-    if(servicesSwiper!=null){
+    if(servicesSwiper == true){
     console.log(isInViewport(servicesSwiper));
     }
 
