cbf725c7ee0542a884a412f86b0e080f583091a5
Tomek <TK-54-4B>
2023-08-28 12:40:10 +0200

    test

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -50,7 +50,7 @@
           <!-- wp:group -->
           <div class="wp-block-group overflow-hidden">
             <!-- wp:paragraph -->
-            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="85" class="uppercase inline desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">działa<span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">jak jeden organizm</span></p>
+            <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="85" class="uppercase inline desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">działa<span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">jak jeden organizm</span></p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
