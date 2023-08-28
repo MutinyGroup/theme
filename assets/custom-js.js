c1f66754bae8bd51521b2452d12df178be3211ba
Tomek <TK-54-4B>
2023-08-28 13:14:53 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -10,9 +10,35 @@ window.addEventListener("load", function () {
         console.log(entry.target);
         console.log('visible');
     })
-}); 
+  }); 
+  observer.observe(document.querySelector('#services'));
 
-observer.observe(document.querySelector('.skills__list-item'));
+
+  function isInViewport(element) {
+    // Get the bounding client rectangle position in the viewport
+    var bounding = element.getBoundingClientRect();
+    
+    // Checking part. Here the code checks if it's *fully* visible
+    // Edit this part if you just want a partial visibility
+    if (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+        console.log('In the viewport! :)');
+        return true;
+    } else {
+        console.log('Not in the viewport. :(');
+        return false;
+    }
+  };
+
+  document.addEventListener("scroll", (event) => {
+    isInViewport(servicesSwiper);
+    });
+
+
 
   function scrollPage() {
     let scrollPos = window.scrollY;
