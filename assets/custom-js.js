3045623bb4bed1b88d2a5368762133ea2ed9a3cb
Tomek <TK-54-4B>
2023-08-28 13:42:46 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -35,22 +35,21 @@ window.addEventListener("load", function () {
   document.addEventListener("scroll", update);
   // update();
 
-  let el = document.getElementById("#services");
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
-  let elementIsVisibleInViewport = (el, partiallyVisible = false) => {
-    const { top, left, bottom, right } = el.getBoundingClientRect();
-    const { innerHeight, innerWidth } = window;
-    return partiallyVisible
-      ? ((top > 0 && top < innerHeight) ||
-          (bottom > 0 && bottom < innerHeight)) &&
-          ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
-      : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
-  };
+  document.addEventListener("scroll", (event) => {
+    isInViewport(servicesSwiper);
 
-
-  // e.g. 100x100 viewport and a 10x10px element at position {top: -1, left: 0, bottom: 9, right: 10}
-elementIsVisibleInViewport(el); // false - (not fully visible)
-elementIsVisibleInViewport(el, true); // true - (partially visible)
+    });
+  
 
 
 
