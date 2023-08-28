f1806cda5704485b7b2369063b3272bf106be8ea
Tomek <TK-54-4B>
2023-08-29 01:54:56 +0200

    one column layout

--- patterns/single-realizacje.php
+++ patterns/single-realizacje.php
@@ -42,19 +42,6 @@
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group -->
-    <div class="hidden loop-test wp-block-group">
-      <!-- wp:paragraph -->
-      <p>php loop test2</p>
-      <!-- /wp:paragraph -->
-      <?php while (have_posts()) : the_post(); ?>
-        <!-- wp:paragraph -->
-        <p><?php the_title(); ?></p>
-        <?php echo 'test'; ?>
-        <!-- /wp:paragraph -->
-      <?php endwhile; ?>
-    </div>
-    <!-- /wp:group -->
 
     <!-- wp:paragraph -->
     <a href="/realizations/" class="group mt-[60px] mb-[40px] w-fit font-semibold flex items-center gap-[8px]">
@@ -123,51 +110,13 @@
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group -->
-    <div class="wp-block-group layout-two-cols mb-[50px] desktop:flex gap-[50px]">
-      <!-- wp:group -->
-      <div class="wp-block-group post-content desktop:w-[70%]">
-        <!-- wp:post-content /-->
-      </div>
-      <!-- /wp:group -->
 
-      <!-- wp:group -->
-      <div class="wp-block-group share-wrapper desktop:w-[30%] h-[80px]">
-        <!-- wp:group -->
-        <div class="wp-block-group share-border w-fit pt-[25px] border-[#D5D5D5] border-t-[2px] flex gap-[20px]">
-          <!-- wp:paragraph -->
-          <p class="uppercase text-right font-semibold leading-[19px]">UDOSTĘPNIJ <br> ARTYKUŁ</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:list -->
-          <ul class="wp-block-list flex gap-[20px]">
-            <!-- wp:list-item -->
-            <li class="cursor-pointer group">
-              <!-- wp:paragraph -->
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" class="group">
-                <path d="M24.0088 21.125L24.5087 17.8674H21.383V15.7535C21.383 14.8623 21.8196 13.9936 23.2195 13.9936H24.6406V11.2201C24.6406 11.2201 23.351 11 22.1181 11C19.5439 11 17.8614 12.5602 17.8614 15.3847V17.8674H15V21.125H17.8614V29H21.383V21.125H24.0088Z" fill="black" />
-                <rect x="1" y="1" width="38" height="38" rx="19" stroke="#D5D5D5" stroke-width="2" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500" />
-              </svg>
-              <!-- /wp:paragraph -->
-            </li>
-            <!-- /wp:list-item -->
-            <!-- wp:list-item -->
-            <li class="cursor-pointer group">
-              <!-- wp:paragraph -->
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" class="group">
-                <path d="M11.3484 29H14.8323V16.5125H11.3484V29ZM13.0903 11C11.929 11 11 11.9 11 13.025C11 14.15 11.929 15.05 13.0903 15.05C14.2516 15.05 15.1806 14.15 15.1806 13.025C15.1806 11.9 14.2516 11 13.0903 11ZM20.7548 18.425V16.5125H17.271V29H20.7548V22.5875C20.7548 18.9875 25.5161 18.7625 25.5161 22.5875V29H29V21.35C29 15.275 22.3806 15.5 20.7548 18.425Z" fill="black" />
-                <rect x="1" y="1" width="38" height="38" rx="19" stroke="#D5D5D5" stroke-width="2" class="group-hover:stroke-primaryYellow transition-stroke ease-out duration-500" />
-              </svg>
-              <!-- /wp:paragraph -->
-            </li>
-            <!-- /wp:list-item -->
-          </ul>
-          <!-- /wp:list -->
-        </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:group -->
+    <!-- wp:group -->
+    <div class="wp-block-group post-content desktop:w-[100%]">
+      <!-- wp:post-content /-->
     </div>
     <!-- /wp:group -->
+
 
     <!-- wp:group -->
     <div class="wp-block-group border-wrapper pt-[20px] border-opacity-[40%] border-[#D5D5D5] border-t-[2px]">
