a59d88d1d15ec9991a75468bc831366646c89f5c
Tomek <TK-54-4B>
2023-08-28 23:08:08 +0200

    test before inlone block

--- patterns/page-services-why.php
+++ patterns/page-services-why.php
@@ -128,7 +128,7 @@
               <ul class="wp-block-list leading-[32px] font-semibold">
                 <!-- wp:list-item -->
                 <li class="flex items-center gap-[10px] ">
-                  <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="85" class="inline before:content-[''] before:inline before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow">Lorem ipsum dolor sit amet</p>
+                  <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="85" class="inline before:content-[''] before:inline-block before:mr-[5px] before:mb-[5px] before:w-[20px] before:h-[2px] before:bg-primaryYellow">Lorem ipsum dolor sit amet</p>
                 </li>
                 <!-- /wp:list-item -->
                 <!-- wp:list-item -->
