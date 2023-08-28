d2cac99ce3c8b14319226c1f294572ff81c5ab71
Tomek <TK-54-4B>
2023-08-29 01:48:08 +0200

    new test

--- patterns/page-services-marketing-banner.php
+++ patterns/page-services-marketing-banner.php
@@ -38,64 +38,47 @@
   </div>
   <!-- /wp:group -->
   <!-- wp:group -->
-  <div class="wp-block-group services-marketing-banner-img relative">
-    <!-- wp:image -->
-    <figure class="wp-block-image object-cover h-full">
-      <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/page-marketing/marketing-banner.jpg'; ?>" alt="bg-image" />
-    </figure>
-    <!-- /wp:image -->
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:group -->
-  <div class="wp-block-group paragraph-wrapper container mx-auto desktop:px-0 px-[20px] relative w-full desktop:mb-[150px] mb-[50px] desktop:pt-[100px] pt-[50px]">
-    <!-- wp:paragraph -->
-    <p>
-      <svg class="bg-shape scale-[0.6] opacity-[80%] desktop:scale-[1] z-[0] absolute top-0 desktop:right-[0px] right-auto left-[-40px] desktop:left-auto" width="470" height="396" viewBox="0 0 470 396" fill="none">
-        <path d="M468.243 393.924L1 236.702V1.9702L468.243 159.192V393.924Z" stroke="#FFFFFF" stroke-width="2" />
-      </svg>
-    </p>
-    <!-- /wp:paragraph -->
+  <div class="wp-block-group about-banner-img relative h-[500px] desktop:h-auto">
     <!-- wp:group -->
-    <div class="wp-block-group paragraph-wrapper desktop:px-0 px-[20px] desktop:ml-[145px] relative">
-      <!-- wp:paragraph -->
-      <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="130" class="desktop:text-[35px] text-[22px] font-medium desktop:leading-[42px] leading-[32px] desktop:w-[897px]">Jesteśmy ekspertami od komunikacji i marketingu. Pracujemy dla topowych firm, stawiających przed nami duże wyzwania.</p>
-      <!-- /wp:paragraph -->
+    <div class="wp-block-group overflow-hidden w-full h-[350px] desktop:h-[500px]">
+      <!-- wp:image -->
+      <figure class="wp-block-image h-full w-[920px] desktop:w-auto">
+        <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/page-about/about-banner-bg.jpg'; ?>" alt="bg-image" />
+      </figure>
+      <!-- /wp:image -->
     </div>
     <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
 
 
-  <!-- wp:group -->
-  <div class="wp-block-group btn-more group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full z-[1]  desktop:absolute desktop:top-[80px] desktop:left-[85%] desktop:translate-x-[-50%] transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer  overflow-hidden">
     <!-- wp:group -->
-    <div class="wp-block-group yellow-bg w-full h-full rounded-full bg-primaryYellow scale-100 group-hover:scale-[85%] absolute transition ease-in-out duration-[700ms] z-0">
+    <div class="wp-block-group btn-more group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full z-[1]  absolute desktop:top-[-130px] bottom-[80px] wide:left-[70%] desktop:left-[85%] left-[50%] translate-x-[-50%] transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer  overflow-hidden">
+      <!-- wp:group -->
+      <div class="wp-block-group yellow-bg w-full h-full rounded-full bg-primaryYellow scale-100 group-hover:scale-[85%] absolute transition ease-in-out duration-[700ms] z-0">
+        <!-- wp:paragraph -->
+        <p class="hidden">yellow bg</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group white-bg w-full h-full rounded-full bg-white scale-0 group-hover:scale-[85%] absolute transition ease-in-out duration-[500ms] z-[1]">
+        <!-- wp:paragraph -->
+        <p class="hidden">white bg</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
       <!-- wp:paragraph -->
-      <p class="hidden">yellow bg</p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
-    <!-- wp:group -->
-    <div class="wp-block-group white-bg w-full h-full rounded-full bg-white scale-0 group-hover:scale-[85%] absolute transition ease-in-out duration-[500ms] z-[1]">
-      <!-- wp:paragraph -->
-      <p class="hidden">white bg</p>
+      <p class="z-[2] text-white group-hover:text-black transition ease-in-out duration-[550ms]"><a href="#Poznaj nas"><span class="font-bold block">Zobacz</span><span class="font-bold block text-[25px]">WIĘCEJ</span></a></p>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:paragraph -->
-    <p class="z-[2] text-white group-hover:text-black transition ease-in-out duration-[550ms]"><a href="#Poznaj nas"><span class="font-bold block">Zobacz</span><span class="font-bold block text-[25px]">WIĘCEJ</span></a></p>
-    <!-- /wp:paragraph -->
-  </div>
-  <!-- /wp:group -->
 
 
-  <!-- wp:buttons -->
-  <div class="wp-block-buttons old hidden desktop:w-[175px] mx-auto w-[140px] desktop:h-[175px] h-[140px] text-center rounded-full desktop:border-[4px] border-[3px] border-[#AAAAAA] hover:border-none hover:bg-primaryYellow hover:text-white transition ease-out duration-300 fflex flex-col justify-center items-center gap-y-0 cursor-pointer z-[1] relative desktop:absolute desktop:top-[80px] desktop:left-[85%] desktop:translate-x-[-50%]">
-    <!-- wp:paragraph -->
-    <p><a href="#O nas"><span class="font-bold block">Zobacz</span><span class="font-bold block text-[25px]">WIĘCEJ</span></a></p>
-    <!-- /wp:paragraph -->
+
+
+
+
   </div>
   <!-- /wp:buttons -->
 </div>
