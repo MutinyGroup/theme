4089ca527df0e824eba3069deb7cd528ffc3dbca
Tomek <TK-54-4B>
2023-08-29 09:58:44 +0200

    animation added

--- patterns/page-realizations-banner.php
+++ patterns/page-realizations-banner.php
@@ -72,7 +72,7 @@
     <!-- wp:group -->
     <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] mb-[80px]">
       <!-- wp:paragraph -->
-      <p class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px] mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+      <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="130" class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px] mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
       <!-- /wp:paragraph -->
 
       <!-- wp:group -->
