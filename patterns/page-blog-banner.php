8a6f142a79b9dbf1bc3a2b4f5416b99c8da69e99
Tomek <TK-54-4B>
2023-08-29 12:40:19 +0200

    test animation

--- patterns/page-blog-banner.php
+++ patterns/page-blog-banner.php
@@ -47,7 +47,7 @@
           <!-- wp:group -->
           <div class="wp-block-group overflow-hidden">
             <!-- wp:paragraph -->
-            <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="130" class="uppercase test01 inline-block ">U NAS<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">SŁYCHAĆ</span></p>
+            <p class="uppercase test01 inline-block ">U NAS<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">SŁYCHAĆ</span></p>
             <!-- /wp:paragraph -->
             <!-- wp:paragraph -->
             <p class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></p>
@@ -67,7 +67,7 @@
     <!-- wp:group -->
     <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] mb-[80px]">
       <!-- wp:paragraph -->
-      <p class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px] mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+      <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="130" class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px] mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
       <!-- /wp:paragraph -->
 
       <!-- wp:group -->
