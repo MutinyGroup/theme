c0fddbd63f5d2d42422c954f35945d67fca14865
Tomek <TK-54-4B>
2023-08-29 12:57:26 +0200

    style fix

--- patterns/blog.php
+++ patterns/blog.php
@@ -252,7 +252,7 @@
       <ul class="wp-block-list min-w-fit">
         <!-- wp:list-item -->
         <li class="wp-block-group">
-          <a href="<?php echo get_home_url() . '/blog'; ?>" class="uppercase group bg-black text-white text-[14px] font-bold min-w-fit flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] rounded-full hover:bg-white hover:text-black">
+          <a href="<?php echo get_home_url() . '/blog'; ?>" class="uppercase group bg-darkBlack text-white text-[14px] font-bold min-w-fit flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] rounded-full hover:bg-white hover:text-black">
             <!-- wp:group -->
             <div class="wp-block-group overflow-hidden h-[18px]">
               <!-- wp:group -->
