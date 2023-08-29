89ee5a060a23f3496618d38e5fc5ab546aca84ee
Tomek <TK-54-4B>
2023-08-29 16:22:44 +0200

    animation added

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -295,7 +295,7 @@
           <!-- wp:group -->
           <div class="new wp-block-group gray-border hidden desktop:inline-block w-[285px] h-[2px] bg-[#808080] opacity-[15%]">
             <!-- wp:paragraph -->
-            <p class="hidden">gray separator</p>
+            <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="130" class="hidden">gray separator</p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
