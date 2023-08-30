5e31be5c6cbee8cca8f23ebad21ca131d79fa950
Tomek <TK-54-4B>
2023-08-30 15:58:36 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -12,12 +12,12 @@ function titleImgChange(){
   if(currentScrollY > 4530 && currentScrollY < 4630){
     titleImg1.classList.replace('opacity-1', 'opacity-0');
     titleImg2.classList.replace('opacity-0', 'opacity-1');
-    paginationScroll.classList.add('translate-y-[-60px]');
+    paginationScroll.classList.add('translate-y-[-30px]');
     console.log('change');
   }else if(currentScrollY > 5100 && currentScrollY < 5200){
     titleImg2.classList.replace('opacity-1', 'opacity-0');
     titleImg3.classList.replace('opacity-0', 'opacity-1');
-    paginationScroll.classList.add('translate-y-[-120px]');
+    paginationScroll.classList.add('translate-y-[-60px]');
     console.log('change2');
   }
   if(currentScrollY < 4530 && currentScrollY > 4000){
@@ -29,7 +29,7 @@ function titleImgChange(){
   if(currentScrollY < 5100 && currentScrollY > 4530){
     titleImg2.classList.replace('opacity-0', 'opacity-1');
     titleImg3.classList.replace('opacity-1', 'opacity-0');
-    paginationScroll.classList.add('translate-y-[-60px]');
+    paginationScroll.classList.add('translate-y-[-30px]');
     console.log('change4');
   };
 };
