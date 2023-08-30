90c597bc8a25e81526eb794045a8d37f2efac71d
Tomek <TK-54-4B>
2023-08-30 13:27:30 +0200

    test

--- patterns/blog.php
+++ patterns/blog.php
@@ -57,7 +57,7 @@
       <!-- wp:group -->
       <div class="wp-block-group h-fit overflow-hidden">
         <!-- wp:paragraph -->
-        <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="50" class="section-title uppercase text-primaryYellow">Aktualności</p>
+        <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="section-title uppercase text-primaryYellow">Aktualności</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -67,14 +67,14 @@
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph -->
-          <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="50" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">Aktualności i wydarzenia</p>
+          <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">Aktualności i wydarzenia</p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph {"templateLock":"contentOnly"} -->
-          <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="50" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">z Mutiny Group</p>
+          <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">z Mutiny Group</p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
