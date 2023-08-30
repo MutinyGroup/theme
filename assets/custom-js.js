0074004ccce17cd6bd8802cce417ea2ef3c8847c
Tomek <TK-54-4B>
2023-08-30 16:12:39 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -12,12 +12,12 @@ function titleImgChange(){
   if(currentScrollY > 4530 && currentScrollY < 4630){
     titleImg1.classList.replace('opacity-1', 'opacity-0');
     titleImg2.classList.replace('opacity-0', 'opacity-1');
-    paginationScroll.classList.add('translate-y-[-30px]');
+    paginationScroll.classList.add('translate-y-[-42px]');
     console.log('change');
   }else if(currentScrollY > 5100 && currentScrollY < 5200){
     titleImg2.classList.replace('opacity-1', 'opacity-0');
     titleImg3.classList.replace('opacity-0', 'opacity-1');
-    paginationScroll.classList.replace('translate-y-[-30px]', 'translate-y-[-60px]');
+    paginationScroll.classList.replace('translate-y-[-30px]', 'translate-y-[-84px]');
     console.log('change2');
   }
   if(currentScrollY < 4530 && currentScrollY > 4000){
