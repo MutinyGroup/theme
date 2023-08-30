bae830177e06f58e8598a5e32a0d4db4549321e7
Tomek <TK-54-4B>
2023-08-30 15:30:40 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -23,7 +23,7 @@ function titleImgChange(){
     titleImg2.classList.replace('opacity-1', 'opacity-0');
     console.log('change3');
   };
-  if(currentScrollY < 5100 && currentScrollY < 4530){
+  if(currentScrollY < 5100 && currentScrollY > 4530){
     titleImg2.classList.replace('opacity-0', 'opacity-1');
     titleImg3.classList.replace('opacity-1', 'opacity-0');
     console.log('change3');
