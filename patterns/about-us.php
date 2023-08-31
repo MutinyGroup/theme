183fca4ee086bc729ee60e410a6e5cc2b613e5cd
Tomek <TK-54-4B>
2023-08-31 16:47:32 +0200

    test

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -122,7 +122,7 @@
               <!-- /wp:group -->
 
               <!-- wp:paragraph -->
-              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">01</p>
+              <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="85" class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold">01</p>
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
@@ -130,10 +130,10 @@
             <!-- wp:group -->
             <div class="wp-block-group text-wrapper">
               <!-- wp:paragraph -->
-              <p class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
+              <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="85" class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]">Mamy plan</p>
               <!-- /wp:paragraph -->
               <!-- wp:paragraph -->
-              <p class="target-paragraph font-light leading-[22px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+              <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="85" class="target-paragraph font-light leading-[22px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
               <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
