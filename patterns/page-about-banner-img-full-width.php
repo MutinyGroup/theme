a49fd29b317216a596b9d98dfc79493ff259e477
Tomek <TK-54-4B>
2023-08-31 16:46:07 +0200

    test

--- patterns/page-about-banner-img-full-width.php
+++ patterns/page-about-banner-img-full-width.php
@@ -40,7 +40,7 @@
           <!-- wp:group -->
           <div class="wp-block-group overflow-hidden">
             <!-- wp:paragraph -->
-            <p class="uppercase test01 inline-block ">PRZEKRACZAĆ<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">GRANIC</span><span class="desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></p>
+            <p class="uppercase test01 inline-block ">PRZEKRACZAĆ<span class="relative desktop:ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">GRANIC</span><span class="desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></p>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
