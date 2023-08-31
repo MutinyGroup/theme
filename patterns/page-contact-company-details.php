c9413195fa2fccf7525745ac50a5e2f76176dcc8
Tomek <TK-54-4B>
2023-08-31 17:22:00 +0200

    test

--- patterns/page-contact-company-details.php
+++ patterns/page-contact-company-details.php
@@ -87,9 +87,10 @@
         <!-- wp:list -->
         <ul class="wp-block-list">
           <!-- wp:list-item -->
-          <li><span class="font-medium">NIP: </span>9571133012</li>
-          <!-- /wp:list-item -->
-          <!-- wp:list-item -->
+          <li>
+            <p data-aos="fade-righ" data-aos-duration="600" data-aos-offset="30"><span class="font-medium">NIP: </span>9571133012</p< /li>
+              <!-- /wp:list-item -->
+              <!-- wp:list-item -->
           <li><span class="font-medium">REGON: </span>388790205</li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
