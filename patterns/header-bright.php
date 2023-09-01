310d2b3ed314530d56df16f47c0e323c77a0711d
Tomek <TK-54-4B>
2023-09-02 00:06:39 +0200

    test

--- patterns/header-bright.php
+++ patterns/header-bright.php
@@ -55,10 +55,10 @@
     <!-- wp:list-item -->
     <li class="lang-btn group h-[48px] hover:h-[120px] hidden desktop:block p-[10px_12px] text-center uppercase font-medium border-[2px] border-[#AAAAAA] rounded-[50px] hover:rounded-[20px] absolute top-[20px] right-[20px] cursor-pointer hover:bg-white hover:border-white hover:text-black transition-height ease-out duration-300">
       <!-- wp:paragraph -->
-      <p class="block hover:text-primaryYellow group-hover:mb-[10px]">SE</p>
+      <p class="block hover:text-primaryYellow group-hover:mb-[10px]">PL</p>
       <!-- /wp:paragraph -->
-      <a class="block hover:text-primaryYellow group-hover:mb-[10px] opacity-0 group-hover:opacity-100 transition ease-out duration-300" href="http://mutiny.webo.design/" href="http://mutiny.webo.design/">PL</a>
-      <a class="block hover:text-primaryYellow opacity-0 group-hover:opacity-100 transition ease-out duration-300" href="http://mutiny.webo.design/eng/">ENG</a>
+      <a class="block hover:text-primaryYellow group-hover:mb-[10px] opacity-0 group-hover:opacity-100 transition ease-out duration-300" href="http://mutiny.webo.design/eng/">ENG</a>
+      <a class="block hover:text-primaryYellow opacity-0 group-hover:opacity-100 transition ease-out duration-300" href="http://mutiny.webo.design/se/">SE</a>
     </li>
     <!-- /wp:list-item -->
   <?php
