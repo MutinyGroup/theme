d6a7b84465fecf5c839a0464b1164d32baa8d9d1
Tomek <TK-54-4B>
2023-09-01 22:09:11 +0200

    test

--- patterns/page-services-marketing-content4.php
+++ patterns/page-services-marketing-content4.php
@@ -194,7 +194,7 @@
                   <p class="pb-[30px] text-[20px] font-medium">Lorem ipsum dolor sit amet</p>
                   <!-- /wp:paragraph -->
                   <!-- wp:paragraph -->
-                  <p class="leading-[21px] hidden desktop:block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <p class="leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                   <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -256,7 +256,7 @@
                   <p class="pb-[30px] text-[20px] font-medium">Lorem ipsum dolor sit amet</p>
                   <!-- /wp:paragraph -->
                   <!-- wp:paragraph -->
-                  <p class="leading-[21px] hidden desktop:block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <p class="leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                   <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -325,7 +325,7 @@
                   <p class="pb-[30px] text-[20px] font-medium">Lorem ipsum dolor sit amet</p>
                   <!-- /wp:paragraph -->
                   <!-- wp:paragraph -->
-                  <p class="leading-[21px] hidden desktop:block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <p class="leading-[21px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                   <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
