b804e7b2eddec7fd04d30c6e337a991a8207a444
Tomek <TK-54-4B>
2023-09-01 22:16:49 +0200

    test

--- patterns/page-services-marketing-content4.php
+++ patterns/page-services-marketing-content4.php
@@ -149,7 +149,7 @@
               <!-- wp:group -->
               <div class="wp-block-group swiper-slide swiper-slide-marketing relative desktop:p-[25px] desktop:max-w-[716px] max-w-[310px] desktop:h-[450px] h-fit mr-[8px] desktop:flex gap-[43px]">
                 <!-- wp:group -->
-                <div class="wp-block-group desktop:w-[50%] h-[270px] rounded-[20px] overflow-hidden">
+                <div class="wp-block-group mb-[20px] desktop:w-[50%] h-[270px] rounded-[20px] overflow-hidden">
                   <!-- wp:image -->
                   <figure class="wp-block-image clip-path-url-['#clipped-content4'] h-full">
                     <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/page-marketing/swiper/swiper2-1.jpg'; ?>" alt="marketing-backgroun-image">
@@ -204,7 +204,7 @@
               <!-- wp:group -->
               <div class="wp-block-group swiper-slide swiper-slide-marketing relative desktop:p-[25px] desktop:max-w-[716px] max-w-[310px] desktop:h-[450px] h-fit mr-[8px] desktop:flex gap-[43px]">
                 <!-- wp:group -->
-                <div class="wp-block-group desktop:w-[50%] h-[270px] rounded-[20px] overflow-hidden">
+                <div class="wp-block-group mb-[20px] desktop:w-[50%] h-[270px] rounded-[20px] overflow-hidden">
                   <!-- wp:image -->
                   <figure class="wp-block-image clip-path-url-['#clipped-content4'] h-full">
                     <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/page-marketing/swiper/swiper2-1.jpg'; ?>" alt="marketing-backgroun-image">
@@ -266,7 +266,7 @@
               <!-- wp:group -->
               <div class="wp-block-group swiper-slide swiper-slide-marketing relative desktop:p-[25px] desktop:max-w-[716px] max-w-[310px] desktop:h-[450px] h-fit mr-[8px] desktop:flex gap-[43px]">
                 <!-- wp:group -->
-                <div class="wp-block-group desktop:w-[50%] h-[270px] rounded-[20px] overflow-hidden">
+                <div class="wp-block-group mb-[20px] desktop:w-[50%] h-[270px] rounded-[20px] overflow-hidden">
                   <!-- wp:image -->
                   <figure class="wp-block-image clip-path-url-['#clipped-content4'] h-full">
                     <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/page-marketing/swiper/swiper2-1.jpg'; ?>" alt="marketing-backgroun-image">
