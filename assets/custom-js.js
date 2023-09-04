e936c8f2871238a6aa73f343efb6465ee731bc05
Tomek <TK-54-4B>
2023-09-04 13:48:35 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -114,7 +114,7 @@ if(document.body.classList.contains('page-id-101')){
 
   if(document.body.classList.contains('page-id-110')){
     showMoreBtn.addEventListener('click',()=>{
-      realizationsContainer.style.maxHeight = 1500 + more + 'px';
+      realizationsContainer.style.maxHeight = ((window.innerWidth > 480) ? 1500 : 1820) + more + 'px';
       (window.innerWidth > 480) ? more += 900 : more += 1100;
     });
 };
