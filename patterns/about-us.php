313f8445e3e05b80607165e159d0be79343141eb
Tomek <TK-54-4B>
2023-09-28 15:07:18 +0200

    test

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -267,14 +267,7 @@
           <ul class="wp-block-list">
             <!-- wp:list-item -->
             <li class="wp-block-group">
-              <a href="<?php echo get_home_url() . '/about'; ?>" class="uppercase group bg-darkBlack text-white text-[14px] font-bold min-w-[225px] flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] rounded-full hover:bg-white hover:text-black" data-aos="fade-right" data-aos-duration="600" data-aos-offset="130">
-                <span class=" overflow-hidden h-[18px]">
-                  <span class=" overflow-hidden">
-                    <span class=" translate-y-0 group-hover:translate-y-[-22px] transition ease duration-[300ms]">Dowiedz się więcej</span>
-                    <span class=" translate-y-0 group-hover:translate-y-[-22px] transition ease duration-[300ms]">Dowiedz się więcej</span>
-                  </span>
-                </span>
-              </a>
+              <a href="<?php echo get_home_url() . '/about'; ?>" class="uppercase group bg-darkBlack text-white text-[14px] font-bold min-w-[225px] flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] rounded-full hover:bg-white hover:text-black" data-aos="fade-right" data-aos-duration="600" data-aos-offset="130"><span class=" overflow-hidden h-[18px]"><span class=" overflow-hidden"><span class=" translate-y-0 group-hover:translate-y-[-22px] transition ease duration-[300ms]">Dowiedz się więcej</span><span class=" translate-y-0 group-hover:translate-y-[-22px] transition ease duration-[300ms]">Dowiedz się więcej</span></span></span></a>
             </li>
             <!-- /wp:list-item -->
           </ul>
