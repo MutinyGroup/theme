ce29d3c071587618e887ba13483456a8af36b4c6
Tomek <TK-54-4B>
2023-10-25 12:51:28 +0200

    btn fix

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -263,21 +263,17 @@
 
         <!-- wp:group -->
         <div class="wp-block-group button-wrapper max-w-[535px] flex justify-center items-center gap-[64px]">
-          <!-- wp:list -->
-          <ul class="wp-block-list">
-            <!-- wp:list-item -->
-            <li class="wp-block-group">
-              <a href="<?php echo get_home_url() . '/about'; ?>" class="uppercase btn-text-roll group bg-darkBlack text-white text-[14px] font-bold min-w-[225px] flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] rounded-full hover:bg-white hover:text-black" data-aos="fade-right" data-aos-duration="600" data-aos-offset="130">
-                <span class="overflow-hidden h-[18px]">
-                  <span class="overflow-hidden">
-                    <span class="inline-block">Dowiedz się więcej</span>
-                  </span>
-                </span>
-              </a>
-            </li>
-            <!-- /wp:list-item -->
-          </ul>
-          <!-- /wp:list -->
+
+          <!-- wp:paragraph -->
+          <a href="<?php echo get_home_url() . '/about'; ?>" class="uppercase btn-text-roll group bg-darkBlack text-white text-[14px] font-bold min-w-[225px] flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] rounded-full hover:bg-white hover:text-black" data-aos="fade-right" data-aos-duration="600" data-aos-offset="130">
+            <span class="overflow-hidden h-[18px]">
+              <span class="overflow-hidden">
+                <span class="inline-block">Dowiedz się więcej</span>
+              </span>
+            </span>
+          </a>
+          <!-- wp:paragraph -->
+
 
           <!-- wp:paragraph -->
           <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="130" class="new wp-block gray-border hidden desktop:inline-block w-[285px] h-[2px] bg-[#808080] opacity-[15%]">
