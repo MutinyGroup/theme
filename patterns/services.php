5efd543f6206d961c59fe9ac5475e6e9b47e6982
Tomek <TK-54-4B>
2023-10-26 09:10:51 +0200

    test

--- patterns/services.php
+++ patterns/services.php
@@ -100,10 +100,7 @@
     <!-- wp:group -->
     <div class="wp-block-group mx-[0px] desktop:ml-[360px] desktop:mb-[0px] mb-[40px] max-w-full desktop:max-w-[400px] desktop:leading-[30px] leading-[26px] desktop:text-[20px] text-[15px] font-light relative z-[2]">
       <!-- wp:paragraph -->
-      <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="85" class="inline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi est similique, sint</p><!-- wp:paragraph -->
-      <p class="inline text-primaryYellow">illum architecto</p><!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <p class="inline">fugit quae beatae</p>
+      <p data-aos="fade-right" data-aos-duration="600" data-aos-offset="85" class="targets-title desktop:mb-[80px] mb-[60px] max-w-[535px] text-[18px] font-light leading-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <span class="text-primaryYellow">Ut enim ad minim veniam</span>, quis nostrud exercitation.</p>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
