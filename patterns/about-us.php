835001fd423573919c47d04c0c5b13c16d3a6788
Tomek <TK-54-4B>
2023-10-26 12:04:20 +0200

    test

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -30,7 +30,8 @@
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph -->
           <p class="uppercase inline desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="85">Holistyczne podejście do działań reklamowych sprawia, że nasza firma działa</span>
-            <!-- wp:paragraph -->
+          </p>
+          <!-- wp:paragraph -->
           <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">jak jeden organizm</p>
           <!-- /wp:paragraph -->
           </p>
