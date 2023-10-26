9821b9bd0e12ac5c9383ea5680f2b7bed5e38a52
Tomek <TK-54-4B>
2023-10-26 18:10:24 +0200

    test

--- patterns/page-about-opinions.php
+++ patterns/page-about-opinions.php
@@ -27,7 +27,7 @@
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph -->
-          <span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="uppercase desktop:text-[35px] text-[26px] font-medium leading-[40px]">Co mówią nasi klienci?</span>
+          <span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="block uppercase desktop:text-[35px] text-[26px] font-medium leading-[40px]">Co mówią nasi klienci?</span>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -104,7 +104,7 @@
             </div>
             <!-- /wp:group -->
             <!-- wp:paragraph -->
-            <span data-aos="fade-right" data-aos-duration="600" data-aos-offset="30" class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            <span data-aos="fade-right" data-aos-duration="600" data-aos-offset="30" class="block opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </span>
             <!-- /wp:paragraph -->
             <!-- wp:group -->
@@ -142,7 +142,7 @@
             </div>
             <!-- /wp:group -->
             <!-- wp:paragraph -->
-            <span data-aos="fade-right" data-aos-duration="600" data-aos-offset="30" class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            <span data-aos="fade-right" data-aos-duration="600" data-aos-offset="30" class="block opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </span>
             <!-- /wp:paragraph -->
             <!-- wp:group -->
@@ -180,7 +180,7 @@
             </div>
             <!-- /wp:group -->
             <!-- wp:paragraph -->
-            <span data-aos="fade-right" data-aos-duration="600" data-aos-offset="30" class="opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            <span data-aos="fade-right" data-aos-duration="600" data-aos-offset="30" class="block opinion-text desktop:text-[20px] text-[18px] font-light mb-[50px] desktop:mt-0 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </span>
             <!-- /wp:paragraph -->
             <!-- wp:group -->
