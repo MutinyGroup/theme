da45b5c5bbcdd9c3597facd486d9d7a772dbac30
Tomek <TK-54-4B>
2023-10-26 08:15:31 +0200

    test

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -266,13 +266,13 @@
 
           <!-- wp:group -->
           <div class="wp-block-group uppercase btn-text-roll group bg-darkBlack text-white text-[14px] font-bold min-w-[225px] flex justify-center items-center px-[20px] btn-more ml-[0px] p-[15px_30px] border-[2px] rounded-full hover:bg-white hover:text-black">
-            <!-- wp:paragraph -->
-            <p class="overflow-hidden h-[18px]">
+            <!-- wp:group -->
+            <div class="wp-block-group overflow-hidden h-[18px]">
               <!-- wp:paragraph -->
               <a href="<?php echo get_home_url() . '/about'; ?>" class="inline-block">Dowiedz się więcej</a>
               <!-- /wp:paragraph -->
-            </p>
-            <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
           </div>
           <!-- /wp:group -->
 
