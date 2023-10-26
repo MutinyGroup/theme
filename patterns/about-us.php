dc37170abb1889bea14fc2fbb69c3e19936f1128
Tomek <TK-54-4B>
2023-10-26 12:40:48 +0200

    test

--- patterns/about-us.php
+++ patterns/about-us.php
@@ -172,19 +172,19 @@
               <!-- /wp:group -->
 
               <!-- wp:paragraph -->
-              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold"><span data-aos="fade-right" data-aos-duration="600" data-aos-offset="85">03</span< /p>
-                  <!-- /wp:paragraph -->
+              <p class="target-num inline-block desktop:text-[85px] text-[80px] font-semibold"><span data-aos="fade-right" data-aos-duration="600" data-aos-offset="85">03</span></p>
+              <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
 
             <!-- wp:group -->
             <div class="wp-block-group text-wrapper">
               <!-- wp:paragraph -->
-              <p class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]"><span data-aos="fade-right" data-aos-duration="600" data-aos-offset="85">Mamy plan</span< /p>
-                  <!-- /wp:paragraph -->
-                  <!-- wp:paragraph -->
-                  <p class="target-paragraph font-light leading-[22px]"><span data-aos="fade-right" data-aos-duration="600" data-aos-offset="85">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</span< /p>
-                      <!-- /wp:paragraph -->
+              <p class="target-title desktop:mb-[15px] mb-[5px] text-[25px] font-semibold leading-[30px]"><span data-aos="fade-right" data-aos-duration="600" data-aos-offset="85">Mamy plan</span></p>
+              <!-- /wp:paragraph -->
+              <!-- wp:paragraph -->
+              <p class="target-paragraph font-light leading-[22px]"><span data-aos="fade-right" data-aos-duration="600" data-aos-offset="85">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</span></p>
+              <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
           </li>
