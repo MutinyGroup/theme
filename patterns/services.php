77c13f9ce3c79ce747a977d22550c8872b7269ee
Tomek <TK-54-4B>
2023-10-26 12:45:39 +0200

    test

--- patterns/services.php
+++ patterns/services.php
@@ -58,7 +58,7 @@
       <!-- wp:group -->
       <div class="wp-block-group h-fit overflow-hidden">
         <!-- wp:paragraph -->
-        <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="section-title uppercase text-primaryYellow">Usługi</p>
+        <p class="section-title uppercase text-primaryYellow"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30">Usługi</span></p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -68,14 +68,14 @@
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph -->
-          <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">KAMPANIE REALIZUJEMY</p>
+          <p class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30"> KAMPANIE REALIZUJEMY</span></p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph {"templateLock":"contentOnly"} -->
-          <p data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">KOMPLEKSOWO</p>
+          <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30">KOMPLEKSOWO</span></p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
