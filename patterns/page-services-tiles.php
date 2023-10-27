58dd3aebc02222e26469f962d58885f3e2cccc8b
Tomek <TK-54-4B>
2023-10-27 11:54:19 +0200

    test

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -96,11 +96,11 @@
           <!-- wp:group -->
           <div class="wp-block-group h-full w-full">
             <!-- wp:group -->
-            <div class="wp-block-group group relative gallery-tile w-full before:content-[''] before:opacity-100 before:absolute before:top-0 before:left-0 before:z-[0] before:w-full before:h-full before:bg-cover before:transition before:duration-[300ms] before:ease-out <?php echo ($counter == 1) ? "clip-path-url-['#clipped-realizations-1'] tile-services-1" : (($counter == $post_query->post_count) ? "clip-path-url-['#clipped-realizations-2'] tile-services-2" : ''); ?>  z-[1] group desktop:p-[25px] desktop:h-[600px] h-[320px] overflow-hidden rounded-[20px] transition ease-out duration-300 mr-[8px] mb-[30px]">
+            <div class="wp-block-group group relative gallery-tile w-full before:content-[''] before:opacity-100 before:absolute before:top-0 before:left-0 before:z-[0] before:w-full before:h-full before:bg-cover before:transition before:duration-[300ms] before:ease-out clip-path-url-['#clipped-realizations-1'] tile-services-1 z-[1] group desktop:p-[25px] desktop:h-[600px] h-[320px] overflow-hidden rounded-[20px] transition ease-out duration-300 mr-[8px] mb-[30px]">
               <!-- wp:group -->
               <div class="wp-block-group img-clip-path-wrapper w-full h-full rounded-tr-[20px] rounded-br-[20px] overflow-hidden absolute left-0 top-0 z-[-1]">
                 <!-- wp:group -->
-                <div class="wp-block-group h-full group-hover:grayscale transition ease-in-out duration-[400ms] <?php echo ($counter == 1) ? "clip-path-url-['#clipped-realizations-1']" : (($counter == $post_query->post_count) ? "clip-path-url-['#clipped-realizations-2']" : ''); ?>">
+                <div class="wp-block-group h-full group-hover:grayscale transition ease-in-out duration-[400ms] clip-path-url-['#clipped-realizations-1'] tile-services-1">
                   <!-- wp:post-featured-image {"isLink":true} /-->
                 </div>
                 <!-- /wp:group -->
