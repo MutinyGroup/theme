2a0c503c45e34a8901f72d343c52e9fdbb36dfd1
Tomek <TK-54-4B>
2023-10-27 14:28:20 +0200

    test

--- patterns/page-realizations-events.php
+++ patterns/page-realizations-events.php
@@ -82,7 +82,7 @@
 
               <!-- wp:group -->
               <div class="wp-block-group cats-wrapper-realizacje">
-                <!-- wp:post-terms {"term":"category","className":"cats-list-container","separator": {"type": "string","default": ""},"data":"test"} /-->
+                <!-- wp:post-terms {"term":"category","className":"cats-list-container","separator": {"type": "string","default": ""},"attributes":"[data-type="test"]"} /-->
               </div>
               <!-- /wp:group -->
 
