b7960a72b16878522aa384a1c7709fe7dea66e35
Tomek <TK-54-4B>
2023-10-27 13:56:28 +0200

    test

--- patterns/realizations.php
+++ patterns/realizations.php
@@ -166,7 +166,7 @@
                     <!-- wp:group -->
                     <div class="wp-block-group tile-title z-[1] absolute bottom-[50px] desktop:left-[50px] left-[20px] pr-[20px]">
                       <!-- wp:paragraph -->
-                      <p class="w-fit desktop:text-[35px] text-[30px] desktop:mb-[20px] mb-[10px] leading-[42px] font-semibold desktop:group-hover:translate-y-[-50px] group-hover:translate-y-[-70px] transition ease-in-out duration-[400ms] cursor-pointer"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></p>
+                      <p class="w-fit desktop:text-[35px] text-[26px] desktop:mb-[20px] mb-[10px] desktop:leading-[42px] leading-[38px] font-semibold desktop:group-hover:translate-y-[-50px] group-hover:translate-y-[-70px] transition ease-in-out duration-[400ms] cursor-pointer"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></p>
                       <!-- /wp:paragraph -->
 
                       <!-- wp:list -->
