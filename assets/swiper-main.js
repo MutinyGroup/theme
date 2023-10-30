ab60632cdc8c8b5a3f8f59d70b5151a6dd5aa301
Tomek <TK-54-4B>
2023-10-30 09:18:20 +0100

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -152,7 +152,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     slidesPerView: 'auto',
     clickable: true,
     grabCursor: true,
-    slideClass: 'post',
     navigation: {
       nextEl: '#blog-next',
       prevEl: '#blog-prev',
