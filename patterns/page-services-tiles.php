9417de0805dc71e943417764935aafa6b45cb21e
Tomek <TK-54-4B>
2023-11-02 13:09:12 +0100

    test

--- patterns/page-services-tiles.php
+++ patterns/page-services-tiles.php
@@ -230,7 +230,7 @@
             <!-- /wp:group -->
 
             <!-- wp:paragraph -->
-            <p class="btn-swiper-more group z-[2] pr-[20px] desktop:pr-0 text-[14px] font-semibold uppercase absolute  desktop:bottom-[75px] bottom-[20px] desktop:left-[25px] left-[20px] transition ease-out duration-300">
+            <p class="btn-swiper-more group z-[2] pr-[20px] desktop:pr-0 text-[14px] font-semibold uppercase absolute pointer-events-none desktop:bottom-[75px] bottom-[20px] desktop:left-[25px] left-[20px] transition ease-out duration-300">
 
               <span>Dowiedz się więcej <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none " class="inline-block ml-[10px]">
                   <rect x="1" y="1" width="34" height="34" rx="18" stroke="#AAAAAA" stroke-width="2" fill="transparent" class="transition ease-out duration-300 group-hover:fill-[#FEC400] group-hover:stroke-[#FEC400]" />
