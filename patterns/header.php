ed3466f3b76bf42b7750490ba891c29e2fb2fb1d
Tomek <TK-54-4B>
2023-11-10 20:10:10 +0100

    test

--- patterns/header.php
+++ patterns/header.php
@@ -39,6 +39,8 @@ print_r(get_home_url());
   echo strpos($url, 'eng2');
   if (strpos($url, 'eng2') == '') {
     echo 'wersja PL ';
+    echo 'wersja ENG:  ';
+    print_r(str_contains($url, 'eng2'));
   ?>
     <!-- wp:list-item -->
     <li class="lang-btn group h-[48px] hover:h-[120px] hidden desktop:block p-[10px_12px] text-center uppercase font-medium border-[2px] border-[#AAAAAA] rounded-[50px] hover:rounded-[20px] absolute top-[20px] right-[20px] hover:bg-white hover:border-white hover:text-black transition-height ease-out duration-300">
@@ -60,6 +62,7 @@ print_r(get_home_url());
   <?php
   } elseif (str_contains($url, 'eng2') > 10) {
     echo '>10 eng2';
+    echo 'wersja ENG';
     print_r(str_contains($url, 'eng2'));
   ?>
     <!-- wp:list-item -->
