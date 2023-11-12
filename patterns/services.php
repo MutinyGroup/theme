27daeebb8ad92496614822da4a58b159680b9426
Tomek <TK-54-4B>
2023-11-12 21:21:42 +0100

    test

--- patterns/services.php
+++ patterns/services.php
@@ -67,16 +67,20 @@
       <div class="wp-block-group uslugi-title z-[1]">
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
-          <!-- wp:paragraph -->
-          <p class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30"> KAMPANIE REALIZUJEMY</span></p>
-          <!-- /wp:paragraph -->
+          <!-- wp:heading -->
+          <h2>
+            <p class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30"> KAMPANIE REALIZUJEMY</span></p>
+          </h2>
+          <!-- /wp:heading -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
-          <!-- wp:paragraph {"templateLock":"contentOnly"} -->
-          <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic "><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">KOMPLEKSOWO</span></p>
-          <!-- /wp:paragraph -->
+          <!-- wp:heading {"templateLock":"contentOnly"} -->
+          <h2>
+            <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic "><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">KOMPLEKSOWO</span></p>
+          </h2>
+          <!-- /wp:heading -->
         </div>
         <!-- /wp:group -->
 
