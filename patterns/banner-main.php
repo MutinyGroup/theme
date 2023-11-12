45963028887d96ec0e48aa107ec30d9b21121732
Tomek <TK-54-4B>
2023-11-12 20:42:09 +0100

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -64,15 +64,15 @@
         <!-- wp:group -->
         <div class="wp-block-group title-text-wrapper mb-[60px] desktop:mb-[70px] uppercase relative z-[1]">
           <!-- wp:paragraph -->
-          <h1 class="company-title font-normal text-[16px] leading-[125%] overflow-hidden"><span class="block">Mutiny Group</span></h1>
+          <p class="company-title font-normal text-[16px] leading-[125%] overflow-hidden"><span class="block">Mutiny Group</span></p>
           <!-- /wp:paragraph -->
 
           <!-- wp:paragraph -->
-          <h1 class="company-title font-medium overflow-hidden"><span class="block">Wchodzimy</span></h1>
+          <p class="company-title font-medium overflow-hidden"><span class="block">Wchodzimy</span></p>
           <!-- /wp:paragraph -->
 
           <!-- wp:paragraph -->
-          <h1 class="company-title font-medium overflow-hidden"><span class="block">NA RYNEK Z MOCNYM</span></h1>
+          <p class="company-title font-medium overflow-hidden"><span class="block">NA RYNEK Z MOCNYM</span></p>
           <!-- /wp:paragraph -->
 
           <!-- wp:group -->
--- patterns/services.php
+++ patterns/services.php
@@ -68,14 +68,14 @@
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph -->
-          <h2 class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30"> KAMPANIE REALIZUJEMY</span></h2>
+          <p class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30"> KAMPANIE REALIZUJEMY</span></p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group -->
         <div class="wp-block-group overflow-hidden">
           <!-- wp:paragraph {"templateLock":"contentOnly"} -->
-          <h2 class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic "><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">KOMPLEKSOWO</span></h2>
+          <p class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic "><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">KOMPLEKSOWO</span></p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
