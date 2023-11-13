2ed5780febd4d05549d88c181cf2c961e5e67bd3
Tomek <TK-54-4B>
2023-11-13 14:08:27 +0100

    test

--- patterns/services.php
+++ patterns/services.php
@@ -74,8 +74,8 @@
             <span class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic "><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">KOMPLEKSOWO</span></span>
           </span>
 
-          <!-- /wp:heading -->
         </h2>
+        <!-- /wp:heading -->
       </div>
       <!-- /wp:group -->
     </div>
