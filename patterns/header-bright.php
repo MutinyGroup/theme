5d7f811677eed6e874c50add3221bf281d11b110
Tomek <TK-54-4B>
2023-11-13 12:11:46 +0100

    test

--- patterns/header-bright.php
+++ patterns/header-bright.php
@@ -17,7 +17,7 @@ $url = home_url();
     <a href="<?php echo home_url(); ?>" class="fixed top-[32px] left-[22px] z-[3]">
       <!-- wp:image -->
       <figure class="wp-block-image w-[30px] mr-[10px]">
-        <?php echo home_url() . '/wp-content/themes/mutiny/assets/img/front-page/logo-mutiny-black.png" alt="webo-logo' ?>" />
+        <img src="<?php echo home_url() . '/wp-content/themes/mutiny/assets/img/front-page/logo-mutiny-black.png' ?>" alt="webo-logo" />
       </figure>
       <!-- /wp:image -->
     </a>
