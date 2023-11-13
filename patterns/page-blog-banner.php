654d712668e814b1941bac16400a5cc6221aaa3e
Tomek <TK-54-4B>
2023-11-13 16:35:31 +0100

    test

--- patterns/page-blog-banner.php
+++ patterns/page-blog-banner.php
@@ -33,18 +33,9 @@
         <!-- /wp:group -->
 
         <!-- wp:heading -->
-        <h2 class="wp-block-heading uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]">
-          <span class="block overflow-hidden">
-            <span class="test01 company-title block font-medium overflow-hidden"><span class="block">SPRAWDŹ CO</span></span>
-          </span>
-
-          <span class="block overflow-hidden">
-            <span class="uppercase test01 inline-block ">U NAS<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">SŁYCHAĆ</span></span>
-            <span class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></span>
-          </span>
-
-        </h2>
+        <h2 class="wp-block-heading uslugi-title uppercase font-medium desktop:text-[60px] text-[30px] desktop:leading-[60px] leading-[30px]"><span class="block overflow-hidden"><span class="test01 company-title block font-medium overflow-hidden"><span class="block">SPRAWDŹ CO</span></span></span><span class="block overflow-hidden"><span class="uppercase test01 inline-block ">U NAS<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">SŁYCHAĆ</span></span><span class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></span></span></h2>
         <!-- /wp:heading -->
+
       </div>
       <!-- /wp:group -->
     </div>
