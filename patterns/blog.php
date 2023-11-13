8285ef0ab9f2aa10348df9dc4fe8e7f05b1a4608
Tomek <TK-54-4B>
2023-11-13 14:24:51 +0100

    test

--- patterns/blog.php
+++ patterns/blog.php
@@ -63,22 +63,7 @@
       <!-- /wp:group -->
 
       <!-- wp:heading -->
-      <h2 class="wp-block-heading blog-title z-[1]">
-
-        <span class="block overflow-hidden">
-
-          <span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">Aktualności i wydarzenia</span>
-
-        </span>
-
-
-        <span class="block overflow-hidden">
-
-          <span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">z Mutiny Group</span>
-
-        </span>
-
-      </h2>
+      <h2 class="wp-block-heading blog-title z-[1]"><span class="block overflow-hidden"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="uppercase inline-block desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px]">Aktualności i wydarzenia</span></span><span class="block overflow-hidden"><span data-aos="fade-up" data-aos-duration="600" data-aos-offset="30" class="relative ml-[12px] desktop:text-[50px] text-[24px] desktop:leading-[55px] leading-[30px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-white">z Mutiny Group</span></span></h2>
       <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
