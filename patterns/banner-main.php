17149526bf6d755b201cf646a19f4dc4d61dd847
Tomek <TK-54-4B>
2023-11-13 12:59:12 +0100

    test title

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -75,25 +75,7 @@
           <p class="company-title font-medium overflow-hidden"><span class="block">NA RYNEK Z MOCNYM</span></p>
           <!-- /wp:paragraph -->
 
-          <!-- wp:group -->
-          <div class="wp-block-group relative italic font-medium  company-title overflow-hidden">
-            <!-- wp:group -->
-            <div class="wp-block-group test01 flex transition ease-out duration-300">
-              <!-- wp:group -->
-              <div class="wp-block-group self-end mb-[5px] desktop:mr-[70px] mr-[5px]  desktop:w-[280px] w-[30px] desktop:h-[6px] h-[2px] bg-white">
-                <!-- wp:paragraph -->
-                <p class="hidden">separator</p>
-                <!-- /wp:paragraph -->
-              </div>
-              <!-- /wp:group -->
 
-              <!-- wp:paragraph -->
-              <p>UDERZENIEM</p>
-              <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-          </div>
-          <!-- /wp:group -->
         </h1>
         <!-- /wp:heading -->
 
