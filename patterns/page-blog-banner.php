28e8819077106d7fdffe9045f908b37b6bd14293
Tomek <TK-54-4B>
2023-11-13 16:32:11 +0100

    test

--- patterns/page-blog-banner.php
+++ patterns/page-blog-banner.php
@@ -42,51 +42,51 @@
             <span class="uppercase test01 inline-block ">U NAS<span class="relative ml-[12px] inline-block uppercase italic before:content-[''] before:inline-block desktop:before:mr-[18px] before:mr-[8px] before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[30px] desktop:before:h-[3px] before:h-[2px] before:bg-black">SŁYCHAĆ</span></span>
             <span class="test01 inline-block company-title font-medium"><span class="ml-[-8px] desktop:ml-[-16px] desktop:text-[100px] text-[70px] leading-[1px] desktop:leading-auto">.</span></span>
           </span>
-          <!-- /wp:group -->
+
+        </h2>
+        <!-- /wp:heading -->
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
-</div>
-<!-- /wp:group -->
-<!-- wp:group -->
-<div class="wp-block-group relative">
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] mb-[80px]">
-    <!-- wp:paragraph -->
-    <p class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px] mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
-    <!-- /wp:paragraph -->
-
+  <div class="wp-block-group relative">
     <!-- wp:group -->
-    <div class="wp-block-group btn-more group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full z-[1] desktop:absolute desktop:top-[-130px] top-[320px] wide:left-[70%] desktop:left-[85%] left-[50%] desktop:translate-x-[-50%] transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer overflow-hidden">
-      <!-- wp:group -->
-      <div class="wp-block-group yellow-bg w-[140px] desktop:w-full h-[140px] desktop:h-full rounded-full bg-primaryYellow scale-100 group-hover:scale-[85%] absolute transition ease-in-out duration-[700ms] z-0">
-        <!-- wp:paragraph -->
-        <p class="hidden">yellow bg</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-
-      <!-- wp:group -->
-      <div class="wp-block-group white-bg w-full h-full rounded-full bg-white scale-0 group-hover:scale-[85%] absolute transition ease-in-out duration-[500ms] z-[1]">
-        <!-- wp:paragraph -->
-        <p class="hidden">white bg</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-
+    <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] mb-[80px]">
       <!-- wp:paragraph -->
-      <p class="z-[2] text-white group-hover:text-black transition ease-in-out duration-[550ms]"><a href="#Posty"><span class="font-bold block">Zobacz</span><span class="font-bold block text-[25px]">WIĘCEJ</span></a></p>
+      <p class="text text-[20px] leading-[30px] desktop:max-w-[445px] desktop:ml-[200px] mb-[80px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
       <!-- /wp:paragraph -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group btn-more group desktop:w-[175px] w-[140px] desktop:h-[175px] h-[140px] mx-auto text-center rounded-full z-[1] desktop:absolute desktop:top-[-130px] top-[320px] wide:left-[70%] desktop:left-[85%] left-[50%] desktop:translate-x-[-50%] transition ease-out duration-300 flex flex-col justify-center items-center gap-y-0 cursor-pointer overflow-hidden">
+        <!-- wp:group -->
+        <div class="wp-block-group yellow-bg w-[140px] desktop:w-full h-[140px] desktop:h-full rounded-full bg-primaryYellow scale-100 group-hover:scale-[85%] absolute transition ease-in-out duration-[700ms] z-0">
+          <!-- wp:paragraph -->
+          <p class="hidden">yellow bg</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group white-bg w-full h-full rounded-full bg-white scale-0 group-hover:scale-[85%] absolute transition ease-in-out duration-[500ms] z-[1]">
+          <!-- wp:paragraph -->
+          <p class="hidden">white bg</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:paragraph -->
+        <p class="z-[2] text-white group-hover:text-black transition ease-in-out duration-[550ms]"><a href="#Posty"><span class="font-bold block">Zobacz</span><span class="font-bold block text-[25px]">WIĘCEJ</span></a></p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
     </div>
     <!-- /wp:group -->
-
   </div>
   <!-- /wp:group -->
-</div>
-<!-- /wp:group -->
 
 
 
